542caa9766388bd9cb5a9def5811cb21f178f57e
lichao.lc <lichao.lc@alibaba-inc.com>
2019-05-14 17:46:40 +0800

    refactor: 调整代码结构适应新的renderApp

--- packages/main-layout/example/app.tsx
+++ packages/main-layout/example/app.tsx
@@ -1,8 +1,10 @@
 import { renderApp } from '@ali/ide-dev-tool/src/dev-app';
 import { FileTreeModule } from '../../file-tree/src/browser';
+import { MenuBarModule } from '../../menu-bar/src/browser';
 import { MainLayoutModule } from '../src/browser';
 const moduleInstance = new MainLayoutModule();
 
 renderApp(moduleInstance, [
+  new MenuBarModule(),
   new FileTreeModule(),
 ]);
