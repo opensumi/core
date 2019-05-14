3434b66753f88384577338c05bd8bbeffc14d88c
lichao.lc <lichao.lc@alibaba-inc.com>
2019-05-14 17:49:42 +0800

    feat: menu-bar基础框架

---
+++ packages/menu-bar/src/common/menu-bar.defination.ts
@@ -0,0 +1,13 @@
+import { Injectable, Provider } from '@ali/common-di';
+import { ConstructorOf } from '@ali/ide-core';
+
+@Injectable()
+export abstract class MenuBarAPI {
+}
+
+export function createMenuBarAPIProvider<T extends MenuBarAPI>(cls: ConstructorOf<T>): Provider {
+  return {
+    token: MenuBarAPI as any,
+    useClass: cls as any,
+  };
+}
