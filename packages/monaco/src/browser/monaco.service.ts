7fb77dd75915c7963400e9cf140d75e8351a83ac
ensorrow <zheyanglv@qq.com>
2019-05-15 15:14:52 +0800

    feat: 新增ts的tokenize和theme支持

--- packages/monaco/src/browser/monaco.service.ts
+++ packages/monaco/src/browser/monaco.service.ts
@@ -1,11 +1,25 @@
 import { observable } from 'mobx';
 import { Injectable, Autowired } from '@ali/common-di';
 import { Disposable } from '@ali/ide-core-browser';
+import { LanguageRegistry } from './language-registry';
+import { MonacoThemeRegistry } from './theme-registry';
+
+const initValue = `
+function test() {
+  console.log('hello world');
+}
+`;
 
 @Injectable()
 export default class MonacoService extends Disposable {
 
   private editor!: monaco.editor.IStandaloneCodeEditor;
+
+  @Autowired()
+  private languageRegistry!: LanguageRegistry;
+
+  @Autowired()
+  private themeRegistry!: MonacoThemeRegistry;
 
   constructor() {
     super();
@@ -14,7 +28,7 @@ export default class MonacoService extends Disposable {
   async initMonaco(monacoContainer: HTMLElement) {
     this.editor = monaco.editor.create(monacoContainer, {
       model: monaco.editor.createModel(
-        'console.log("hello world")',
+        initValue,
         'typescript',
         monaco.Uri.parse('inmemory://test.ts'),
       ),
@@ -24,5 +38,11 @@ export default class MonacoService extends Disposable {
       },
       automaticLayout: true,
     });
+    const currentTheme = this.themeRegistry.register(require('./themes/dark_plus.json'), {
+      './dark_defaults.json': require('./themes/dark_defaults.json'),
+      './dark_vs.json': require('./themes/dark_vs.json'),
+    }, 'dark-plus', 'vs-dark').name as string;
+    monaco.editor.setTheme(currentTheme);
+    await this.languageRegistry.initialize(this.themeRegistry.getTheme(currentTheme));
   }
 }
