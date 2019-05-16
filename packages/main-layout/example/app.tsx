fe5297e36857b4fdb6cfa056dd74ae69e419e15e
lichao.lc <lichao.lc@alibaba-inc.com>
2019-05-16 09:55:18 +0800

    feat: 将monaco和main-layout进行集成

--- packages/main-layout/example/app.tsx
+++ packages/main-layout/example/app.tsx
@@ -1,5 +1,6 @@
 import { renderApp } from '@ali/ide-dev-tool/src/dev-app';
 import { FileTreeModule } from '../../file-tree/src/browser';
+import { MonacoModule } from '../../Monaco/src/browser';
 import { MenuBarModule } from '../../menu-bar/src/browser';
 import { MainLayoutModule } from '../src/browser';
 const moduleInstance = new MainLayoutModule();
@@ -7,4 +8,5 @@ const moduleInstance = new MainLayoutModule();
 renderApp(moduleInstance, [
   new MenuBarModule(),
   new FileTreeModule(),
+  new MonacoModule(),
 ]);
