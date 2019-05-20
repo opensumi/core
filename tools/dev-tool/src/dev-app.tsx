e6808e1997bb386ea76d00c9eca213e2d057c813
erha19 <danwu.wdw@alibaba-inc.com>
2019-05-20 10:35:48 +0800

    chore: 在example中引入的公共样式

--- tools/dev-tool/src/dev-app.tsx
+++ tools/dev-tool/src/dev-app.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { App, SlotLocation, SlotMap, BrowserModule } from '@ali/ide-core-browser';
+import 'font-awesome/css/font-awesome.min.css';
+import '@ali/ide-core-browser/lib/style/index.less';
 
 export function renderApp(main: BrowserModule, modules: BrowserModule[] = []) {
   const { value: component } = main.slotMap.values().next();
