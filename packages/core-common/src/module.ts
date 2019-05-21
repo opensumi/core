fbfbca3d876bd113516816272c24aff899452177
suyu34 <Joooye1991@gmail.com>
2019-05-21 11:57:15 +0800

    test: 增加测试使用的创建 injector 的辅助函数

--- packages/core-common/src/module.ts
+++ packages/core-common/src/module.ts
@@ -2,9 +2,11 @@
  * 项目中会使用到的模块接口定义
  */
 
-import { Autowired, INJECTOR_TOKEN, Injector } from '@ali/common-di';
+import { Autowired, INJECTOR_TOKEN, Injector, Provider } from '@ali/common-di';
 
 export class BasicModule {
   @Autowired(INJECTOR_TOKEN)
   protected injector: Injector;
+
+  providers?: Provider[];
 }
