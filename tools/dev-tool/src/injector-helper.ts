ded74e1c92d77083515166f84842d296ef1a56a5
suyu34 <Joooye1991@gmail.com>
2019-05-22 17:30:00 +0800

    test: 提供一个带有 mock 能力的 injector 对象

--- tools/dev-tool/src/injector-helper.ts
+++ tools/dev-tool/src/injector-helper.ts
@@ -1,16 +1,17 @@
 import { ConstructorOf } from '@ali/ide-core-common';
-import { Injector } from '@ali/common-di';
 import { BrowserModule, RootApp } from '@ali/ide-core-browser';
 import { NodeModule } from '@ali/ide-core-node';
+import { MockInjector } from './mock-injector';
 
 export function createBrowserInjector(modules: Array<ConstructorOf<BrowserModule>>) {
-  const app = new RootApp({ modules });
-  return app.injector;
+  const injector = new MockInjector();
+  const app = new RootApp({ modules, injector });
+  return app.injector as MockInjector;
 }
 
 export function createNodeInjector(constructors: Array<ConstructorOf<NodeModule>>) {
   // TODO: 等 Node 这边的加载器写好之后，再把这里改一下
-  const injector = new Injector();
+  const injector = new MockInjector();
 
   for (const item of constructors) {
     const instance = injector.get(item);
---
+++ tools/dev-tool/src/mock-injector.ts
@@ -0,0 +1,35 @@
+import { Injector, Token, TokenResult, InstanceOpts, ConstructorOf } from '@ali/common-di';
+import mm from '@ali/mm';
+
+afterEach(() => {
+  mm.restore();
+});
+
+export class MockInjector extends Injector {
+  // tslint:disable-next-line
+  private mockMap = new Map<Token, [any, any]>();
+
+  mock<T extends Token, K extends keyof TokenResult<T>>(token: T, method: K, value: TokenResult<T>[K]) {
+    if (this.hasInstance(token)) {
+      const instance = this.get(token);
+      mm(instance, method as any, value);
+    } else {
+      this.mockMap.set(token, [method, value]);
+    }
+  }
+
+  get<T extends ConstructorOf<any>>(token: T, args?: ConstructorParameters<T>, opts?: InstanceOpts): TokenResult<T>;
+  get<T extends Token>(token: T, opts?: InstanceOpts): TokenResult<T>;
+  get(arg1: any, arg2?: any, arg3?: any) {
+    const instance = super.get(arg1, arg2, arg3);
+
+    const mockDefination = this.mockMap.get(arg1);
+    if (mockDefination) {
+      const method = mockDefination[0];
+      const value = mockDefination[1];
+      mm(instance, method, value);
+    }
+
+    return instance;
+  }
+}
