a124db7b39411f6ffae3f30729c8b6063541f6f1
lichao.lc <lichao.lc@alibaba-inc.com>
2019-05-22 12:02:02 +0800

    feat: 给ResizePayload增加slotLocation的参数

--- packages/main-layout/src/browser/ide-widget.ts
+++ packages/main-layout/src/browser/ide-widget.ts
@@ -1,15 +1,16 @@
 import { Injectable, Autowired, Optinal } from '@ali/common-di';
-import { IEventBus, IEventLisnter, IEventFireOpts, EventBusImpl, BasicEvent } from '@ali/ide-core-common';
+import { IEventBus, BasicEvent } from '@ali/ide-core-common';
 import { Widget } from '@phosphor/widgets';
+import { SlotLocation } from '@ali/ide-main-layout';
 
 export class ResizePayload {
-  constructor(public target: HTMLElement, public width: number, public height: number) {
+  constructor(public width: number, public height: number, public slotLocation: SlotLocation) {
   }
-
 }
 export class ResizeEvent extends BasicEvent<ResizePayload> {}
 
-const WIDGET_TOKEN = Symbol();
+const WIDGET_OPTION = Symbol();
+const WIDGET_LOCATION = Symbol();
 
 @Injectable()
 export class IdeWidget extends Widget {
@@ -17,12 +18,12 @@ export class IdeWidget extends Widget {
   @Autowired(IEventBus)
   private eventBus!: IEventBus;
 
-  constructor(@Optinal(WIDGET_TOKEN) options?: Widget.IOptions) {
+  constructor(@Optinal(WIDGET_LOCATION) private slotLocation: SlotLocation, @Optinal(WIDGET_OPTION) options?: Widget.IOptions) {
     super(options);
   }
 
   onResize = (resizeMessage: Widget.ResizeMessage) => {
-    this.eventBus.fire(new ResizeEvent(new ResizePayload(this.node, resizeMessage.width, resizeMessage.height)));
+    this.eventBus.fire(new ResizeEvent(new ResizePayload(resizeMessage.width, resizeMessage.height, this.slotLocation)));
   }
 
 }
