4593c8d2dd728d87d2406c16b0246a771253f524
intellism <intellism@gmail.com>
2019-05-22 14:11:42 +0800

    test: fileService Client

---
+++ packages/file-service/src/browser/file-service-client.ts
@@ -0,0 +1,27 @@
+
+// import { Event } from '@ali/ide-core-common/lib/event';
+import { Injectable, Inject } from '@ali/common-di';
+import { servicePath as FileServicePath, IFileService, FileStat } from '../common/index';
+import { TextDocumentContentChangeEvent } from '.3.14.0@vscode-languageserver-types';
+
+@Injectable()
+export class FileServiceClient {
+  constructor(@Inject(FileServicePath) protected readonly fileService: IFileService) {
+  }
+
+  async resolveContent(uri: string, options?: { encoding?: string }) {
+    return this.fileService.resolveContent(uri, options);
+  }
+
+  async getFileStat(uri: string) {
+    return this.fileService.getFileStat(uri);
+  }
+
+  async updateContent(file: FileStat, contentChanges: TextDocumentContentChangeEvent[], options?: { encoding?: string }) {
+    return this.fileService.updateContent(file, contentChanges, options);
+  }
+
+  async createFile(uri: string, options?: { content?: string, encoding?: string }) {
+    return this.fileService.createFile(uri, options);
+  }
+}
--- packages/file-service/src/node/index.ts
+++ packages/file-service/src/node/index.ts
@@ -2,8 +2,7 @@ import { NodeModule } from '@ali/ide-core-node';
 import { FileSystemNodeOptions, FileService } from './file-service';
 import { Injectable } from '@ali/common-di';
 export * from './file-service';
-import {servicePath} from '../common/index';
-import {servicePath as FileTreeServicePath} from '@ali/ide-file-tree';
+import { servicePath } from '../common/index';
 @Injectable()
 export class FileServiceModule extends NodeModule {
   providers = [{ token: 'FileServiceOptions', useValue: FileSystemNodeOptions.DEFAULT }];
@@ -12,11 +11,6 @@ export class FileServiceModule extends NodeModule {
     {
       servicePath,
       token: FileService,
-    },
-  ];
-  frontServices = [
-    {
-      servicePath: FileTreeServicePath,
     },
   ];
 }
