8770a6dc910dff106e4b0689225450d6b0f7a1c5
ensorrow <zheyanglv@qq.com>
2019-05-23 20:26:53 +0800

    feat: 添加一些语言支持

--- packages/monaco/src/browser/languages/index.ts
+++ packages/monaco/src/browser/languages/index.ts
@@ -3,11 +3,22 @@ import {TypescriptContribution} from './ts';
 import {JsxTagsContribution} from './jsx-tags';
 import {CssContribution} from './css';
 import {HtmlContribution} from './html';
+import {LessContribution} from './less';
+import {MarkdownContribution} from './markdown';
+import {PythonContribution} from './python';
+import {ScssContribution} from './scss';
+import {XmlContribution} from './xml';
+
 
 export const languageTokens = [
   JavascriptContribution,
   TypescriptContribution,
   CssContribution,
   HtmlContribution,
-  JsxTagsContribution
+  JsxTagsContribution,
+  LessContribution,
+  MarkdownContribution,
+  PythonContribution,
+  ScssContribution,
+  XmlContribution,
 ];
