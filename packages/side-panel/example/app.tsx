2f4689678a03e8839deefae1174532df9f845aa6
lichao.lc <lichao.lc@alibaba-inc.com>
2019-05-29 17:05:55 +0800

    refactor: 删除原side-panel模块，用activator-bar和activator-panel模块替代

--- packages/side-panel/example/app.tsx
+++
@@ -1,4 +0,0 @@
-import { renderApp } from '@ali/ide-dev-tool/src/dev-app';
-import { LeftPanelModule } from '../src/browser/left-panel-module';
-const moduleInstance = new LeftPanelModule();
-renderApp(moduleInstance);
