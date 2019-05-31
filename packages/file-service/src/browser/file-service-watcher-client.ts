0ebe1d5783410d1ef3d910d4443b3317410e9040
intellism <intellism@gmail.com>
2019-05-31 17:55:07 +0800

    feat: 添加file watcher client

---
+++ packages/file-service/src/browser/file-service-watcher-client.ts
@@ -0,0 +1,32 @@
+import { Injectable, Autowired } from '@ali/common-di';
+import { IDisposable, Disposable, URI, Emitter, Event } from '@ali/ide-core-common';
+import { FileChangeEvent, DidFilesChangedParams, FileChange } from '../common/file-service-watcher-protocol';
+import { servicePath as FileServicePath, FileWatcherServicePath } from '../common/index';
+
+@Injectable()
+export class FileServiceWatcherClient {
+  protected readonly onFileChangedEmitter = new Emitter<FileChangeEvent>();
+  readonly onFilesChanged: Event<FileChangeEvent> = this.onFileChangedEmitter.event;
+
+  @Autowired(FileWatcherServicePath)
+  private fileWatcherService;
+
+  onDidFilesChanged(event: DidFilesChangedParams): void {
+    const changes: FileChange[] = event.changes.map((change) => {
+      return {
+        uri: change.uri,
+        type: change.type,
+      } as FileChange;
+    });
+    this.onFileChangedEmitter.fire(changes);
+  }
+
+  // 添加监听文件
+  async watchFileChanges(uri: URI): Promise<IDisposable> {
+    const watcher = await this.fileWatcherService.watchFileChanges;
+    const toDispose = Disposable.create(() => {
+      this.fileWatcherService.unwatchFileChanges(watcher);
+    });
+    return toDispose;
+  }
+}
