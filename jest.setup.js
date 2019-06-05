ff030848fd8919483101b5b160bfe4b07717c857
frankLife <shangpo.zw@alibaba-inc.com>
2019-06-05 13:45:20 +0800

    chore: 增加 jest 前置浏览器环境

---
+++ jest.setup.js
@@ -0,0 +1,10 @@
+const {JSDOM} = require('jsdom');
+
+const jsdom = new JSDOM(``, {
+  resources: 'usable',
+  runScripts: 'dangerously',
+});
+global.document = jsdom.window.document;
+global.navigator = jsdom.window.navigator;
+global.Element = jsdom.window.Element;
+// global.window = jsdom.window;
