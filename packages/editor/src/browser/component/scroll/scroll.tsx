ec7af70a7732d0d16b0ac803935c4e6ecd07e6d9
MMhunter <kengtou.mh@antfin.com>
2019-06-05 11:35:17 +0800

    feat(tabs): 修复tabs scroll

---
+++ packages/editor/src/browser/component/scroll/scroll.tsx
@@ -0,0 +1,306 @@
+import * as React from 'react';
+import * as styles from './scroll.module.less';
+import classnames from 'classnames';
+import {MouseEvent, UIEvent } from 'react';
+
+export interface ScrollAreaProps {
+  className?: string;
+  onScroll?: (position: ScrollPosition) => any;
+  atTopClassName?: string;
+  style?: any;
+  containerStyle?: any;
+}
+
+export interface ScrollPosition {
+  top: number;
+  left: number;
+}
+
+export interface ScrollSizes {
+  scrollHeight: number;
+  offsetHeight: number;
+  offsetWidth: number;
+  scrollWidth: number;
+}
+
+export class Scroll extends React.Component<ScrollAreaProps, any> {
+
+  public ref: HTMLDivElement;
+
+  public container: HTMLDivElement;
+
+  private thumbV!: HTMLDivElement;
+
+  private trackV!: HTMLDivElement;
+
+  private thumbH!: HTMLDivElement;
+
+  private trackH!: HTMLDivElement;
+
+  private size: ScrollSizes;
+
+  private position: ScrollPosition = {
+    top: 0,
+    left: 0,
+  };
+
+  private dragging: boolean = false;
+
+  private draggingStart: number = 0;
+
+  private draggingStartPos: number = 0;
+
+  private requestFrame: any;
+
+  private shouldHideThumb: boolean = true;
+
+  private isAtTop: boolean = true;
+
+  onScroll(e: UIEvent<HTMLDivElement>) {
+    this.position = {
+      top: this.ref.scrollTop,
+      left: this.ref.scrollLeft,
+    };
+    if (this.props.onScroll) {
+      this.props.onScroll(this.position);
+    }
+    this.update(() => {
+      const contentWidth = this.ref.scrollWidth;
+      const width = this.ref.offsetWidth;
+      const contentHeight = this.ref.scrollHeight;
+      const height = this.ref.offsetHeight;
+      this.thumbH.style.left = (this.position.left * width) / contentWidth  + 'px';
+      this.thumbV.style.top = (this.position.top * height) / contentHeight  + 'px';
+    });
+    if (!this.isAtTop && this.ref.scrollTop === 0) {
+      this.isAtTop = true;
+      this.setCss();
+    } else if (this.isAtTop && this.ref.scrollTop !== 0) {
+      this.isAtTop = false;
+      this.setCss();
+    }
+  }
+
+  scrollTo(position: ScrollPosition) {
+    this.ref.scrollLeft = position.left;
+    this.ref.scrollTop = position.top;
+  }
+
+  onMouseDownHorizontal(e: MouseEvent<HTMLDivElement>) {
+    this.dragging = true;
+    if (e.target === this.trackH) {
+      this.onMouseDownOnTrack(e);
+    }
+    this.draggingStart = e.pageX;
+    this.draggingStartPos = this.ref.scrollLeft;
+    document.addEventListener('mousemove', this.onMouseMoveHorizontal);
+    document.addEventListener('mouseup', this.onMouseUpHorizontal);
+  }
+
+  onMouseMoveHorizontal = (e) => {
+    if (!this.dragging) {
+      return;
+    }
+    const move = e.pageX - this.draggingStart;
+    this.ref.scrollLeft = this.draggingStartPos + this.calculateXToLeft(move);
+  }
+
+  onMouseUpHorizontal = (e) => {
+    this.dragging = false;
+    document.removeEventListener('mousemove', this.onMouseMoveHorizontal);
+    document.removeEventListener('mouseup', this.onMouseUpHorizontal);
+    if (this.shouldHideThumb) {
+      this.hideThumb();
+    }
+  }
+
+  onMouseDownOnTrack(e: MouseEvent<HTMLDivElement>) {
+    const track = e.target as HTMLDivElement;
+    const x = e.clientX - track.getBoundingClientRect().left;
+    const contentWidth = this.ref.scrollWidth;
+    const width = this.ref.offsetWidth;
+    const left = x * contentWidth / width - 0.5 * width;
+    this.scrollTo({
+      left,
+      top: this.position.top,
+    });
+  }
+
+  calculateXToLeft(x) {
+    const contentWidth = this.ref.scrollWidth;
+    const width = this.ref.offsetWidth;
+    return x * contentWidth / width;
+  }
+
+  onMouseDownVertical(e: MouseEvent<HTMLDivElement>) {
+    this.dragging = true;
+    if (e.target === this.trackV) {
+      this.onMouseDownOnTrackVertical(e);
+    }
+    this.draggingStart = e.pageY;
+    this.draggingStartPos = this.ref.scrollTop;
+    document.addEventListener('mousemove', this.onMouseMoveVertical);
+    document.addEventListener('mouseup', this.onMouseUpVertical);
+  }
+
+  onMouseMoveVertical = (e) => {
+    if (!this.dragging) {
+      return;
+    }
+    const move = e.pageY - this.draggingStart;
+    this.ref.scrollTop = this.draggingStartPos + this.calculateYToTop(move);
+  }
+
+  onMouseUpVertical = (e) => {
+    this.dragging = false;
+    document.removeEventListener('mousemove', this.onMouseMoveVertical);
+    document.removeEventListener('mouseup', this.onMouseUpVertical);
+    if (this.shouldHideThumb) {
+      this.hideThumb();
+    }
+  }
+
+  onMouseDownOnTrackVertical(e: MouseEvent<HTMLDivElement>) {
+    const track = e.target as HTMLDivElement;
+    const x = e.clientY - track.getBoundingClientRect().top;
+    const contentHeight = this.ref.scrollHeight;
+    const height = this.ref.offsetHeight;
+    const top = x * contentHeight / height - 0.5 * height;
+    this.scrollTo({
+      left: this.position.left,
+      top,
+    });
+  }
+
+  calculateYToTop(y) {
+    const contentHeight = this.ref.scrollHeight;
+    const height  = this.ref.offsetHeight;
+    return y * contentHeight / height;
+  }
+
+  componentDidUpdate() {
+    this.update();
+  }
+
+  componentDidMount() {
+    this.update();
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+    if (this.requestFrame) {
+      window.cancelAnimationFrame(this.requestFrame);
+    }
+  }
+
+  handleWindowResize = () => {
+    this.update();
+  }
+
+  sizeEqual(size1: ScrollSizes, size2: ScrollSizes): boolean {
+    return size1 && size2 && size1.offsetHeight === size2.offsetHeight
+      && size1.scrollHeight === size2.scrollHeight && size1.offsetWidth === size2.offsetWidth
+      && size1.scrollWidth === size2.scrollWidth;
+  }
+
+  update(callback?) {
+    if (this.requestFrame) {
+      window.cancelAnimationFrame(this.requestFrame);
+    }
+    this.requestFrame = window.requestAnimationFrame(() => {
+      this._update();
+      if (callback) {
+        callback();
+      }
+    });
+  }
+
+  _update() {
+    if (this.ref) {
+      if (!this.sizeEqual(this.size, this.ref)) {
+        this.size = {
+          offsetHeight: this.ref.offsetHeight,
+          offsetWidth: this.ref.offsetWidth,
+          scrollWidth: this.ref.scrollWidth,
+          scrollHeight: this.ref.scrollHeight,
+        };
+        this.updateScrollBar();
+      }
+    }
+  }
+
+  updateScrollBar() {
+    const contentWidth = this.ref.scrollWidth;
+    const width = this.ref.offsetWidth;
+    if (width < contentWidth) {
+      const thumbHWidth = width * width / contentWidth;
+      this.thumbH.style.width = thumbHWidth + 'px';
+      this.trackH.parentElement!.style.display = 'block';
+    } else {
+      this.trackH.parentElement!.style.display = 'none';
+    }
+    const contentHeight = this.ref.scrollHeight;
+    const height = this.ref.offsetHeight;
+    if (height < contentHeight) {
+      this.thumbV.style.height = height * height / contentHeight + 'px';
+      this.trackV.parentElement!.style.display = 'block';
+    } else {
+      this.trackV.parentElement!.style.display = 'none';
+    }
+  }
+
+  hideThumb() {
+    this.shouldHideThumb = true;
+    if (!this.dragging) {
+      this.setCss();
+    }
+  }
+
+  showThumb() {
+    this.shouldHideThumb = false;
+    this.setCss();
+  }
+
+  setCss() {
+    this.container.className = classnames({
+      [styles.scroll] : true,
+      [styles['hide-thumb']]: this.shouldHideThumb && !this.dragging,
+    });
+    const cls = {
+      [this.props.atTopClassName || '']: this.isAtTop,
+    };
+    if (this.props.className) {
+      cls[this.props.className] = true;
+    }
+    this.ref.className = classnames(cls);
+  }
+
+  render() {
+    return (<div className={classnames(styles.scroll, styles['hide-thumb'])} ref = {(e) => e && (this.container = e)}
+                 onMouseMove={() => this.showThumb()}
+                 onMouseLeave={() => this.hideThumb()}
+                  style = {this.props.containerStyle}>
+                  <div style={this.props.style}
+                 className={classnames(this.props.className)}
+                 onScroll={this.onScroll.bind(this)} ref={(e) => e && (this.ref = e) }
+                  onMouseEnter={() => this.update()}
+                  onMouseDown={() => this.update()}
+                  onMouseUp={() => this.update()}>
+                  {this.props.children}
+                  </div>
+                  <div className={styles['track-horizontal']}>
+                    <div className={styles['track-horizontal']} ref={(e) => e && (this.trackH = e) }
+                         onMouseDown={this.onMouseDownHorizontal.bind(this)}/>
+                      <div className={styles['thumb-horizontal']} onMouseDown={this.onMouseDownHorizontal.bind(this)}
+                           ref={(e) => e && (this.thumbH = e)}/>
+                  </div>
+                  <div className={styles['track-vertical']}>
+                    <div className={styles['track-vertical']} ref={(e) => e && (this.trackV = e) }
+                         onMouseDown={this.onMouseDownVertical.bind(this)}/>
+                    <div className={styles['thumb-vertical']} onMouseDown={this.onMouseDownVertical.bind(this)}
+                         ref={(e) => e && (this.thumbV = e) }/>
+                  </div>
+            </div>);
+  }
+}
--- packages/editor/src/browser/tab.view.tsx
+++ packages/editor/src/browser/tab.view.tsx
@@ -5,8 +5,11 @@ import { useInjectable } from '@ali/ide-core-browser/lib/react-hooks';
 import { IResource } from '../common';
 import * as styles from './editor.module.less';
 import classnames from 'classnames';
-import { MaybeNull } from '@ali/ide-core-browser';
+import { MaybeNull, IEventBus } from '@ali/ide-core-browser';
 import { PerfectScrollbar } from '@ali/ide-core-browser/lib/components';
+import { ResizeEvent } from '@ali/ide-main-layout/lib/browser/ide-widget.view';
+import { SlotLocation } from '@ali/ide-main-layout';
+import { Scroll } from './component/scroll/scroll';
 export interface ITabsProps {
   resources: IResource[];
   currentResource: MaybeNull<IResource>;
@@ -17,7 +20,38 @@ export interface ITabsProps {
 }
 
 export const Tabs = observer(({resources, currentResource, onActivate, onClose, onDragStart, onDrop}: ITabsProps) => {
-  return <div className={styles.kt_editor_tabs}><PerfectScrollbar style={ {width: '100%', height: '35px'} } options={{suppressScrollY: true}}>
+  const currentTabRef = React.useRef<HTMLElement>();
+  const tabContainer = React.useRef<HTMLDivElement | null>();
+
+  const eventBus = useInjectable(IEventBus) as IEventBus;
+
+  function scrollToCurrent() {
+    if (currentTabRef.current && tabContainer.current) {
+      scrollToTabEl(tabContainer.current, currentTabRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+
+    if (tabContainer.current) {
+      tabContainer.current.addEventListener('mousewheel', preventNavigation as any);
+    }
+    scrollToCurrent();
+    const disposer = eventBus.on(ResizeEvent, (event) => {
+      if (event.payload.slotLocation === SlotLocation.topPanel) {
+        // TODO 监听逻辑可能需要修改
+        scrollToCurrent();
+      }
+    });
+    return () => {
+      disposer.dispose();
+      tabContainer.current!.removeEventListener('mousewheel', preventNavigation as any);
+    };
+  });
+
+  return <div className={styles.kt_editor_tabs}>
+    {/* <PerfectScrollbar style={ {width: '100%', height: '35px'} } options={{suppressScrollY: true}} containerRef={(el) => tabContainer.current = el}> */}
+    <Scroll ref={(el) => el ? tabContainer.current = el.ref : null }>
     <div className={styles.kt_editor_tabs_content}>
     {resources.map((resource) => {
       let ref: HTMLDivElement | null;
@@ -25,6 +59,14 @@ export const Tabs = observer(({resources, currentResource, onActivate, onClose, 
                     [styles.kt_editor_tab]: true,
                     [styles.kt_editor_tab_current]: currentResource === resource,
                   })}
+                  ref={(el) => {
+                    if (el) {
+                      ref = el;
+                      if ((currentResource === resource)) {
+                        currentTabRef.current = el;
+                      }
+                    }
+                  }}
                   key={resource.uri.toString()}
                   onClick={(e) => onActivate(resource)}
                   onDragOver={(e) => {
@@ -50,8 +92,7 @@ export const Tabs = observer(({resources, currentResource, onActivate, onClose, 
                     if (onDragStart) {
                       onDragStart(e, resource);
                     }
-                  }}
-                  ref={(el) => ref = el} >
+                  }}>
         <span className={resource.icon}> </span>
         <span>{resource.name}</span>
         <span className={styles.close_tab} onClick={(e) => {
@@ -63,5 +104,48 @@ export const Tabs = observer(({resources, currentResource, onActivate, onClose, 
       </div>;
     })}
   </div>
-  </PerfectScrollbar></div>;
+  </Scroll></div>;
 });
+
+/**
+   * 获取tab DOM在可视范围的位置
+   * @param {HTMLElement} container
+   * @param {HTMLElement} el
+   * @returns {number} -1左边或骑跨，0可见，1右边
+   */
+function getTabDOMPosition(container: HTMLElement , el: HTMLElement): number {
+  const left = container.scrollLeft;
+  const right = left + container.offsetWidth;
+  const elLeft = el.offsetLeft;
+  const elRight = el.offsetWidth + elLeft;
+  if (el.offsetWidth > container.offsetWidth) {
+    return -1;
+  }
+  if (left <= elLeft) {
+    if (right >= elRight) {
+      return 0;
+    } else {
+      return 1;
+    }
+  } else {
+    return -1;
+  }
+}
+
+function scrollToTabEl(container: HTMLElement, el: HTMLElement) {
+  const position = getTabDOMPosition(container, el);
+  if (position < 0) {
+      container.scrollLeft = el.offsetLeft;
+    } else if (position > 0) {
+      container.scrollLeft = el.offsetLeft + el.offsetWidth - container.offsetWidth;
+    }
+}
+
+function preventNavigation(this: HTMLDivElement, e: WheelEvent) {
+  console.log(e);
+  if (this.offsetWidth + this.scrollLeft + e.deltaX > this.scrollWidth) {
+    e.preventDefault();
+  } else if (this.scrollLeft + e.deltaX < 0) {
+    e.preventDefault();
+  }
+}
