2aded4e02902879c4c6871e0aaef88cccd658282
ensorrow <zheyanglv@qq.com>
2019-06-14 15:16:15 +0800

    chore: import status bar into editor

--- packages/editor/src/browser/editor.contribution.ts
+++ packages/editor/src/browser/editor.contribution.ts
@@ -1,20 +1,29 @@
 import { CommandContribution, CommandRegistry, URI, Domain, MenuContribution, MenuModelRegistry, localize } from '@ali/ide-core-common';
 import { Injectable, Autowired, INJECTOR_TOKEN, Injector } from '@ali/common-di';
-import { WorkbenchEditorService, IResource } from '../common';
+import { WorkbenchEditorService, IResource, IEditor } from '../common';
 import { EDITOR_BROWSER_COMMANDS } from '../common/commands';
 import { BrowserCodeEditor } from './editor-collection.service';
 import { WorkbenchEditorServiceImpl, EditorGroupSplitAction } from './workbench-editor.service';
 import { ClientAppContribution } from '@ali/ide-core-browser';
 import { MonacoService, ServiceNames } from '@ali/ide-monaco';
+import { StatusBar, StatusBarAlignment } from '@ali/ide-status-bar/lib/browser/status-bar.service';
+import { MonacoLanguages } from '@ali/ide-language/lib/browser/services/monaco-languages';
+import { Languages } from '@ali/ide-language/lib/browser/language-client-services';
 
 @Injectable()
 @Domain(CommandContribution, MenuContribution, ClientAppContribution)
-export class EditorContribution implements CommandContribution, MenuContribution, ClientAppContribution  {
+export class EditorContribution implements CommandContribution, MenuContribution, ClientAppContribution {
   @Autowired(INJECTOR_TOKEN)
   injector: Injector;
 
   @Autowired()
   monacoService: MonacoService;
+
+  @Autowired(StatusBar)
+  statusBar: StatusBar;
+
+  @Autowired(MonacoLanguages)
+  languages: Languages;
 
   @Autowired(WorkbenchEditorService)
   private workbenchEditorService: WorkbenchEditorServiceImpl;
@@ -40,104 +49,120 @@ export class EditorContribution implements CommandContribution, MenuContribution
     });
   }
 
+  // TODO 更新 Language 状态
+  protected updateLanguageStatus(editor: IEditor | undefined): void {
+    if (!editor) {
+      this.statusBar.removeElement('editor-status-language');
+      return;
+    }
+    const language = this.languages.getLanguage && this.languages.getLanguage(editor.getId());
+    const languageName = language ? language.name : '';
+    this.statusBar.setElement('editor-status-language', {
+      text: languageName,
+      alignment: StatusBarAlignment.RIGHT,
+      priority: 1,
+      command: 'EditorCommands.CHANGE_LANGUAGE',
+    });
+  }
+
   registerCommands(commands: CommandRegistry): void {
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.openResource,
     }, {
-      execute: (uri: URI) => {
-        this.workbenchEditorService.open(uri);
-      },
-    });
+        execute: (uri: URI) => {
+          this.workbenchEditorService.open(uri);
+        },
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.saveCurrent,
     }, {
-      execute: async () => {
-        const editor = this.workbenchEditorService.currentEditor as BrowserCodeEditor;
-        if (editor) {
-          await editor.save(editor.currentDocumentModel.uri);
-        }
-      },
-    });
+        execute: async () => {
+          const editor = this.workbenchEditorService.currentEditor as BrowserCodeEditor;
+          if (editor) {
+            await editor.save(editor.currentDocumentModel.uri);
+          }
+        },
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.getCurrent,
     }, {
-      execute: () => this.workbenchEditorService.currentEditorGroup,
-    });
+        execute: () => this.workbenchEditorService.currentEditorGroup,
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.splitToLeft,
       label: localize('editor.splitToLeft'),
     }, {
-      execute: () => {
-        if (this.workbenchEditorService.currentEditorGroup.currentResource) {
-          this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Left, this.workbenchEditorService.currentEditorGroup.currentResource!);
-        }
-      },
-    });
+        execute: () => {
+          if (this.workbenchEditorService.currentEditorGroup.currentResource) {
+            this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Left, this.workbenchEditorService.currentEditorGroup.currentResource!);
+          }
+        },
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.splitToRight,
       label: localize('editor.splitToRight'),
     }, {
-      execute: () => {
-        if (this.workbenchEditorService.currentEditorGroup.currentResource) {
-          this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Right, this.workbenchEditorService.currentEditorGroup.currentResource!);
-        }
-      },
-    });
+        execute: () => {
+          if (this.workbenchEditorService.currentEditorGroup.currentResource) {
+            this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Right, this.workbenchEditorService.currentEditorGroup.currentResource!);
+          }
+        },
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.splitToLeft,
       label: localize('editor.splitToLeft'),
     }, {
-      execute: () => {
-        if (this.workbenchEditorService.currentEditorGroup.currentResource) {
-          this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Left, this.workbenchEditorService.currentEditorGroup.currentResource!);
-        }
-      },
-    });
+        execute: () => {
+          if (this.workbenchEditorService.currentEditorGroup.currentResource) {
+            this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Left, this.workbenchEditorService.currentEditorGroup.currentResource!);
+          }
+        },
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.splitToRight,
       label: localize('editor.splitToRight'),
     }, {
-      execute: () => {
-        if (this.workbenchEditorService.currentEditorGroup.currentResource) {
-          this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Right, this.workbenchEditorService.currentEditorGroup.currentResource!);
-        }
-      },
-    });
+        execute: () => {
+          if (this.workbenchEditorService.currentEditorGroup.currentResource) {
+            this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Right, this.workbenchEditorService.currentEditorGroup.currentResource!);
+          }
+        },
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.splitToTop,
       label: localize('editor.splitToTop'),
     }, {
-      execute: () => {
-        if (this.workbenchEditorService.currentEditorGroup.currentResource) {
-          this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Top, this.workbenchEditorService.currentEditorGroup.currentResource!);
-        }
-      },
-    });
+        execute: () => {
+          if (this.workbenchEditorService.currentEditorGroup.currentResource) {
+            this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Top, this.workbenchEditorService.currentEditorGroup.currentResource!);
+          }
+        },
+      });
 
     commands.registerCommand({
       id: EDITOR_BROWSER_COMMANDS.splitToBottom,
       label: localize('editor.splitToBottom'),
     }, {
-      execute: () => {
-        if (this.workbenchEditorService.currentEditorGroup.currentResource) {
-          this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Bottom, this.workbenchEditorService.currentEditorGroup.currentResource!);
-        }
-      },
-    });
+        execute: () => {
+          if (this.workbenchEditorService.currentEditorGroup.currentResource) {
+            this.workbenchEditorService.currentEditorGroup.split(EditorGroupSplitAction.Bottom, this.workbenchEditorService.currentEditorGroup.currentResource!);
+          }
+        },
+      });
 
   }
 
   registerMenus(menus: MenuModelRegistry) {
     menus.registerMenuAction(['editor', 'split-to-left'], {
-        commandId: EDITOR_BROWSER_COMMANDS.splitToLeft,
+      commandId: EDITOR_BROWSER_COMMANDS.splitToLeft,
     });
     menus.registerMenuAction(['editor', 'split-to-right'], {
       commandId: EDITOR_BROWSER_COMMANDS.splitToRight,
