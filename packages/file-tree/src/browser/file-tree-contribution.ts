f03731eb3f1c1878a41ab4765aeebeb44d3ca17d
erha19 <danwu.wdw@alibaba-inc.com>
2019-06-20 14:42:21 +0800

    chore: 修改右键菜单label

--- packages/file-tree/src/browser/file-tree-contribution.ts
+++ packages/file-tree/src/browser/file-tree-contribution.ts
@@ -1,14 +1,11 @@
 import { Autowired } from '@ali/common-di';
-import { CommandContribution, CommandRegistry, Command } from '@ali/ide-core-common';
-import { KeybindingContribution, KeybindingRegistry, Logger, ClientAppContribution } from '@ali/ide-core-browser';
+import { CommandContribution, CommandRegistry, Command, localize, URI, KeybindingContribution, KeybindingRegistry, Logger, ClientAppContribution } from '@ali/ide-core-browser';
 import { Domain } from '@ali/ide-core-common/lib/di-helper';
 import { MenuContribution, MenuModelRegistry } from '@ali/ide-core-common/lib/menu';
 import { ActivatorBarService } from '@ali/ide-activator-bar/lib/browser/activator-bar.service';
-import { FileTree } from './file-tree.view';
 import { EDITOR_BROWSER_COMMANDS } from '@ali/ide-editor';
 import { CONTEXT_SINGLE_MENU, CONTEXT_MULTI_MENU, CONTEXT_FOLDER_MENU } from './file-tree.view';
 import FileTreeService from './file-tree.service';
-import { URI } from '@ali/ide-core-common';
 import { FileTreeKeybindingContexts } from './file-tree-keybinding-contexts';
 
 export const FILETREE_BROWSER_COMMANDS: {
@@ -111,55 +108,55 @@ export class FileTreeContribution implements CommandContribution, KeybindingCont
 
     // 单选菜单
     menus.registerMenuAction(FileTreeContextSingleMenu.OPEN, {
-      label: '新建文件',
+      label: localize('filetree.newfile', '新建文件'),
       commandId: FILETREE_BROWSER_COMMANDS.NEW_FILE.id,
     });
     menus.registerMenuAction(FileTreeContextSingleMenu.OPEN, {
-      label: '新建文件夹',
+      label: localize('filetree.newfolder', '新建文件夹'),
       commandId: FILETREE_BROWSER_COMMANDS.NEW_FOLDER.id,
     });
     menus.registerMenuAction(FileTreeContextSingleMenu.OPEN, {
-      label: '打开文件',
+      label: localize('filetree.openfile', '打开文件'),
       commandId: EDITOR_BROWSER_COMMANDS.openResources,
     });
     menus.registerMenuAction(FileTreeContextSingleMenu.OPERATOR, {
-      label: '删除文件',
+      label: localize('filetree.deletefile', '删除文件'),
       commandId: FILETREE_BROWSER_COMMANDS.DELETE_FILE.id,
     });
     menus.registerMenuAction(FileTreeContextSingleMenu.OPERATOR, {
-      label: '重命名',
+      label: localize('filetree.renamefile', '重命名'),
       commandId:  FILETREE_BROWSER_COMMANDS.RENAME_FILE.id,
     });
 
     // 多选菜单，移除部分选项
     menus.registerMenuAction(FileTreeContextMutiMenu.OPEN, {
-      label: '新建文件',
+      label: localize('filetree.newfile', '新建文件'),
       commandId: FILETREE_BROWSER_COMMANDS.NEW_FILE.id,
     });
     menus.registerMenuAction(FileTreeContextMutiMenu.OPEN, {
-      label: '新建文件夹',
+      label: localize('filetree.newfolder', '新建文件夹'),
       commandId: FILETREE_BROWSER_COMMANDS.NEW_FOLDER.id,
     });
     menus.registerMenuAction(FileTreeContextMutiMenu.OPERATOR, {
-      label: '删除文件',
+      label: localize('filetree.deletefile', '删除文件'),
       commandId: FILETREE_BROWSER_COMMANDS.DELETE_FILE.id,
     });
 
     // 文件夹菜单
     menus.registerMenuAction(FileTreeContextFolderMenu.OPEN, {
-      label: '新建文件',
+      label: localize('filetree.newfile', '新建文件'),
       commandId: FILETREE_BROWSER_COMMANDS.NEW_FILE.id,
     });
     menus.registerMenuAction(FileTreeContextFolderMenu.OPEN, {
-      label: '新建文件夹',
+      label: localize('filetree.newfolder', '新建文件夹'),
       commandId: FILETREE_BROWSER_COMMANDS.NEW_FOLDER.id,
     });
     menus.registerMenuAction(FileTreeContextFolderMenu.OPERATOR, {
-      label: '删除文件',
+      label: localize('filetree.deletefolder', '删除文件夹'),
       commandId: FILETREE_BROWSER_COMMANDS.DELETE_FILE.id,
     });
     menus.registerMenuAction(FileTreeContextFolderMenu.OPERATOR, {
-      label: '重命名',
+      label: localize('filetree.rename', '重命名'),
       commandId:  FILETREE_BROWSER_COMMANDS.RENAME_FILE.id,
     });
   }
