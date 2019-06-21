42b3480d870c1f70340674492ae9a92ae94726b4
ensorrow <zheyanglv@qq.com>
2019-06-21 14:38:51 +0800

    fix: worker cross domain

--- packages/monaco/src/browser/monaco-loader.ts
+++ packages/monaco/src/browser/monaco-loader.ts
@@ -35,6 +35,18 @@ export function loadVsRequire(context: any): Promise<any> {
 }
 
 export function loadMonaco(vsRequire: any): Promise<void> {
+    const global = window as any;
+    // https://github.com/Microsoft/monaco-editor/blob/master/docs/integrate-amd-cross.md
+    global.MonacoEnvironment = {
+        getWorkerUrl() {
+            return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+            self.MonacoEnvironment = {
+              baseUrl: 'https://g.alicdn.com/tb-theia-app/theia-assets/0.0.8/'
+            };
+            importScripts('https://g.alicdn.com/tb-theia-app/theia-assets/0.0.8/vs/base/worker/workerMain.js');`,
+            )}`;
+        },
+    };
     // NOTE 直接加载 editor.main 时不会 load 其他service
     return new Promise<void>((resolve) => {
         vsRequire(['vs/editor/editor.main'], () => {
@@ -49,7 +61,6 @@ export function loadMonaco(vsRequire: any): Promise<void> {
                 'vs/base/common/filters',
             ], (standaloneServices: any, codeEditorService: any, codeEditorServiceImpl: any, contextViewService: any,
                 quickOpen: any, quickOpenWidget: any, quickOpenModel: any, filters: any ) => {
-                const global = window as any;
 
                 global.monaco.services = Object.assign({}, standaloneServices, codeEditorService, codeEditorServiceImpl, contextViewService);
                 global.monaco.quickOpen = Object.assign({}, quickOpen, quickOpenWidget, quickOpenModel);
