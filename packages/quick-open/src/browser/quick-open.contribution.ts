cf693b2d0b44b82ecd83efe34226219fe5f02247
hacke2 <xinglong.wangwxl@alibaba-inc.com>
2019-06-25 17:15:17 +0800

    feat: 新增 quick-pick 组件，廉价使用 quick-open

--- packages/quick-open/src/browser/quick-open.contribution.ts
+++ packages/quick-open/src/browser/quick-open.contribution.ts
@@ -2,6 +2,7 @@ import { Autowired } from '@ali/common-di';
 import { Domain, CommandContribution, ContributionProvider, IClientApp } from '@ali/ide-core-browser';
 import { ClientAppContribution } from '@ali/ide-core-browser';
 import { MonacoService } from '@ali/ide-monaco';
+import { QuickOpenService, QuickPickService } from './quick-open.model';
 
 @Domain(ClientAppContribution)
 export class QuickOpenClientContribution implements ClientAppContribution {
@@ -16,12 +17,16 @@ export class QuickOpenClientContribution implements ClientAppContribution {
     await this.monacoService.loadMonaco();
     // 加载依赖 monaco 的其他组件
     const { QuickOpenCommandContribution } = require('./quick-open.command.contributuin');
-    const { QuickOpenService, MonacoQuickOpenService } = require('./quick-open.service');
+    const { MonacoQuickOpenService } = require('./quick-open.service');
+    const { QuickPickServiceImpl } = require('./quick-pick.service');
 
     this.commandContributionProvider.addContribution(QuickOpenCommandContribution);
     app.injector.addProviders({
       token: QuickOpenService,
       useClass: MonacoQuickOpenService,
+    }, {
+      token: QuickPickService,
+      useClass: QuickPickServiceImpl,
     });
   }
 }
