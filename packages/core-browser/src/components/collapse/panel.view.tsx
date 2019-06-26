57f5e6f375e8d8393264b9c8977db3fcc75922b4
erha19 <danwu.wdw@alibaba-inc.com>
2019-06-26 16:10:23 +0800

    feat: 添加手风琴组件

---
+++ packages/core-browser/src/components/collapse/panel.view.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import * as cls from 'classnames';
+import * as styles from './panel.module.less';
+
+export interface CollapsePanelProps extends React.PropsWithChildren<any> {
+  // panel 头部标题
+  header: string;
+  // key 用于识别唯一panel的id
+  key: string;
+  // key 作为map时的key，同时需要panel用的key
+  panelKey?: string;
+  // 头部样式名
+  headerClass?: string;
+  // panel 点击事件监听
+  onItemClick?: any;
+  // 计算宽度时的优先级
+  priority?: number;
+  // panel宽高
+  size?: {
+    width: string | number;
+    height: string | number;
+  };
+}
+
+export const CollapsePanel = (
+  {
+    header,
+    headerClass,
+    onItemClick,
+    panelKey,
+    children,
+    isActive,
+    onResize,
+    size,
+  }: CollapsePanelProps,
+) => {
+
+  const clickHandler = (event) => {
+    onItemClick(event, panelKey);
+  };
+
+  const attrs = {
+    tabIndex: 0,
+  };
+
+  React.useEffect(() => {
+    if (onResize) {
+      onResize(size);
+    }
+  }, [size]);
+
+  // 往子元素添加布局属性
+  const getNewChild = (child, index) => {
+    if (!child) { return null; }
+    const key: string = child.key || String(index);
+    const props = {
+      key,
+      ...size,
+    };
+
+    return React.cloneElement(child, props);
+  };
+
+  const getItems = (children: React.ReactChild) => {
+    const newChildren = React.Children.map(children, getNewChild);
+    return newChildren;
+  };
+
+  return  <div className={ styles.kt_split_overlay } style={ size }  >
+    <div className={ styles.kt_split_panel }>
+      <div {...attrs} className={ cls(isActive ? '' : styles.kt_mod_collapsed, styles.kt_split_panel_header, headerClass)} onClick={clickHandler}>{header}</div>
+      <div className={ styles.kt_split_panel_body }>
+         { getItems(children) }
+      </div>
+    </div>;
+  </div > ;
+};
