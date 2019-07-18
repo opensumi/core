01be1370adf88ad256db0b56fa5da3e43cd0c674
lichao.lc <lichao.lc@alibaba-inc.com>
2019-07-18 17:41:42 +0800

    fix: fix jest error

--- packages/startup/__tests__/node/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { StartupModule } from '../../src/node';
-
-describe('template test', () => {
-  it('StartupModule', () => {
-    const cls = new StartupModule();
-    expect(cls.providers).toEqual([]);
-  });
-});
