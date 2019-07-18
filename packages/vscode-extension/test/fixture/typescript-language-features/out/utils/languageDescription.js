c13a8a3e2d662a58bab943d4ea9547028a964c42
frankLife <shangpo.zw@alibaba-inc.com>
2019-07-18 22:04:21 +0800

    chore: 插件代码

---
+++ packages/vscode-extension/test/fixture/typescript-language-features/out/utils/languageDescription.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+const languageModeIds = require("./languageModeIds");
+exports.allDiagnosticLangauges = [0 /* JavaScript */, 1 /* TypeScript */];
+exports.standardLanguageDescriptions = [
+    {
+        id: 'typescript',
+        diagnosticOwner: 'typescript',
+        diagnosticSource: 'ts',
+        diagnosticLanguage: 1 /* TypeScript */,
+        modeIds: [languageModeIds.typescript, languageModeIds.typescriptreact],
+        configFile: 'tsconfig.json'
+    }, {
+        id: 'javascript',
+        diagnosticOwner: 'typescript',
+        diagnosticSource: 'ts',
+        diagnosticLanguage: 0 /* JavaScript */,
+        modeIds: [languageModeIds.javascript, languageModeIds.javascriptreact],
+        configFile: 'jsconfig.json'
+    }
+];
