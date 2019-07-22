00af94624e0d2d8426db02b2d72b8bc9fd0dd214
erha19 <danwu.wdw@alibaba-inc.com>
2019-07-22 20:59:35 +0800

    chore: 移动实现了的workspace API

--- packages/vscode-extension/typings-pending/vscode.filesystem.d.ts
+++ packages/vscode-extension/typings-pending/vscode.filesystem.d.ts
@@ -31,60 +31,10 @@ declare module 'vscode' {
     export function registerFileSystemProvider(scheme: string, provider: FileSystemProvider, options?: { isCaseSensitive?: boolean, isReadonly?: boolean }): Disposable;
 
     /**
-     * ~~The folder that is open in the editor. `undefined` when no folder
-     * has been opened.~~
-     *
-     * @deprecated Use [`workspaceFolders`](#workspace.workspaceFolders) instead.
-     */
-    export const rootPath: string | undefined;
-
-    /**
-     * List of workspace folders or `undefined` when no folder is open.
-     * *Note* that the first entry corresponds to the value of `rootPath`.
-     */
-    export const workspaceFolders: WorkspaceFolder[] | undefined;
-
-    /**
      * The name of the workspace. `undefined` when no folder
      * has been opened.
      */
     export const name: string | undefined;
-
-    /**
-     * The location of the workspace file, for example:
-     *
-     * `file:///Users/name/Development/myProject.code-workspace`
-     *
-     * or
-     *
-     * `untitled:1555503116870`
-     *
-     * for a workspace that is untitled and not yet saved.
-     *
-     * Depending on the workspace that is opened, the value will be:
-     *  * `undefined` when no workspace or  a single folder is opened
-     *  * the path of the workspace file as `Uri` otherwise. if the workspace
-     * is untitled, the returned URI will use the `untitled:` scheme
-     *
-     * The location can e.g. be used with the `vscode.openFolder` command to
-     * open the workspace again after it has been closed.
-     *
-     * **Example:**
-     * ```typescript
-     * vscode.commands.executeCommand('vscode.openFolder', uriOfWorkspace);
-     * ```
-     *
-     * **Note:** it is not advised to use `workspace.workspaceFile` to write
-     * configuration data into the file. You can use `workspace.getConfiguration().update()`
-     * for that purpose which will work both when a single folder is opened as
-     * well as an untitled or saved workspace.
-     */
-    export const workspaceFile: Uri | undefined;
-
-    /**
-     * An event that is emitted when a workspace folder is added or removed.
-     */
-    export const onDidChangeWorkspaceFolders: Event<WorkspaceFoldersChangeEvent>;
 
     /**
      * Returns a path that is relative to the workspace folder or folders.
