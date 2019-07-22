a0881817642791e458bca69525b9d66d723833a8
GGICE <i@ice.gs>
2019-07-22 10:32:19 +0800

    feat: mv extensions types

--- packages/vscode-extension/typings-pending/vscode.d.ts
+++ packages/vscode-extension/typings-pending/vscode.d.ts
@@ -665,7 +665,7 @@ declare module 'vscode' {
 	/**
 	 * A cancellation source creates and controls a [cancellation token](#CancellationToken).
 	 * @墨蛰
-	 * 
+	 *
 	 */
 	export class CancellationTokenSource {
 
@@ -2993,56 +2993,6 @@ declare module 'vscode' {
 		 * Extension runs where the remote extension host runs.
 		 */
 		Workspace = 2,
-	}
-
-	/**
-	 * Represents an extension.
-	 *
-	 * To get an instance of an `Extension` use [getExtension](#extensions.getExtension).
-	 */
-	export interface Extension<T> {
-
-		/**
-		 * The canonical extension identifier in the form of: `publisher.name`.
-		 */
-		readonly id: string;
-
-		/**
-		 * The absolute file path of the directory containing this extension.
-		 */
-		readonly extensionPath: string;
-
-		/**
-		 * `true` if the extension has been activated.
-		 */
-		readonly isActive: boolean;
-
-		/**
-		 * The parsed contents of the extension's package.json.
-		 */
-		readonly packageJSON: any;
-
-		/**
-		 * The extension kind describes if an extension runs where the UI runs
-		 * or if an extension runs where the remote extension host runs. The extension kind
-		 * if defined in the `package.json` file of extensions but can also be refined
-		 * via the the `remote.extensionKind`-setting. When no remote extension host exists,
-		 * the value is [`ExtensionKind.UI`](#ExtensionKind.UI).
-		 */
-		extensionKind: ExtensionKind;
-
-		/**
-		 * The public API exported by this extension. It is an invalid action
-		 * to access this field before this extension has been activated.
-		 */
-		readonly exports: T;
-
-		/**
-		 * Activates this extension and returns its public API.
-		 *
-		 * @return A promise that will resolve when this extension has been activated.
-		 */
-		activate(): Thenable<T>;
 	}
 
 	/**
