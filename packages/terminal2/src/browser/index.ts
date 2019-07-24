1450aa988bd6e32888ded3179b2eceaf489f5e03
frankLife <shangpo.zw@alibaba-inc.com>
2019-07-24 23:34:26 +0800

    chore: 初始化结构

--- packages/terminal2/src/browser/index.ts
+++ packages/terminal2/src/browser/index.ts
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { Provider, Injectable } from '@ali/common-di';
 import { BrowserModule } from '@ali/ide-core-browser';
-import { HelloWorld } from './hello-world.view';
 
 @Injectable()
 export class Terminal2Module extends BrowserModule {
   providers: Provider[] = [];
 
-  component = HelloWorld;
 }
