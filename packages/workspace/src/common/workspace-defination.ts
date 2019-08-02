5d27392b071435151d0bf7802f9c8da85eb87956
hacke2 <xinglong.wangwxl@alibaba-inc.com>
2019-08-02 12:18:53 +0800

    feat: 新增 workspaceContains activationEvents

--- packages/workspace/src/common/workspace-defination.ts
+++ packages/workspace/src/common/workspace-defination.ts
@@ -1,4 +1,5 @@
 import { URI } from '@ali/ide-core-common';
+import { FileStat } from '@ali/ide-file-service';
 
 export const KAITIAN_MUTI_WORKSPACE_EXT = 'kaitian-workspace';
 export const VSCODE_MUTI_WORKSPACE_EXT = 'code-workspace';
@@ -7,4 +8,10 @@ export const WORKSPACE_RECENT_DATA_FILE = 'recentdata.json';
 
 export function getTemporaryWorkspaceFileUri(home: URI): URI {
     return home.resolve(WORKSPACE_USER_STORAGE_FOLDER_NAME).resolve(`Untitled.${KAITIAN_MUTI_WORKSPACE_EXT}`).withScheme('file');
+}
+
+export const IWorkspaceService = Symbol('IWorkspaceService');
+export interface IWorkspaceService {
+  containsSome(paths: string[]): Promise<boolean>;
+  tryGetRoots(): FileStat[];
 }
