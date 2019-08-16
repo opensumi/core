665cac93438b523b860c960526d2ade68dc6ddfb
lichao.lc <lichao.lc@alibaba-inc.com>
2019-08-16 23:59:10 +0800

    feat: prefrencelist读取

---
+++ packages/preferences/src/browser/preference.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, Autowired } from '@ali/common-di';
+import { WorkspaceFilePreferenceProvider } from './workspace-file-preference-provider';
+import { observable } from 'mobx';
+import { FileServiceClient } from '@ali/ide-file-service/lib/browser/file-service-client';
+import { preferenceScopeProviderTokenMap, PreferenceScope, PreferenceProvider } from '@ali/ide-core-browser';
+
+@Injectable()
+export class PreferenceService {
+
+  @Autowired(preferenceScopeProviderTokenMap[PreferenceScope.Folder])
+  private folderFilePreference: PreferenceProvider;
+
+  @Autowired(preferenceScopeProviderTokenMap[PreferenceScope.User])
+  userFilePreference: PreferenceProvider;
+
+  @Autowired(FileServiceClient)
+  private fileServiceClient: FileServiceClient;
+
+  @observable
+  public preferenceList = async () => {
+    const uri = this.userFilePreference.getConfigUri();
+    const filePath = uri && uri.toString();
+
+    if (filePath && await this.fileServiceClient.exists(filePath)) {
+      const fileContent = await this.fileServiceClient.resolveContent(filePath);
+    } else {
+
+    }
+  }
+
+}
--- packages/preferences/src/browser/preferences.view.tsx
+++ packages/preferences/src/browser/preferences.view.tsx
@@ -2,9 +2,26 @@ import * as React from 'react';
 import { observer } from 'mobx-react-lite';
 import * as styles from './index.module.less';
 import { ReactEditorComponent } from '@ali/ide-editor/lib/browser';
+import { useInjectable } from '@ali/ide-core-browser';
+import { PreferenceService } from './preference.service';
 
 export const PreferenceView: ReactEditorComponent<null> = (props) => {
+
+  const preferenceService: PreferenceService  = useInjectable(PreferenceService);
+
+  const list = preferenceService.preferenceList();
+  const renderPreferenceList = () => {
+    const results: React.ReactNode[] = [];
+
+    for (const key of Object.keys(list)) {
+      results.push(<div>{key}:{list[key]}</div>);
+    }
+    return results;
+  };
+
   return (
-    <h1 className={ styles.name }>preference</h1>
+    <div>
+      {renderPreferenceList()}
+    </div>
   );
 };
