c0c0d2b9cf06be83c5b84618638e8b70731b9af6
frankLife <shangpo.zw@alibaba-inc.com>
2019-08-19 09:33:20 +0800

    chore: 插件激活逻辑迁移, workspaces activation events(未调试)

--- packages/kaitian-extension/src/browser/vscode/meta/index.ts
+++ packages/kaitian-extension/src/browser/vscode/meta/index.ts
@@ -3,6 +3,8 @@ import { VSCodeContributeRunner } from './contributes';
 import { IExtension } from '../../../common';
 import { Disposable } from '@ali/ide-core-browser';
 import { ActivationEventService } from '@ali/ide-activation-event';
+import { IWorkspaceService } from '@ali/ide-workspace';
+import { FileSearchServicePath, IFileSearchService } from '@ali/ide-search/lib/common';
 
 @Injectable({multiple: true})
 export class VSCodeMetaService extends Disposable {
@@ -13,10 +15,17 @@ export class VSCodeMetaService extends Disposable {
   @Autowired()
   private activationService: ActivationEventService;
 
+  @Autowired(IWorkspaceService)
+  private workspaceService: IWorkspaceService;
+
+  @Autowired(FileSearchServicePath)
+  private fileSearchService: IFileSearchService;
+
   public async run(extension: IExtension) {
     const runner = this.injector.get(VSCodeContributeRunner, [extension]);
     await runner.run();
     await this.registerActivationEvent(extension);
+    await this.activateByWorkspaceContains(extension);
   }
 
   private registerActivationEvent(extension: IExtension) {
@@ -29,6 +38,48 @@ export class VSCodeMetaService extends Disposable {
         activateDisposer.dispose();
       });
     });
+  }
+
+  private async activateByWorkspaceContains(extension: IExtension) {
+    const { activationEvents = [] } = extension.packageJSON;
+
+    const paths: string[] = [];
+    const includePatterns: string[] = [];
+    for (const activationEvent of activationEvents) {
+      if (/^workspaceContains:/.test(activationEvent)) {
+        const fileNameOrGlob = activationEvent.substr('workspaceContains:'.length);
+        if (fileNameOrGlob.indexOf('*') >= 0 || fileNameOrGlob.indexOf('?') >= 0) {
+          includePatterns.push(fileNameOrGlob);
+        } else {
+          paths.push(fileNameOrGlob);
+        }
+      }
+    }
+
+    const promises: Promise<boolean>[] = [];
+    if (paths.length) {
+      promises.push(this.workspaceService.containsSome(paths));
+    }
+
+    if (includePatterns.length) {
+      promises.push((async () => {
+        try {
+          const result = await this.fileSearchService.find('', {
+            rootUris: this.workspaceService.tryGetRoots().map((r) => r.uri),
+            includePatterns,
+            limit: 1,
+          });
+          return result.length > 0;
+        } catch (e) {
+          this.logger.error(e);
+          return false;
+        }
+      })());
+    }
+
+    if (promises.length && await Promise.all(promises).then((exists) => exists.some((v) => v))) {
+      this.activationService.fireEvent('workspaceContains', [...paths, ...includePatterns][0]);
+    }
   }
 
 }
