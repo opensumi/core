6f7cf0623679561326748193e4b04f1819bfd776
GGICE <i@ice.gs>
2019-08-20 10:31:40 +0800

    test: fix test without AppConfig

--- packages/logs-core/__tests__/node/log.service.ts
+++ packages/logs-core/__tests__/node/log.service.ts
@@ -1,24 +1,30 @@
-// import * as os from 'os';
-// import * as path from 'path';
-// import { LoggerManage } from '../../src/node/';
-// import { LogLevel, SupportLogNamespace } from '../../src/common/';
+import * as os from 'os';
+import * as path from 'path';
+import { createNodeInjector } from '@ali/ide-dev-tool/src/injector-helper';
+import { AppConfig } from '@ali/ide-core-node';
+import { LogServiceModule } from '../../src/node/';
+import { LogLevel, SupportLogNamespace, ILogServiceManage } from '../../src/common/';
 
-// const { setGlobalLogLevel, getGlobalLogLevel, getLogger, init } = LoggerManage;
+describe('Log level', () => {
+  const injector = createNodeInjector([LogServiceModule]);
+  injector.addProviders({
+    token: AppConfig,
+    useValue: {},
+  });
+  const LoggerManage = injector.get(ILogServiceManage);
+  const { setGlobalLogLevel, getGlobalLogLevel, getLogger, init } = LoggerManage;
+  init({
+    logDir: path.join(os.homedir(), `.kaitian-test/logs`),
+  });
+  test('setLogLevel', () => {
+    setGlobalLogLevel(LogLevel.Error);
 
-// init({
-//   logDir: path.join(os.homedir(), `.kaitian-test/logs`),
-// });
+    const logger = getLogger(SupportLogNamespace.Node);
+    logger.debug('debug!!!');
+    logger.log('info!!!');
+    logger.warn('warn!!!');
+    logger.error('error!!!');
 
-// describe('Log level', () => {
-//   test('setLogLevel', () => {
-//     setGlobalLogLevel(LogLevel.Error);
-
-//     const logger = getLogger(SupportLogNamespace.Node);
-//     logger.debug('debug!!!');
-//     logger.log('info!!!');
-//     logger.warn('warn!!!');
-//     logger.error('error!!!');
-
-//     expect(getGlobalLogLevel()).toBe(LogLevel.Error);
-//   });
-// });
+    expect(getGlobalLogLevel()).toBe(LogLevel.Error);
+  });
+});
