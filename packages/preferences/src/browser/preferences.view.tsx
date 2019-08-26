48c97ec28644c838e9b3b2dee2cebb63dd3ccc90
lichao.lc <lichao.lc@alibaba-inc.com>
2019-08-26 10:29:07 +0800

    feat: 多scope的读取

--- packages/preferences/src/browser/preferences.view.tsx
+++ packages/preferences/src/browser/preferences.view.tsx
@@ -2,68 +2,65 @@ import * as React from 'react';
 import { observer } from 'mobx-react-lite';
 import * as styles from './index.module.less';
 import { ReactEditorComponent } from '@ali/ide-editor/lib/browser';
-import { useInjectable, PreferenceSchemaProvider } from '@ali/ide-core-browser';
+import { useInjectable, PreferenceSchemaProvider, PreferenceDataProperty } from '@ali/ide-core-browser';
 import { PreferenceService } from './preference.service';
 import './index.less';
 
 export const PreferenceView: ReactEditorComponent<null> = observer((props) => {
 
   const preferenceService: PreferenceService  = useInjectable(PreferenceService);
+  const defaultPreferenceProvider: PreferenceSchemaProvider = (preferenceService.defaultPreference as PreferenceSchemaProvider);
   let selectedPreference = preferenceService.userPreference;
+
+  const defaultList = defaultPreferenceProvider.getPreferences();
+  const [list, setList] = React.useState({});
 
   const renderScopeSelect = () => {
     const options: React.ReactNode[] = [];
-    options.push(<option value='default'>Default</option>);
     options.push(<option value='user'>User</option>);
     options.push(<option value='workspace'>WorkSpace</option>);
-    options.push(<option value='folder'>Folder</option>);
 
     return (<select
       value={selectedPreference ? selectedPreference.name : 'user'}
       onChange={async (event) => {
-
         const name = (event.target as HTMLSelectElement).value;
         switch (name) {
-          case 'default':
-            selectedPreference = preferenceService.defaultPreference;
-            const list = (selectedPreference as PreferenceSchemaProvider).getPreferences();
-            setList(list);
-            break;
           case 'user':
             selectedPreference = preferenceService.userPreference;
             preferenceService.getPreferences(preferenceService.userPreference).then((list) => {
-              setList(list);
+              setList(Object.assign({}, defaultList, list));
             });
             break;
           case 'workspace':
             selectedPreference = preferenceService.workspacePreference;
             preferenceService.getPreferences(preferenceService.workspacePreference).then((list) => {
-              setList(list);
-            });
-            break;
-          case 'folder':
-            selectedPreference = preferenceService.folderPreference;
-            preferenceService.getPreferences(preferenceService.folderPreference).then((list) => {
-              setList(list);
+              setList(Object.assign({}, defaultList, list));
             });
             break;
         }
 
     }}>{options}</select>);
   };
+  setTimeout(() => {
+    selectedPreference = preferenceService.userPreference;
+    preferenceService.getPreferences(preferenceService.userPreference).then((list) => {
+      setList(Object.assign({}, defaultList, list));
+    });
+  });
 
-  const [list, setList] = React.useState({});
   const renderPreferenceList = () => {
     const results: React.ReactNode[] = [];
 
     for (const key of Object.keys(list)) {
-      results.push(<div key={key}>{key}:{list[key]}</div>);
+      const prop: PreferenceDataProperty|undefined = defaultPreferenceProvider.getPreferenceProperty(key);
+      results.push(<div key={key}>{key}: {JSON.stringify(list[key])} {prop && prop.description && <span className='desc'>({prop.description})</span>}</div>);
+
     }
     return results;
   };
 
   return (
-    <div>
+    <div className='wrap'>
       <div>
         {renderScopeSelect()}
       </div>
