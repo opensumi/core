5518be15c8bed90899a6d3cad320c1a4a4b35a71
hacke2 <xinglong.wangwxl@alibaba-inc.com>
2019-08-27 16:55:35 +0800

    feat: quickpick 加入 icon 支持

---
+++ packages/core-browser/src/utils/icon.ts
@@ -0,0 +1,14 @@
+
+/**
+ * 获取 icon class name
+ * @param text status-bar 传过来的文字
+ * @returns [icon, text]
+ */
+export function getIconClass(text: string): [string | null, string] {
+  const regExp = /^\$\((.*)\)(.*)/;
+  const result = text.match(regExp);
+  if (!result) {
+    return [null, text];
+  }
+  return [result[1], result[2].trim()];
+}
