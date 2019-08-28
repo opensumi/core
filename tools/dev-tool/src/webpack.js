6c309984de96f67a3cc0134fcb48365841b5a347
vagusxl <vagusxl@gmail.com>
2019-08-28 14:05:20 +0800

    feat: revert  f8d6acb7

--- tools/dev-tool/src/webpack.js
+++ tools/dev-tool/src/webpack.js
@@ -6,13 +6,6 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
-const threadLoader = require('thread-loader');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
-threadLoader.warmup({}, [
-  'ts-loader',
-]);
-
 const utils = require('./utils');
 
 const tsConfigPath = path.join(__dirname, '../../../tsconfig.json');
@@ -132,11 +125,6 @@ exports.createWebpackConfig = function (dir, entry) {
       new CopyPlugin([
         { from: path.join(__dirname, '../vendor'), to: dir + '/dist' },
       ]),
-      new ForkTsCheckerWebpackPlugin({
-        checkSyntacticErrors: true,
-        tsconfig: tsConfigPath,
-        reportFiles: ['packages/**/*.{ts,tsx}']
-      }),
     ],
     devServer: {
       contentBase: dir + '/public',
@@ -187,31 +175,12 @@ exports.createWebviewWebpackConfig = (entry, dir) => {
     module: {
       // https://github.com/webpack/webpack/issues/196#issuecomment-397606728
       exprContextCritical: false,
-      rules: [
-        {
+      rules: [{
           test: /\.tsx?$/,
-          use: [
-            {
-              loader: 'cache-loader',
-              options: {
-                cacheDirectory: path.resolve(__dirname, '../../../.cache'),
-              }
-            },
-            {
-              loader: 'thread-loader',
-              options: {
-                workers: require('os').cpus().length - 1,
-              }
-            },
-            {
-              loader: 'ts-loader',
-              options: {
-                happyPackMode: true,
-                transpileOnly: true,
-                configFile: tsConfigPath,
-              },
-            },
-          ],
+          loader: 'ts-loader',
+          options: {
+            configFile: tsConfigPath,
+          }
         },
       ],
     },
