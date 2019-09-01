a71b47bae29bcadfa90ff93f4af9b7e7619f9d58
vagusxl <vagusxl@gmail.com>
2019-09-01 15:25:03 +0800

    fix: placeholder in scm commit msg input

--- packages/scm/src/browser/scm.view.tsx
+++ packages/scm/src/browser/scm.view.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { observer, useComputed } from 'mobx-react-lite';
 import { useInjectable, IContextKeyService, IContextKey } from '@ali/ide-core-browser';
 import { RecycleTree, TreeNode, TreeViewActionTypes, TreeViewAction } from '@ali/ide-core-browser/lib/components';
-import { URI, CommandService, localize } from '@ali/ide-core-common';
+import { isOSX, URI, CommandService, localize } from '@ali/ide-core-common';
 import * as paths from '@ali/ide-core-common/lib/path';
+import { format } from '@ali/ide-core-common/lib/utils/strings';
 import { ContextMenuRenderer } from '@ali/ide-core-browser/lib/menu';
 import { LabelService } from '@ali/ide-core-browser/lib/services';
 import { ViewState } from '@ali/ide-activity-panel';
@@ -16,6 +17,7 @@ import { ISCMRepository, SCMService, SCMMenuId } from '../common';
 import { ViewModelContext, ResourceGroupSplicer, ISCMDataItem } from './scm.store';
 import { isSCMResource, getSCMResourceContextKey } from './scm-util';
 import * as styles from './scm.module.less';
+import { InputValidationType } from '../../lib';
 
 const itemLineHeight = 22; // copied from vscode
 
@@ -145,18 +147,49 @@ const SCMEmpty = () => {
   );
 };
 
+function convertValidationType(type: InputValidationType) {
+  return ['info', 'warning', 'error'][type];
+}
+
 const SCMHeader: React.FC<{
   repository: ISCMRepository;
 }> = ({ repository }) => {
   const commandService = useInjectable<CommandService>(CommandService);
 
   const [ commitMsg, setCommitMsg ] = React.useState('');
+  const [ placeholder, setPlaceholder] = React.useState('');
+  const inputRef = React.useRef<HTMLTextAreaElement>(null);
 
   const handleChange = React.useCallback((msg: string) => {
-    setCommitMsg(msg);
+    // todo: 校验貌似并不需要
+    // repository.input.validateInput(msg, inputRef.current!.selectionStart || 0)
+    //   .then((result) => {
+    //     if (!result) {
+    //       console.log('validation passed');
+    //     } else {
+    //       console.log('validation failed');
+    //       console.log({ content: result.message, type: result.type });
+    //     }
+    //   });
+
     // 上层只有存在 repository 时才会渲染 Header 部分
     repository.input.value = msg;
   }, [ repository ]);
+
+  React.useEffect(() => {
+    // 单向同步 input value
+    repository.input.onDidChange((value) => {
+      setCommitMsg(value);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    // 单向同步 input placeholder
+    repository.input.onDidChangePlaceholder(() => {
+      const placeholder = format(repository.input.placeholder, isOSX ? '⌘Enter' : 'Ctrl+Enter');
+      setPlaceholder(placeholder);
+    });
+  }, []);
 
   const handleCommit = React.useCallback((keyName, e, handle) => {
     if (!repository || !repository.provider.acceptInputCommand) {
@@ -175,7 +208,7 @@ const SCMHeader: React.FC<{
 
   return (
     <Hotkeys
-      keyName='command+l,ctrl+l'
+      keyName={isOSX ? 'command+enter' : 'ctrl+enter'}
       filter={(event: FilterEvent) => {
         const target = (event.target as HTMLElement) || event.srcElement;
         const tagName = target.tagName;
@@ -184,7 +217,8 @@ const SCMHeader: React.FC<{
       onKeyUp={handleCommit}>
       <div className={styles.scmInput}>
         <TextareaAutosize
-          placeholder={localize('commit msg', 'Message (press ⌘Enter to commit)')}
+          ref={inputRef}
+          placeholder={placeholder}
           autoFocus={true}
           tabIndex={1}
           value={commitMsg}
