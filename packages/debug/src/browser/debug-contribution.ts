7502f5114b240facef21193771e63353d196f57f
erha19 <danwu.wdw@alibaba-inc.com>
2019-09-02 17:23:48 +0800

    feat: 使用视图API注册调试工具栏

--- packages/debug/src/browser/debug-contribution.ts
+++ packages/debug/src/browser/debug-contribution.ts
@@ -1,26 +1,28 @@
 import { Domain } from '@ali/ide-core-browser';
 import { ComponentContribution, ComponentRegistry, Command } from '@ali/ide-core-browser';
-import { TabBarToolbarContribution, TabBarToolbarRegistry } from '@ali/ide-activity-panel/lib/browser/tab-bar-toolbar';
 import { DebugThreadView } from './view/debug-threads.view';
 import { DebugBreakpointView } from './view/debug-breakpoints.view';
 import { DebugStackFrameView } from './view/debug-stack-frames.view';
 import { DebugVariableView } from './view/debug-variable.view';
 import { DebubgConfigurationView } from './view/debug-configuration.view';
+import { MainLayoutContribution, IMainLayoutService } from '@ali/ide-main-layout';
+import { Autowired } from '@ali/common-di';
 
 const DEBUG_SETTING_COMMAND: Command = {
   id: 'debug.setting',
   iconClass: 'volans_icon icon-file_setting',
 };
 
-@Domain(ComponentContribution, TabBarToolbarContribution)
-export class DebugContribution implements ComponentContribution, TabBarToolbarContribution {
+@Domain(ComponentContribution, MainLayoutContribution)
+export class DebugContribution implements ComponentContribution, MainLayoutContribution {
+
+  @Autowired(IMainLayoutService)
+  protected readonly mainlayoutService: IMainLayoutService;
+
+  containerId: string = 'debug';
+
   registerComponent(registry: ComponentRegistry) {
     registry.register('@ali/ide-debug', [
-      {
-        component: DebubgConfigurationView,
-        id: 'debug-toolbar',
-        name: 'TOOLBAR',
-      },
       {
         component: DebugThreadView,
         id: 'debug-thread',
@@ -44,10 +46,12 @@ export class DebugContribution implements ComponentContribution, TabBarToolbarCo
     ], {
       iconClass: 'volans_icon remote_debug',
       title: 'DEBUG',
-      containerId: 'debug',
+      containerId: this.containerId,
     });
   }
 
-  registerToolbarItems(registry: TabBarToolbarRegistry) {
+  onDidUseConfig() {
+    const handler = this.mainlayoutService.getTabbarHandler(this.containerId);
+    handler!.setTitleComponent(DebubgConfigurationView, 85);
   }
 }
