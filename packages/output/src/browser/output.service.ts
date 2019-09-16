ae6e7c3a7dfea6bafa75dea2d9d18e340e86912b
lichao.lc <lichao.lc@alibaba-inc.com>
2019-09-16 16:25:42 +0800

    fix: observable 改成 observable.ref，对selectedChannel属性只需要监听引用的变化

--- packages/output/src/browser/output.service.ts
+++ packages/output/src/browser/output.service.ts
@@ -17,7 +17,7 @@ export class OutputService extends Themable {
   @observable
   protected readonly channels = new Map<string, OutputChannel>();
 
-  @observable
+  @observable.ref
   selectedChannel: OutputChannel;
 
   @observable
