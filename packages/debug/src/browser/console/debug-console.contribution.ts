5dc0965f055ead1487231722a9b078f68c6ac63c
erha19 <danwu.wdw@alibaba-inc.com>
2019-09-24 15:34:36 +0800

    feat: 添加DebugConsole工具栏

--- packages/debug/src/browser/console/debug-console.contribution.ts
+++ packages/debug/src/browser/console/debug-console.contribution.ts
@@ -2,21 +2,33 @@ import { Autowired } from '@ali/common-di';
 import { Domain } from '@ali/ide-core-common/lib/di-helper';
 import { ComponentContribution, ComponentRegistry, Logger } from '@ali/ide-core-browser';
 import { DebugConsoleView } from '../view/debug-console.view';
+import { MainLayoutContribution, IMainLayoutService } from '@ali/ide-main-layout';
+import { DebugConsoleToolbarView } from '../view/debug-console-toolbar.view';
 
-@Domain(ComponentContribution)
-export class DebugConsoleContribution implements ComponentContribution {
+@Domain(ComponentContribution, MainLayoutContribution)
+export class DebugConsoleContribution implements ComponentContribution, MainLayoutContribution {
 
   @Autowired()
   logger: Logger;
 
+  @Autowired(IMainLayoutService)
+  layoutService: IMainLayoutService;
+
   registerComponent(registry: ComponentRegistry) {
     registry.register('debug-console', {
-      id: 'debug-console',
+      id: 'debug-console-view',
       component: DebugConsoleView,
     }, {
       title: 'DEBUG CONSOLE',
       weight: 8,
-      containerId: 'debug-console',
+      containerId: 'debug-console-container',
     });
+  }
+
+  onDidUseConfig() {
+    const handler = this.layoutService.getTabbarHandler('debug-console-container');
+    if (handler) {
+      handler.setTitleComponent(DebugConsoleToolbarView);
+    }
   }
 }
