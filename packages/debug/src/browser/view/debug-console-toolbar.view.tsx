9389780501e6daaa0a6eeaf3dd1957916de720fd
erha19 <danwu.wdw@alibaba-inc.com>
2019-09-26 17:54:17 +0800

    feat: 添加国际化文本

--- packages/debug/src/browser/view/debug-console-toolbar.view.tsx
+++ packages/debug/src/browser/view/debug-console-toolbar.view.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { observer, useObservable } from 'mobx-react-lite';
-import { useInjectable } from '@ali/ide-core-browser';
+import { useInjectable, localize } from '@ali/ide-core-browser';
 import * as styles from './debug-console.module.less';
 
 export const DebugConsoleToolbarView = observer(() => {
 
   return <div className={styles.debug_console_toolbar}>
     <select>
-      <option value='default'>default</option>
+      <option value='default'>{localize('debug.console.panel.default')}</option>
     </select>
   </div>;
 });
