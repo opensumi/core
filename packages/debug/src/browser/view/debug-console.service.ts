154458d2e13d0bc8b9cfc1c3a04895ce49048295
erha19 <danwu.wdw@alibaba-inc.com>
2019-10-14 15:46:54 +0800

    feat: 调试启动时自动展示调试面板

--- packages/debug/src/browser/view/debug-console.service.ts
+++ packages/debug/src/browser/view/debug-console.service.ts
@@ -2,11 +2,16 @@ import { Injectable, Autowired } from '@ali/common-di';
 import { DebugConsoleSession } from '../console/debug-console-session';
 import { observable, action } from 'mobx';
 import { TreeNode } from '@ali/ide-core-browser';
+import { DebugContribution } from '../debug-contribution';
+import { MainLayoutService } from '@ali/ide-main-layout/lib/browser/main-layout.service';
 
 @Injectable()
-export class DebugStackFramesService {
+export class DebugConsoleService {
   @Autowired(DebugConsoleSession)
   protected readonly debugConsole: DebugConsoleSession;
+
+  @Autowired(MainLayoutService)
+  protected readonly mainlayoutService: MainLayoutService;
 
   constructor() {
     this.debugConsole.onDidChange(() => {
@@ -25,6 +30,18 @@ export class DebugStackFramesService {
     node: any;
     [key: string]: any;
   }> = new Map();
+
+  get isVisible() {
+    const bottomPanelHandler = this.mainlayoutService.getTabbarHandler(DebugContribution.DEBUG_CONSOLE_CONTAINER_ID);
+    return bottomPanelHandler.isVisible;
+  }
+
+  activate() {
+    const bottomPanelHandler = this.mainlayoutService.getTabbarHandler(DebugContribution.DEBUG_CONSOLE_CONTAINER_ID);
+    if (bottomPanelHandler && !bottomPanelHandler.isVisible) {
+      bottomPanelHandler.activate();
+    }
+  }
 
   @observable.shallow
   nodes: TreeNode[] = [];
