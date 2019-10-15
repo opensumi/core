e0b339147007a97a58b2ba12a8def44f64306697
GGICE <i@ice.gs>
2019-10-15 11:15:38 +0800

    fix: terminal closed process not kill

--- packages/terminal2/src/node/terminal.service.client.ts
+++ packages/terminal2/src/node/terminal.service.client.ts
@@ -5,7 +5,7 @@ import { RPCService } from '@ali/ide-connection';
 @Injectable()
 export class TerminalServiceClientImpl extends RPCService implements ITerminalServiceClient {
 
-  @Autowired(ITerminalService)
+  @Autowired(ITerminalService, { multiple: true })
   private terminalService: ITerminalService;
   private clientId: string;
 
@@ -14,24 +14,34 @@ export class TerminalServiceClientImpl extends RPCService implements ITerminalSe
 
     this.terminalService.setClient(this.clientId, this);
   }
+
   clientMessage(id, data) {
     if (this.rpcClient) {
       this.rpcClient[0].onMessage(id, data);
     }
   }
+
   create(id: string, rows: number, cols: number, options: TerminalOptions ) {
     return this.terminalService.create(id, rows, cols, options);
   }
+
   onMessage(id: string, msg: string): void {
     this.terminalService.onMessage(id, msg);
   }
+
   resize(id: string, rows: number, cols: number) {
     this.terminalService.resize(id, rows, cols);
   }
+
   disposeById(id: string) {
     this.terminalService.disposeById(id);
   }
+
   getProcessId(id: string): number {
     return this.terminalService.getProcessId(id);
+  }
+
+  dispose() {
+    this.terminalService.dispose();
   }
 }
