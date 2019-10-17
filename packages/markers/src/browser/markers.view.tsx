60f826cb5bbe26b6daba510ececbae112785d693
野松 <songtao.cst@alibaba-inc.com>
2019-10-17 11:59:14 +0800

    feat: 修改触发方式

--- packages/markers/src/browser/markers.view.tsx
+++ packages/markers/src/browser/markers.view.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { observer } from 'mobx-react-lite';
-import { useInjectable, IMarkerService, MarkerSeverity, MarkerModel } from '@ali/ide-core-browser';
+import { useInjectable, IMarkerService, MarkerSeverity, MarkerModel, UniqueMarkerData } from '@ali/ide-core-browser';
 import { MarkerService } from './markers-service';
 import { nls } from '../common/index';
 import * as styles from './markers.module.less';
@@ -25,15 +25,18 @@ const ICONS = {
   },
 };
 
+const MarkerListContext = React.createContext({
+  tag: '',
+  updateTag: (v: string) => {},
+});
+
 /**
  * Marker标题
  * @param uri 资源
  */
 const MarkerItemTitle: React.FC<{ model: MarkerModel, open: boolean, onClick: () => void }> = observer(({ model, open, onClick }) => {
   return (
-    <div className={ styles.itemTitle } onClick={() => {
-      onClick();
-    }}>
+    <div className={ styles.itemTitle } onClick={onClick}>
       <div className={ cls(open ? styles.fold : styles.unfold, ICONS.FOLD) } />
       <div className={ cls(model.icon)} />
       <div className={styles.filename}>{ model.filename }</div>
@@ -47,21 +50,28 @@ const MarkerItemTitle: React.FC<{ model: MarkerModel, open: boolean, onClick: ()
  * Marker详细信息`
  * @param data marker的数据
  */
-const MarkerItemContents: React.FC<{model: MarkerModel, check: string, updateCheck: (v: string) => void}> = observer(({ model, check, updateCheck}) => {
+const MarkerItemContents: React.FC<{model: MarkerModel}> = observer(({ model }) => {
   const markerItemList: React.ReactNode[] = [];
   if (model) {
-    let key = 0;
+    let index = 0;
     model.markers.forEach((marker) => {
-      const checkTag = `${model.uri}-${key}`; // 选中的item的tag
+      index = index + 1;
+      const markerTag = `${model.uri}-${index}`; // 选中的item的tag，TODO最好每个marker有唯一id
       markerItemList.push((
-        <div key={`marker-item-content-${key++}`} className={ cls(styles.itemContent, check === checkTag && styles.checked) } onClick={() => {
-          updateCheck(checkTag);
-        }}>
-          <div className={cls(ICONS.SEVERITY[marker.severity], styles.severity)} />
-          <div className={styles.detail}>{ marker.message }</div>
-          <div className={styles.type}>{ `${marker.source}(${marker.code})` }</div>
-          <div className={styles.position}>{ `[${marker.startColumn},${marker.endColumn}]` }</div>
-        </div>
+        <MarkerListContext.Consumer>
+          {
+            ({tag, updateTag}) => (
+              <div key={`marker-item-content-${index}`} className={ cls(styles.itemContent, tag === markerTag ? styles.checked : '') } onClick={() => {
+                updateTag(markerTag);
+              }}>
+                <div className={cls(ICONS.SEVERITY[marker.severity], styles.severity)} />
+                <div className={styles.detail}>{ marker.message }</div>
+                <div className={styles.type}>{ `${marker.source}(${marker.code})` }</div>
+                <div className={styles.position}>{ `[${marker.startColumn},${marker.endColumn}]` }</div>
+              </div>
+            )
+          }
+        </MarkerListContext.Consumer>
       ));
     });
   }
@@ -70,19 +80,20 @@ const MarkerItemContents: React.FC<{model: MarkerModel, check: string, updateChe
       { markerItemList }
     </div>
   );
+
 });
 
 /**
  * Marker
  */
-const MarkerItem: React.FC<{key: string, model: MarkerModel, check: string, updateCheck: (v: string) => void}> = observer(({key, model, check, updateCheck}) => {
+const MarkerItem: React.FC<{key: string, model: MarkerModel}> = observer(({key, model}) => {
   const [open, setOpen] = React.useState(true);
   return (
     <div key={key} className={styles.markerItem}>
       <MarkerItemTitle model={model} open={open} onClick={() => {
         setOpen(!open);
       }}/>
-      {open && <MarkerItemContents model={model} check={check} updateCheck={updateCheck} />}
+      {open && <MarkerItemContents model={model} />}
     </div>
   );
 });
@@ -91,16 +102,17 @@ const MarkerItem: React.FC<{key: string, model: MarkerModel, check: string, upda
  * 指定类型的Marker列表
  * @param markerMap markers
  */
-const MarkerType: React.FC<{type: string, markers: Map<string, MarkerModel> | undefined, check: string, updateCheck: (v: string) => void}> = observer(({ type, markers, check, updateCheck }) => {
+const MarkerType: React.FC<{key: string, type: string, markers: Map<string, MarkerModel> | undefined}> =
+  observer(({ key, type, markers }) => {
   const result: React.ReactNode[] = [];
   if (markers) {
-    let key = 0;
+    let index = 0;
     markers.forEach((model, _) => {
-      result.push(<MarkerItem key={`marker-group-${key++}`} model={model} check={check} updateCheck={updateCheck}/>);
+      result.push(<MarkerItem key={`marker-group-${index++}`} model={model}/>);
     });
   }
   return (
-    <div>
+    <div key={key} className={styles.markerType}>
       { type }
       { result }
     </div>
@@ -115,20 +127,19 @@ const MarkerType: React.FC<{type: string, markers: Map<string, MarkerModel> | un
 const MarkerList: React.FC<{markers: Map<string, Map<string, MarkerModel> | undefined>}> = observer(({ markers }) => {
   const result: React.ReactNode[] = [];
   if (markers) {
-    let key = 0;
-    const [check, updateCheck] = React.useState(); // TODO 需要换地方
+    let index = 0;
     markers.forEach((collection, type) => {
-      result.push((
-        <div key={`marker-type-${key++}`} className={styles.markerType}>
-          <MarkerType type={type} markers={collection} check={check} updateCheck={updateCheck}/>
-        </div>
-      ));
+      result.push(<MarkerType key={`marker-type-${index++}`} type={type} markers={collection}/>);
     });
   }
+  const [tag, updateTag] = React.useState('');
+  const tagUpdater = {tag, updateTag}; // 防止创建provider.value
   return (
-    <div>
-      { result }
-    </div>
+    <MarkerListContext.Provider value={tagUpdater}>
+      <div className={styles.markerList}>
+        { result }
+      </div>
+    </MarkerListContext.Provider>
   );
 });
 
