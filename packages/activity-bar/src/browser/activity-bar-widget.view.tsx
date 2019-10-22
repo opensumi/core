adf5ac8a1204e154d8d6f6f079984d34008cc885
MMhunter <kengtou.mh@antfin.com>
2019-10-22 17:09:57 +0800

    fix: 修改label为空时报错， 修复electron下可能出现多个menu问题

--- packages/activity-bar/src/browser/activity-bar-widget.view.tsx
+++ packages/activity-bar/src/browser/activity-bar-widget.view.tsx
@@ -44,6 +44,8 @@ export class ActivityBarWidget extends Widget implements ITabbarWidget {
 
   private expanded = false;
 
+  private _toDispose: DisposableCollection | undefined;
+
   constructor(private side: Side, @Optinal(WIDGET_OPTION) options?: Widget.IOptions) {
     super(options);
 
@@ -69,18 +71,20 @@ export class ActivityBarWidget extends Widget implements ITabbarWidget {
   private handleContextMenu(event: MouseEvent) {
     event.preventDefault();
 
-    const toDisposeOnHide = new DisposableCollection();
+    if (this._toDispose) {
+      this._toDispose.dispose();
+    }
+    this._toDispose = new DisposableCollection();
     for (const title of this.tabBar.titles) {
       const sideWrap = title.owner as any;
-      toDisposeOnHide.push(this.menus.registerMenuAction([`${SIDE_MENU_PATH}/${this.side}`, '1_widgets'], {
-        label: title.label.toUpperCase(),
+      this._toDispose.push(this.menus.registerMenuAction([`${SIDE_MENU_PATH}/${this.side}`, '1_widgets'], {
+        label: (title.label || '').toUpperCase(),
         commandId: sideWrap.command,
       }));
     }
     this.contextMenuRenderer.render(
       [`${SIDE_MENU_PATH}/${this.side}`],
       {x: event.clientX, y: event.clientY},
-      () => toDisposeOnHide.dispose(),
     );
   }
 
