c5a406c32cf2199eb498e63c3384edd90ad5c9ea
suyu34 <Joooye1991@gmail.com>
2019-10-26 13:31:10 +0800

    feat: 设计并实现文档缓存的接口和基本用法

---
+++ packages/editor/src/browser/doc-cache/empty-doc-cache.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@ali/common-di';
+import { URI } from '@ali/ide-core-browser';
+import { IDocPersistentCacheProvider, IDocStatus } from '../../common/doc-cache';
+
+/**
+ * 一个空的，什么都不做的缓存对象，提供一个空的缓存对象实现
+ */
+@Injectable()
+export class EmptyDocCacheImpl implements IDocPersistentCacheProvider  {
+  hasCache(_uri: URI) {
+    return false;
+  }
+
+  isFlushed() {
+    return true;
+  }
+
+  getCache(_uri: URI) {
+    return null;
+  }
+
+  persistCache(_uri: URI, _status: IDocStatus) {
+    // nothing
+  }
+}
---
+++ packages/editor/src/browser/doc-cache/local-storage-cache.ts
@@ -0,0 +1,69 @@
+import { Injectable, Autowired } from '@ali/common-di';
+import { URI } from '@ali/ide-core-common';
+import { IDocPersistentCacheProvider, IDocStatus, IDocCache, parseCacheValueFrom } from '../../common/doc-cache';
+import { IWorkspaceStorageService } from '@ali/ide-workspace';
+import { WorkspaceStorageService } from '@ali/ide-workspace/lib/browser/workspace-storage-service';
+
+/**
+ * 使用 LocalStorage 实现的文档缓存对象
+ */
+@Injectable()
+export class LocalStorageDocCacheImpl implements IDocPersistentCacheProvider  {
+  @Autowired(IWorkspaceStorageService)
+  private storageService: WorkspaceStorageService;
+
+  hasCache(_uri: URI) {
+    return true;
+  }
+
+  /**
+   * LocalStorage 的存储都是瞬间完成的，始终返回 true
+   */
+  isFlushed() {
+    return true;
+  }
+
+  /**
+   * 从 LocalStorage 获取缓存数据，
+   * 因为底层对象设计是异步的，所以这里也是异步的，实际上是立即返回的
+   * @param uri
+   */
+  async getCache(uri: URI) {
+    const key = this.parseKeyFrom(uri);
+    const result = await this.storageService.getData<IDocCache>(key);
+    return result || null;
+  }
+
+  /**
+   * 持久化缓存对象到 LocalStorage 中
+   * @param uri
+   * @param status
+   */
+  persistCache(uri: URI, status: IDocStatus) {
+    const key = this.parseKeyFrom(uri);
+    const cache = this.parseCacheFrom(uri, status);
+    this.storageService.setData(key, cache);
+  }
+
+  /**
+   * 从文档状态解析缓存对象
+   * @param status
+   */
+  private parseCacheFrom(uri: URI, status: IDocStatus): IDocCache | undefined {
+    if (!status.dirty || !status.changeMatrix.length) {
+      return undefined;
+    }
+
+    return {
+      path: uri.path.toString(),
+      startMD5: status.startMD5,
+      changeMatrix: status.changeMatrix.map((changes) => {
+        return changes.map((change) => parseCacheValueFrom(change));
+      }),
+    };
+  }
+
+  private parseKeyFrom(uri: URI): string {
+    return `LocalStorageDocCacheImpl_${uri.toString()}`;
+  }
+}
