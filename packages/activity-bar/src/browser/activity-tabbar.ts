bb93fd328f09886bba85127a4f50b950e8190058
ensorrow <zheyanglv@qq.com>
2019-11-01 14:22:55 +0800

    feat: 支持底部panel的badge

--- packages/activity-bar/src/browser/activity-tabbar.ts
+++ packages/activity-bar/src/browser/activity-tabbar.ts
@@ -22,7 +22,7 @@ class SideTabRender extends TabBar.Renderer {
     // @ts-ignore
     if (data.title.badge && data.title.badge !== '0') {
       // @ts-ignore
-      return h.div({ className: 'p-TabBar-tabBadge' }, data.title.badge);
+      return h.div({ className: 'p-TabBar-tabBadge' }, h.div(data.title.badge));
     }
     return h.div({ className: 'p-TabBar-empty-badge' });
   }
