5b32b2f004bb4b01e4e1ae3c31eb1e77087877f7
ensorrow <zheyanglv@qq.com>
2019-11-01 17:55:48 +0800

    fix: reset schema registry change

--- packages/core-browser/src/monaco/index.ts
+++ packages/core-browser/src/monaco/index.ts
@@ -37,14 +37,14 @@ export const Extensions = {
 };
 
 export interface ISchemaContributions {
-  schemas: { [id: string]: string };
+  schemas: { [id: string]: IJSONSchema };
 }
 
 export interface ISchemaRegistry {
 
   readonly onDidChangeSchema: Event<string>;
 
-  registerSchema(uri: string, unresolvedSchemaContent: string, fileMatch: string[]): void;
+  registerSchema(uri: string, unresolvedSchemaContent: IJSONSchema, fileMatch: string[]): void;
 
   notifySchemaChanged(uri: string): void;
 
