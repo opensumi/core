58e56c5c9fb141e44ca889a390c389f055a9a835
erha19 <danwu.wdw@alibaba-inc.com>
2019-11-11 19:10:33 +0800

    fix: 修复测试循环引用问题

--- packages/core-common/src/utils/paths.ts
+++ packages/core-common/src/utils/paths.ts
@@ -22,8 +22,11 @@
 // tslint:disable:no-bitwise variable-name
 'use strict';
 
-import { CharCode, isWindows, startsWithIgnoreCase } from '../';
+import { isWindows } from '../platform';
+import { CharCode } from '../charCode';
 import { posix } from '../path';
+import { startsWithIgnoreCase } from './strings';
+
 /**
  * The forward slash path separator.
  */
@@ -36,15 +39,6 @@ export const nativeSep = isWindows ? '\\' : '/';
 
 const _posixBadPath = /(\/\.\.?\/)|(\/\.\.?)$|^(\.\.?\/)|(\/\/+)|(\\)/;
 const _winBadPath = /(\\\.\.?\\)|(\\\.\.?)$|^(\.\.?\\)|(\\\\+)|(\/)/;
-
-/**
- * Takes a Windows OS path and changes backward slashes to forward slashes.
- * This should only be done for OS paths from Windows (or user provided paths potentially from Windows).
- * Using it on a Linux or MaxOS path might change it.
- */
-export function toSlashes(osPath: string) {
-	return osPath.replace(/[\\/]/g, posix.sep);
-}
 
 function _isNormal(path: string, win: boolean): boolean {
     return win
