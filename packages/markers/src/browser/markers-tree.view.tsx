00e0d84f57da1e160ecf377b3a65f3a5ea8aaeb6
野松 <songtao.cst@alibaba-inc.com>
2019-11-11 09:50:47 +0800

    feat: update marker tree

--- packages/markers/src/browser/markers-tree.view.tsx
+++ packages/markers/src/browser/markers-tree.view.tsx
@@ -1,11 +1,13 @@
 import { RecycleTree, TreeNode } from '@ali/ide-core-browser/lib/components';
 import { observer, useComputed } from 'mobx-react-lite';
+import { IMatch } from '@ali/ide-core-common/lib/filters';
 import * as React from 'react';
 import { SeverityIconStyle } from './markers-seriverty-icon';
 import { MarkerService, ViewSize } from './markers-service';
 import { MarkerViewModel } from './markers.model';
 import * as styles from './markers.module.less';
 import Messages from './messages';
+import { IFilterMatches, IRenderableMarker, IRenderableMarkerModel } from '../common';
 
 const TAG_NONE = '';
 const EMPTY_FOLDING: string[] = [];
@@ -15,10 +17,85 @@ const DEFAULT_VIEWSIZE: ViewSize = {
 };
 
 /**
+ * render marker filename
+ * @param model model of renderable marker
+ */
+const MarkerItemFilename: React.FC<{ model: IRenderableMarkerModel }> = observer(({ model }) => {
+  const filenameMatches = model.matches && model.matches.filenameMatches;
+  if (filenameMatches) {
+    return <HightlightData data={model.filename} matches={filenameMatches} className={styles.filename} />;
+  } else {
+    return <div className={styles.filename}>{model.filename}</div>;
+  }
+});
+
+/**
+ * render marker filepath
+ * @param model model of renderable marker
+ */
+const MarkerItemFilePath: React.FC<{ model: IRenderableMarkerModel }> = observer(({ model }) => {
+  return <div className={styles.filepath}>{model.longname}</div>;
+});
+
+/**
+ * render highlight info which is filterd
+ */
+const HightlightData: React.FC<{ data: string, matches: IMatch[], className: string }> = observer(({ data, matches, className }) => {
+  const result: React.ReactNode[] = [];
+  let first = 0;
+  matches.forEach((match) => {
+    if (first < match.start) {
+      result.push(<span key={`hightlight-data-${first}-${match.start}`}>{data.substring(first, match.start)}</span>);
+    }
+    result.push(<span key={`hightlight-data-${match.start}-${match.end}`} className={styles.highlight}>{data.substring(match.start, match.end)}</span>);
+    first = match.end;
+  });
+  if (first < data.length) {
+    result.push(<span key={`hightlight-data-${first}-${data.length - 1}`}>{data.substring(first)}</span>);
+  }
+  return (
+    <div className={className}>{result}</div>
+  );
+});
+
+/**
+ * render marker message
+ */
+const MarkerItemMessage: React.FC<{ data: IRenderableMarker }> = observer(({ data }) => {
+  const messageMatchs = data.matches && data.matches.messageMatches;
+  if (messageMatchs) {
+    return <HightlightData data={data.message} matches={messageMatchs} className={styles.detail} />;
+  } else {
+    return (
+      <div className={styles.detail}>{data.message}</div>
+    );
+  }
+});
+
+/**
+ * render marker source and code
+ */
+const MarkerItemDescription: React.FC<{ data: IRenderableMarker }> = observer(({ data }) => {
+  const sourceMatches = data.matches && data.matches.sourceMatches;
+  const codeMatches = data.matches && data.matches.codeMatches;
+  return (
+    <div>
+      <div className={styles.typeContainer}>
+        {sourceMatches ? data.source && <HightlightData data={data.source} matches={sourceMatches} className={styles.type} /> : data.source}
+        {data.code && '('}
+        {data.code && codeMatches ? <HightlightData data={data.code} matches={codeMatches} className={styles.type} /> : data.code}
+        {data.code && ')'}
+      </div>
+      <div className={styles.position}>{`[${data.startLineNumber},${data.startColumn}]`}</div>
+    </div>
+  );
+});
+
+/**
  * render marker list
  * @param viewModel marker view model
  */
-const MarkerList: React.FC<{ viewModel: MarkerViewModel, search?: string }> = observer(({ viewModel, search }) => {
+const MarkerList: React.FC<{ viewModel: MarkerViewModel }> = observer(({ viewModel }) => {
   const markerService = MarkerService.useInjectable();
   const [selectTag, updateSelectTag] = React.useState('');
   const [folding, updateFolding] = React.useState(EMPTY_FOLDING);
@@ -29,7 +106,6 @@ const MarkerList: React.FC<{ viewModel: MarkerViewModel, search?: string }> = ob
       updateSelectTag(TAG_NONE);
     });
     markerService.onViewResize((size: ViewSize) => {
-      // console.error(size);
       updateViewSize(size);
     });
   });
@@ -42,9 +118,9 @@ const MarkerList: React.FC<{ viewModel: MarkerViewModel, search?: string }> = ob
         const isFolding = folding.indexOf(groupId) > -1;
         const item: TreeNode = {
           id: groupId,
-          name: model.filename,
+          name: () => <MarkerItemFilename model={model} />,
           icon: model.icon,
-          description: model.longname,
+          description: () => <MarkerItemFilePath model={model} />,
           badge: model.size(),
           parent: undefined,
           expanded: !isFolding,
@@ -59,8 +135,8 @@ const MarkerList: React.FC<{ viewModel: MarkerViewModel, search?: string }> = ob
             return {
               id,
               iconStyle: SeverityIconStyle[markerService.getThemeType()][marker.severity],
-              name: marker.message,
-              description: `${marker.source}${code} [${marker.startLineNumber},${marker.startColumn}]`,
+              name: () => <MarkerItemMessage data={marker} />,
+              description: () => <MarkerItemDescription data={marker} />,
               depth: 2,
               parent,
               selected: id === selectTag,
@@ -93,8 +169,6 @@ const MarkerList: React.FC<{ viewModel: MarkerViewModel, search?: string }> = ob
     <div style={contentStyle}>
       <RecycleTree
         nodes={ nodes }
-        search={ search }
-        searchable={ true }
         outline={ false }
         scrollContainerStyle={{ width: viewSize.w - (CONTENT_PADDING_RIGHT + CONTENT_PADDING_LEFT), height: viewSize.h - (CONTENT_PADDING_TOP + CONTENT_PADDING_BOTTOM), key: 'marker-list' }}
         containerHeight={ viewSize.h - (CONTENT_PADDING_TOP + CONTENT_PADDING_BOTTOM) }
@@ -155,7 +229,7 @@ export const MarkerPanel = observer(() => {
     <div className={styles.markersContent}>
       {
         viewModel.hasData() ?
-          <MarkerList viewModel={viewModel}  search={'ts'}/> :
+          <MarkerList viewModel={viewModel}/> :
           <Empty />
       }
     </div>
