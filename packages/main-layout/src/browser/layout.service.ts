2aced1904313666975594d3a11a63cedb0f3eb3d
ensorrow <zheyanglv@qq.com>
2019-11-25 14:47:33 +0800

    fix: toggle时tab默认值

--- packages/main-layout/src/browser/layout.service.ts
+++ packages/main-layout/src/browser/layout.service.ts
@@ -103,7 +103,7 @@ export class LayoutService extends WithEventBus {
     } else if (show === false) {
       tabbarService.currentContainerId = '';
     } else {
-      tabbarService.currentContainerId = tabbarService.currentContainerId ? '' : tabbarService.previousContainerId;
+      tabbarService.currentContainerId = tabbarService.currentContainerId ? '' : tabbarService.previousContainerId || tabbarService.containersMap.keys().next().value;
     }
   }
 
