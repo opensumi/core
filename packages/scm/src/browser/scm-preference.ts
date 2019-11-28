18e6f39f26a3f8c08517affb2cbb527d7240e0e5
vagusxl <vagusxl@gmail.com>
2019-11-28 12:24:50 +0800

    fix: 增加 scm.alwaysShowDiffWidget

--- packages/scm/src/browser/scm-preference.ts
+++ packages/scm/src/browser/scm-preference.ts
@@ -25,6 +25,11 @@ export const scmPreferenceSchema: PreferenceSchema = {
       default: 'all',
       description: localize('diffDecorations', 'Controls diff decorations in the editor.'),
     },
+    'scm.alwaysShowDiffWidget': {
+      type: 'boolean',
+      description: localize('alwaysShowDiffWidget', 'Controls whether to always click to show the Dirty Diff Widget.'),
+      default: true,
+    },
     'scm.diffDecorationsGutterWidth': {
       type: 'number',
       enum: [1, 2, 3, 4, 5],
