4abbddc46a30d63230dc190189b3b56e06ae088f
vagusxl <vagusxl@gmail.com>
2019-12-09 21:11:04 +0800

    feat: improve raw context key service

---
+++ packages/core-browser/__tests__/raw-context-key.test.ts
@@ -0,0 +1,81 @@
+import { MockContextKeyService } from '@ali/ide-monaco/lib/browser/mocks/monaco.context-key.service';
+
+import { RawContextKey } from '../src/raw-context-key';
+
+// tslint:disable-next-line:new-parens
+const contextKeyService = new class extends MockContextKeyService {
+  match(bool) {
+    if (bool) {
+      return bool;
+    }
+    return true;
+  }
+};
+
+describe('test for @ali/ide-core-browser/src/raw-context-key', () => {
+  const contextA = new RawContextKey<boolean>('contextA', false);
+  const contextB = new RawContextKey<string>('contextB', 'abc');
+  const contextC = new RawContextKey<'test1' | 'test2'>('contextC', 'test2');
+
+  it('ok with and/or', () => {
+    const when1 = RawContextKey.and(contextA, contextB);
+    const when2 = RawContextKey.or(contextB, contextC);
+    expect(when1).toBe('contextA && contextB');
+    expect(when2).toBe('contextB || contextC');
+
+    const when3 = RawContextKey.and(when1, when2);
+    const when4 = RawContextKey.or(when1, when2);
+    expect(when3).toBe('contextA && contextB && contextB || contextC');
+    expect(when4).toBe('contextA && contextB || contextB || contextC');
+  });
+
+  it('ok with raw/not', () => {
+    expect(contextA.raw).toBe('contextA');
+    expect(contextB.not).toBe('!contextB');
+
+    const when1 = RawContextKey.and(contextA.raw, contextB.not);
+    const when2 = RawContextKey.or(contextB, contextC.not);
+    expect(when1).toBe('contextA && !contextB');
+    expect(when2).toBe('contextB || !contextC');
+
+    const when3 = RawContextKey.and(when1, when2);
+    const when4 = RawContextKey.or(when1, when2);
+    expect(when3).toBe('contextA && !contextB && contextB || !contextC');
+    expect(when4).toBe('contextA && !contextB || contextB || !contextC');
+  });
+
+  it('ok with equalsTo/notEqualTo/regexMatches', () => {
+    const when1 = RawContextKey.and(contextA.equalsTo('nba'), contextB.not);
+    const when2 = RawContextKey.or(contextB.notEqualTo('cba'), contextC.not);
+    const when3 = RawContextKey.or(contextB.regexMatches(/\d+/i), contextC.not);
+    const regex = new RegExp('(\\s|^)' + 'source' + '\\b');
+    const when4 = RawContextKey.and(contextC.regexMatches(regex));
+    expect(when1).toBe('contextA == nba && !contextB');
+    expect(when2).toBe('contextB != cba || !contextC');
+    expect(when3).toBe('contextB =~ /\\d+/i || !contextC');
+    expect(when4).toBe('contextC =~ /(\\s|^)source\\b/');
+
+    const when5 = RawContextKey.and(when1, when2);
+    const when6 = RawContextKey.or(when1, when3);
+    const when7 = RawContextKey.or(when3, when4);
+    expect(when5).toBe('contextA == nba && !contextB && contextB != cba || !contextC');
+    expect(when6).toBe('contextA == nba && !contextB || contextB =~ /\\d+/i || !contextC');
+    expect(when7).toBe('contextB =~ /\\d+/i || !contextC || contextC =~ /(\\s|^)source\\b/');
+  });
+
+  describe('ok with bind/getValue', () => {
+    it('ok', () => {
+      const contextkeyA = contextA.bind(contextKeyService);
+      expect(contextkeyA.get()).toBe(false);
+      expect(contextA.getValue(contextKeyService)).toBe(false);
+
+      contextkeyA.set(true);
+      expect(contextkeyA.get()).toBe(true);
+      expect(contextA.getValue(contextKeyService)).toBe(true);
+
+      contextkeyA.reset();
+      expect(contextkeyA.get()).toBe(false);
+      expect(contextA.getValue(contextKeyService)).toBe(false);
+    });
+  });
+});
--- packages/core-browser/src/raw-context-key.ts
+++ packages/core-browser/src/raw-context-key.ts
@@ -1,14 +1,63 @@
 import { IContextKeyService, IContextKey } from './context-key';
 
+export abstract class IRawContextKey<T> {
+  raw: string;
+  not: string;
+  abstract equalsTo(value: string): string;
+  abstract notEqualTo(value: string): string;
+  abstract regexMatches(regexp: RegExp): string;
+  abstract bind(target: IContextKeyService): IContextKey<T>;
+  abstract getValue(target: IContextKeyService): T | undefined;
+}
+
+type ContextkeyCombinedClause<T = any> = Array<RawContextKey<T> | string>;
+
+function whenNormalizer(arr: ContextkeyCombinedClause, separator: '&&' | '||'): string {
+  const result: string[] = [];
+  if (Array.isArray(arr) && arr.length) {
+    for (const item of arr) {
+      if (item instanceof RawContextKey) {
+        result.push(item.raw);
+      } else {
+        result.push(item);
+      }
+    }
+  }
+
+  return result.join(' ' + separator + ' ');
+}
+
 /**
- * 这里只实现了 RawContextKey 的 bindTo/getValue 方法
  * 我们目前的 when 的使用并没有直接使用 monaco.contextkey.ContextKeyExpr
- * when: ContextKeyExpr.and(ExplorerRootContext, ExplorerFolderContext)
- * todo: 需要考虑是否需要支持类似 vscode 上方写法, string 也能满足需求
+ * 实现了 bind/getValue 方法
+ * 实现了 and/or/not/equals/notEquals/regexMatches 等工具方法
+ * 实现了类似效果:
+ * when: RawContextKey.and(ExplorerRootContext, ExplorerFolderContext)
+ * 参考 https://code.visualstudio.com/docs/getstarted/keybindings#_when-clause-contexts
  */
-export class RawContextKey<T> {
+export class RawContextKey<T> implements IRawContextKey<T> {
   private key: string;
   private defaultValue: T | undefined;
+
+  /**
+   * and alias '&&'
+   */
+  static and(...args: ContextkeyCombinedClause): string {
+    if (!args.length) {
+      return '';
+    }
+    return whenNormalizer(args, '&&');
+  }
+
+  /**
+   * or alias '||'
+   */
+  static or(...args: ContextkeyCombinedClause) {
+    if (!args.length) {
+      return '';
+    }
+    return whenNormalizer(args, '||');
+  }
 
   constructor(key: string, defaultValue: T | undefined) {
     this.key = key;
@@ -19,7 +68,45 @@ export class RawContextKey<T> {
     return target.createKey(this.key, this.defaultValue);
   }
 
-  public getContextValue(target: IContextKeyService): T | undefined {
+  public getValue(target: IContextKeyService): T | undefined {
     return target.getContextValue<T>(this.key);
+  }
+
+  /**
+   * maybe you don't need use this directly
+   */
+  public get raw() {
+    return this.key;
+  }
+
+  /**
+   * get negated value
+   */
+  public get not() {
+    return '!' + this.raw;
+  }
+
+  /**
+   * is equal to
+   */
+  public equalsTo(value: string): string {
+    return this.raw + ' == ' + value;
+  }
+
+  /**
+   * is not equal to
+   */
+  public notEqualTo(value: string): string {
+    return this.raw + ' != ' + value;
+  }
+
+  /**
+   * matches regex
+   */
+  public regexMatches(regexp: RegExp): string {
+    const regexpStr = regexp
+      ? `/${regexp.source}/${regexp.ignoreCase ? 'i' : ''}`
+      : '/invalid/';
+    return this.raw + ' =~ ' + regexpStr;
   }
 }
