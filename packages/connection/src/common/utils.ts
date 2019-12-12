80d8c22f1ac96570ee38677582fa0fbf95b0fc9f
宗羽 <xiaochen.gaoxc@antfin.com>
2019-12-12 12:14:46 +0800

    fix: 暂时放弃压缩消息体，因为可能导致浏览器卡顿

--- packages/connection/src/common/utils.ts
+++ packages/connection/src/common/utils.ts
@@ -1,29 +1,27 @@
-import * as lzutf8 from 'lzutf8';
-import { isElectronEnv, isDevelopment } from '@ali/ide-core-common';
+// import * as lzutf8 from 'lzutf8';
+// import { isElectronEnv, isDevelopment } from '@ali/ide-core-common';
 
-const encoding = 'Base64'; // 'StorageBinaryString'
+// const encoding = 'Base64'; // 'StorageBinaryString'
 
 export function stringify(obj: any): string {
-  const str = JSON.stringify(obj);
-
-  if (!isElectronEnv() && !isDevelopment()) {
-    return lzutf8.compress(str, {
-      outputEncoding: encoding,
-    });
-  } else {
-    return str;
-  }
+  return JSON.stringify(obj);
+  // if (!isElectronEnv() && !isDevelopment()) {
+  //   return lzutf8.compress(str, {
+  //     outputEncoding: encoding,
+  //   });
+  // } else {
+  //   return str;
+  // }
 }
 
 export function parse(input: string, reviver?: (this: any, key: string, value: any) => any): any {
-  let str;
-
-  if (!isElectronEnv() && !isDevelopment()) {
-    str = lzutf8.decompress(input, {
-      inputEncoding: encoding,
-    });
-  } else {
-    str = input;
-  }
-  return JSON.parse(str, reviver);
+  // let str;
+  // if (!isElectronEnv() && !isDevelopment()) {
+  //   str = lzutf8.decompress(input, {
+  //     inputEncoding: encoding,
+  //   });
+  // } else {
+  //   str = input;
+  // }
+  return JSON.parse(input, reviver);
 }
