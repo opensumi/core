8a20706d553724580ee14bcd882cedb73d2ce290
MMhunter <kengtou.mh@antfin.com>
2019-12-20 12:28:15 +0800

    fix: 修复electron webview(git-graph正常工作）

--- packages/core-electron-main/src/bootstrap/services/protocol.ts
+++ packages/core-electron-main/src/bootstrap/services/protocol.ts
@@ -13,7 +13,7 @@ export class ProtocolElectronMainContribution implements ElectronMainContributio
       const uri = new URI(url);
       try {
         const data = await readFile(uri.codeUri.fsPath);
-        callback({ mimeType: 'raw', data});
+        callback({ mimeType: getWebviewContentMimeType(uri), data});
       } catch (e) {
         callback({ error: -2});
       }
@@ -38,4 +38,83 @@ export class ProtocolElectronMainContribution implements ElectronMainContributio
     }
   }
 
+}
+
+export const MIME_UNKNOWN = 'application/unknown';
+
+const webviewMimeTypes = new Map([
+  ['.svg', 'image/svg+xml'],
+  ['.txt', 'text/plain'],
+  ['.css', 'text/css'],
+  ['.js', 'application/javascript'],
+  ['.json', 'application/json'],
+  ['.html', 'text/html'],
+  ['.htm', 'text/html'],
+  ['.xhtml', 'application/xhtml+xml'],
+  ['.oft', 'font/otf'],
+  ['.xml', 'application/xml'],
+]);
+
+export function getWebviewContentMimeType(normalizedPath: URI): string {
+  return webviewMimeTypes.get(normalizedPath.path.ext) || getMediaMime(normalizedPath.path.ext) || MIME_UNKNOWN;
+}
+
+interface MapExtToMediaMimes {
+  [index: string]: string;
+}
+
+// Known media mimes that we can handle
+const mapExtToMediaMimes: MapExtToMediaMimes = {
+  '.aac': 'audio/x-aac',
+  '.avi': 'video/x-msvideo',
+  '.bmp': 'image/bmp',
+  '.flv': 'video/x-flv',
+  '.gif': 'image/gif',
+  '.ico': 'image/x-icon',
+  '.jpe': 'image/jpg',
+  '.jpeg': 'image/jpg',
+  '.jpg': 'image/jpg',
+  '.m1v': 'video/mpeg',
+  '.m2a': 'audio/mpeg',
+  '.m2v': 'video/mpeg',
+  '.m3a': 'audio/mpeg',
+  '.mid': 'audio/midi',
+  '.midi': 'audio/midi',
+  '.mk3d': 'video/x-matroska',
+  '.mks': 'video/x-matroska',
+  '.mkv': 'video/x-matroska',
+  '.mov': 'video/quicktime',
+  '.movie': 'video/x-sgi-movie',
+  '.mp2': 'audio/mpeg',
+  '.mp2a': 'audio/mpeg',
+  '.mp3': 'audio/mpeg',
+  '.mp4': 'video/mp4',
+  '.mp4a': 'audio/mp4',
+  '.mp4v': 'video/mp4',
+  '.mpe': 'video/mpeg',
+  '.mpeg': 'video/mpeg',
+  '.mpg': 'video/mpeg',
+  '.mpg4': 'video/mp4',
+  '.mpga': 'audio/mpeg',
+  '.oga': 'audio/ogg',
+  '.ogg': 'audio/ogg',
+  '.ogv': 'video/ogg',
+  '.png': 'image/png',
+  '.psd': 'image/vnd.adobe.photoshop',
+  '.qt': 'video/quicktime',
+  '.spx': 'audio/ogg',
+  '.svg': 'image/svg+xml',
+  '.tga': 'image/x-tga',
+  '.tif': 'image/tiff',
+  '.tiff': 'image/tiff',
+  '.wav': 'audio/x-wav',
+  '.webm': 'video/webm',
+  '.webp': 'image/webp',
+  '.wma': 'audio/x-ms-wma',
+  '.wmv': 'video/x-ms-wmv',
+  '.woff': 'application/font-woff',
+};
+
+export function getMediaMime(ext: string): string | undefined {
+  return mapExtToMediaMimes[ext.toLowerCase()];
 }
