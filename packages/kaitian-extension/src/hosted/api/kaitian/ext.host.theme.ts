437ba80724e58fa501e320da8d070947e61d86fd
MMhunter <kengtou.mh@antfin.com>
2019-12-24 14:51:35 +0800

    feat: 添加开天用theme API

---
+++ packages/kaitian-extension/src/hosted/api/kaitian/ext.host.theme.ts
@@ -0,0 +1,39 @@
+import { IRPCProtocol } from '@ali/ide-connection';
+import { ExtensionCandiDate, Emitter } from '@ali/ide-core-common';
+
+import { MainThreadKaitianAPIIdentifier } from '../../../common/kaitian';
+import { IExtHostTheme, IMainThreadTheme } from '../../../common/kaitian/theme';
+
+export function createThemeApi(
+  theme: ExtHostTheme,
+) {
+  return {
+    getThemeColors: async (dir: string) => {
+      return theme.getThemeColors();
+    },
+    onThemeChanged: theme.onThemeChanged,
+  };
+}
+
+export class ExtHostTheme implements IExtHostTheme {
+
+  private proxy: IMainThreadTheme;
+
+  private _onThemeChanged = new Emitter<void>();
+
+  public readonly onThemeChanged = this._onThemeChanged.event;
+
+  constructor(
+    private rpcProtocol: IRPCProtocol,
+  ) {
+    this.proxy = this.rpcProtocol.getProxy(MainThreadKaitianAPIIdentifier.MainThreadTheme);
+  }
+
+  async $notifyThemeChanged(): Promise<void> {
+    this._onThemeChanged.fire();
+  }
+
+  async getThemeColors(): Promise<{[key: string]: string}> {
+    return this.proxy.$getThemeColors();
+  }
+}
