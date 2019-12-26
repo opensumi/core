4e5c57744c652c0d1d27876d65f549b8b074beba
GGICE <i@ice.gs>
2019-12-26 15:48:59 +0800

    test: back connect test

--- packages/connection/__test__/node/index.test.ts
+++ packages/connection/__test__/node/index.test.ts
@@ -57,7 +57,7 @@ describe('connection', () => {
       dispose: () => {},
     });
 
-    const connection = new WebSocket('ws://127.0.0.1:7788/service/channel1', ['12345']);
+    const connection = new WebSocket('ws://127.0.0.1:7788/service');
 
     await new Promise((resolve) => {
       connection.on('open', () => {
