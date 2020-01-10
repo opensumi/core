3a117409a58d4863d4978c22f950c7f645c5bf10
MMhunter <kengtou.mh@antfin.com>
2020-01-10 19:07:04 +0800

    fix: 使用inDebugMode contextkey

--- packages/debug/src/browser/editor/editor-hover-contribution.ts
+++ packages/debug/src/browser/editor/editor-hover-contribution.ts
@@ -2,35 +2,36 @@ import { IEditorFeatureContribution } from '@ali/ide-editor/lib/browser';
 import { IEditor } from '@ali/ide-editor';
 import { IDisposable, Disposable  } from '@ali/ide-core-common';
 import { Injectable, Autowired } from '@ali/common-di';
-import { IDebugSessionManager } from '../../common';
-import { DebugSessionManager } from '../debug-session-manager';
+import { IContextKeyService } from '@ali/ide-core-browser';
 
 @Injectable()
 export class EditorHoverContribution implements IEditorFeatureContribution {
 
-  @Autowired(IDebugSessionManager)
-  protected readonly manager: DebugSessionManager;
+  static keySet = new Set(['inDebugMode']);
+
+  @Autowired(IContextKeyService)
+  protected readonly contextKeyService: IContextKeyService;
 
   contribute(editor: IEditor): IDisposable {
 
     const disposer = new Disposable();
+    this.updateHoverEnabled(editor);
 
-    disposer.addDispose(this.manager.onDidStartDebugSession(() => {
-      this.setHoverEnabled(editor, false);
-    }));
-
-    disposer.addDispose(this.manager.onDidStopDebugSession(() => {
-      this.setHoverEnabled(editor, true);
+    disposer.addDispose(this.contextKeyService.onDidChangeContext((e) => {
+      if (e.payload.affectsSome(EditorHoverContribution.keySet)) {
+        this.updateHoverEnabled(editor);
+      }
     }));
 
     return disposer;
   }
 
-  setHoverEnabled(editor: IEditor, hoverEnabled: boolean) {
+  updateHoverEnabled(editor: IEditor) {
+    const inDebugMode = this.contextKeyService.match('inDebugMode');
     // monaco 内置hover选项
     editor.monacoEditor.updateOptions({
       hover: {
-        enabled: hoverEnabled,
+        enabled: !inDebugMode,
       },
     });
   }
