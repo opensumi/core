9d617a5d6e11c5d1cb7f0022fe3285ca115cc910
vagusxl <vagusxl@gmail.com>
2020-02-03 19:03:36 +0800

    feat: 在 addon 模块监听 connection 事件

--- packages/addons/src/browser/index.ts
+++ packages/addons/src/browser/index.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@ali/common-di';
 import { BrowserModule } from '@ali/ide-core-browser';
 import { FileSearchContribution } from './file-search.contribution';
+import { StatusBarContribution } from './status-bar.contribution';
 
 @Injectable()
 export class ClientAddonModule extends BrowserModule {
   providers = [
     FileSearchContribution,
+    StatusBarContribution,
   ];
 }
