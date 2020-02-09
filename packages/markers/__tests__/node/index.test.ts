6952b3fcbad3772e315f8f1206415b0d794b8723
vagusxl <vagusxl@gmail.com>
2020-02-09 22:27:00 +0800

    feat: remove node/index.ts from markers module

--- packages/markers/__tests__/node/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { MarkersModule } from '../../src/node';
-
-describe('template test', () => {
-  it('MarkersModule', () => {
-    const cls = new MarkersModule();
-    expect(cls.providers).toEqual([]);
-  });
-});
