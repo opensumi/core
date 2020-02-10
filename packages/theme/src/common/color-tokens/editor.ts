1a349fbff3db6355ecbff2758563e67bc8c8b53c
MMhunter <kengtou.mh@antfin.com>
2020-02-10 20:05:00 +0800

    fix: tab允许拖动到空白处

--- packages/theme/src/common/color-tokens/editor.ts
+++ packages/theme/src/common/color-tokens/editor.ts
@@ -165,7 +165,7 @@ export const EDITOR_GROUP_BORDER = registerColor('editorGroup.border', {
 }, localize('editorGroupBorder', 'Color to separate multiple editor groups from each other. Editor groups are the containers of editors.'));
 
 export const EDITOR_DRAG_AND_DROP_BACKGROUND = registerColor('editorGroup.dropBackground', {
-  dark: Color.fromHex('#53595D').transparent(0.5),
+  dark: Color.fromHex('#000000').transparent(0.5),
   light: Color.fromHex('#2677CB').transparent(0.18),
   hc: null,
 }, localize('editorDragAndDropBackground', 'Background color when dragging editors around. The color should have transparency so that the editor contents can still shine through.'));
