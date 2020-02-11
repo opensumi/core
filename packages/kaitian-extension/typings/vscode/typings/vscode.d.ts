7a1ceb414695753a102ffbba93044975250ff957
erha19 <danwu.wdw@alibaba-inc.com>
2020-02-11 11:44:53 +0800

    chore: 移动实现了的方法至typings

--- packages/kaitian-extension/typings/vscode/typings/vscode.d.ts
+++ packages/kaitian-extension/typings/vscode/typings/vscode.d.ts
@@ -853,7 +853,14 @@ declare module 'vscode' {
 		/**
 		 * Whether to show collapse all action or not.
 		 */
-		showCollapseAll?: boolean;
+    showCollapseAll?: boolean;
+
+    /**
+		 * Whether the tree supports multi-select. When the tree supports multi-select and a command is executed from the tree,
+		 * the first argument to the command is the tree item that the command was executed on and the second argument is an
+		 * array containing all selected tree items.
+		 */
+		canSelectMany?: boolean;
 	}
 
 
