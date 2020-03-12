5a934866b4916fc246c6b017dd79bc82bac54840
vagusxl <vagusxl@gmail.com>
2020-03-12 15:27:39 +0800

    feat: remove kt.sideBarList colors

--- packages/theme/src/common/color-tokens/custom/sidebar.ts
+++ packages/theme/src/common/color-tokens/custom/sidebar.ts
@@ -3,8 +3,6 @@ import { localize } from '@ali/ide-core-common';
 import { EDITOR_GROUP_HEADER_TABS_BACKGROUND } from '../editor';
 import { registerColor } from '../../color-registry';
 import { SIDE_BAR_TITLE_FOREGROUND } from '../sidebar';
-import { foreground, descriptionForeground } from '../base';
-import { listHoverBackground, listInactiveSelectionBackground } from '../list-tree';
 
 export const ktSideBarTitleBorder = registerColor('kt.sideBarTitle.border', {
   dark: EDITOR_GROUP_HEADER_TABS_BACKGROUND,
@@ -17,27 +15,3 @@ export const ktSideBarTitleActiveBorder = registerColor('kt.sideBarTitle.activeB
   light: SIDE_BAR_TITLE_FOREGROUND,
   hc: SIDE_BAR_TITLE_FOREGROUND,
 }, localize('sideBarTitleActiveBorder', 'SideBar title border color when active'));
-
-export const ktSideBarListForeground = registerColor('kt.sideBarList.foreground', {
-  dark: foreground,
-  light: foreground,
-  hc: foreground,
-}, localize('ktSideBarListForeground', 'SideBarList foreground'));
-
-export const ktSideBarListSecondaryForeground = registerColor('kt.sideBarList.secondaryForeground', {
-  dark: descriptionForeground,
-  light: descriptionForeground,
-  hc: descriptionForeground,
-}, localize('ktSideBarListSecondaryForeground', 'SideBarList secondary foreground'));
-
-export const ktSideBarListHoverBackground = registerColor('kt.sideBarList.hoverForeground', {
-  dark: listHoverBackground,
-  light: listHoverBackground,
-  hc: listHoverBackground,
-}, localize('ktSideBarListHoverBackground', 'SideBarList hover background'));
-
-export const ktSideBarListSelectionBackground = registerColor('kt.sideBarList.selectionBackground', {
-  dark: listInactiveSelectionBackground,
-  light: listInactiveSelectionBackground,
-  hc: listInactiveSelectionBackground,
-}, localize('ktSideBarListSelectionBackground', 'SideBarList Select background'));
