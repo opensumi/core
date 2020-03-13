f108c411d92a1c27aecf3e1828d4a454c66d7e8c
Aaaaash <xubing.bxb@alibaba-inc.com>
2020-03-13 18:57:32 +0800

    feat: move snackbar to ide-components

--- packages/components/src/message/index.ts
+++ packages/components/src/message/index.ts
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import message from 'antd/lib/message';
-import 'antd/lib/message/style/index.less';
-import './snackbar.module.less';
+import antdMessage from 'antd/lib/message';
+import 'antd/lib/message/style/index.css';
+import './style.module.less';
 
 function generateSnackbar(funName: string) {
   return (content: string | React.ReactNode, duration?: number): Promise<void> => {
     return new Promise((resolve) => {
-      message[funName](content, duration, resolve);
+      antdMessage[funName](content, duration, resolve);
     });
   };
 }
 
-export const snackbar = {
+export const message = {
   success: generateSnackbar('success'),
   error: generateSnackbar('error'),
   info: generateSnackbar('info'),
