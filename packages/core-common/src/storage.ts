898ea62cf9422ab6408fe49338509637123df00e
ensorrow <zheyanglv@qq.com>
2020-03-13 14:13:31 +0800

    chore: 废弃旧global layout

--- packages/core-common/src/storage.ts
+++ packages/core-common/src/storage.ts
@@ -55,7 +55,7 @@ export const STORAGE_NAMESPACE = {
   DEBUG: new URI('debug').withScheme(STORAGE_SCHEMA.SCOPE),
   OUTLINE: new URI('outline').withScheme(STORAGE_SCHEMA.SCOPE),
   // global database
-  GLOBAL_LAYOUT: new URI('layout').withScheme(STORAGE_SCHEMA.GLOBAL),
+  GLOBAL_LAYOUT: new URI('layout-global').withScheme(STORAGE_SCHEMA.GLOBAL),
   GLOBAL_EXTENSIONS: new URI('extensions').withScheme(STORAGE_SCHEMA.GLOBAL),
   GLOBAL_RECENT_DATA: new URI('recent').withScheme(STORAGE_SCHEMA.GLOBAL),
 }
