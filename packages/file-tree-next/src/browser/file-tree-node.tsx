d3b6777397ab19b38d74b7cbc305cccbccde4fa6
erha19 <danwu.wdw@alibaba-inc.com>
2020-03-16 02:56:53 +0800

    feat: 实现基础的文件夹展开功能

--- packages/file-tree-next/src/browser/file-tree-node.tsx
+++ packages/file-tree-next/src/browser/file-tree-node.tsx
@@ -1,60 +1,127 @@
 import * as React from 'react';
 import * as cls from 'classnames';
-import * as styles from './file-tree.module.less';
-import { TreeNode, CompositeTreeNode, RenamePromptHandle, NodeType, INodeRendererProps, PromptHandle } from '@ali/ide-components';
+import * as styles from './file-tree-node.module.less';
+import { TreeNode, CompositeTreeNode, NodeType, INodeRendererProps } from '@ali/ide-components';
+import { LabelService } from '@ali/ide-core-browser/lib/services';
+import { getIcon } from '@ali/ide-core-browser';
+import { Directory, File } from './file-tree-nodes';
 
 export interface IFileTreeNodeProps {
   item: any;
+  defaultLeftPadding?: number;
+  leftPadding?: number;
+  labelService: LabelService;
+  onTwistieClick?: (ev: React.MouseEvent, item: TreeNode | CompositeTreeNode, type: NodeType) => void;
   onClick: (ev: React.MouseEvent, item: TreeNode | CompositeTreeNode, type: NodeType) => void;
 }
 export type FileTreeNodeRenderedProps = IFileTreeNodeProps & INodeRendererProps;
 
 export const FileTreeNode: React.FC<FileTreeNodeRenderedProps> = ({
-  onClick,
   item,
+  onClick,
   itemType,
+  labelService,
+  leftPadding = 8,
+  onTwistieClick,
+  defaultLeftPadding = 8,
 }: FileTreeNodeRenderedProps) => {
 
-  const isRenamePrompt = itemType === NodeType.RenamePrompt;
-  const isNewPrompt = itemType === NodeType.NewPrompt;
-  const isPrompt = isRenamePrompt || isNewPrompt;
-  const isDirExpanded = itemType === NodeType.CompositeTreeNode
-    ? (item as CompositeTreeNode).expanded
-    : itemType === NodeType.RenamePrompt && CompositeTreeNode.is((item as RenamePromptHandle).target)
-      ? ((item as RenamePromptHandle).target as CompositeTreeNode).expanded
-      : false;
+  const handleClick = (ev: React.MouseEvent) => {
+    if (itemType === NodeType.TreeNode || itemType === NodeType.CompositeTreeNode) {
+      onClick(ev, item as File, itemType);
+    }
+  };
 
-  const fileOrDir =
-    (itemType === NodeType.TreeNode ||
-      itemType === NodeType.NewPrompt ||
-      (itemType === NodeType.RenamePrompt && !CompositeTreeNode.is((item as RenamePromptHandle).target)))
-      ? 'file'
-      : 'directory';
+  const handlerTwistieClick = (ev: React.MouseEvent) => {
+    if (itemType === NodeType.TreeNode || itemType === NodeType.CompositeTreeNode) {
+      if (onTwistieClick) {
+        onTwistieClick(ev, item as File, itemType);
+      } else {
+        onClick(ev, item as File, itemType);
+      }
+    }
+  };
+
+  const isDirectory = itemType === NodeType.CompositeTreeNode;
+
+  const isFocused = item.focused;
+  const isSelected = item.selected;
+
+  const fileTreeNodeStyle = {
+    height: FILE_TREE_NODE_HEIGHT,
+    lineHeight: `${FILE_TREE_NODE_HEIGHT}px`,
+    paddingLeft: isDirectory ? `${defaultLeftPadding + (item.depth || 0) * (leftPadding || 0)}px` : `${defaultLeftPadding + (item.depth || 0) * (leftPadding || 0) + 5}px`,
+  } as React.CSSProperties;
+
+  const renderFolderToggle = (node: Directory, clickHandler: any) => {
+    // TODO: loading
+    // if (node.isLoading) {
+    //   return <Loading />;
+    // }
+    return <div
+      onClick={clickHandler}
+      className={cls(
+        styles.file_tree_node_segment,
+        styles.expansion_toggle,
+        getIcon('arrow-right'),
+        { [`${styles.mod_collapsed}`]: !node.expanded },
+      )}
+    />;
+  };
+
+  const renderIcon = (node: Directory | File) => {
+    const iconClass = labelService.getIcon(node.uri);
+    return <div className={cls(styles.file_icon, iconClass, {expanded: isDirectory && (node as Directory).expanded})} style={{ height: FILE_TREE_NODE_HEIGHT, lineHeight: `${FILE_TREE_NODE_HEIGHT}px`}}>
+    </div>;
+  };
+
+  const renderDisplayName = (node: Directory | File) => {
+    return <div
+        className={cls(styles.file_tree_node_segment, styles.file_tree_node_displayname)}
+      >
+        {node.name}
+      </div>;
+  };
+
+  const renderStatusTail = (node: Directory | File) => {
+    return <div className={cls(styles.file_tree_node_segment, styles.file_tree_node_tail)}>
+      {renderBadge(node)}
+    </div>;
+  };
+
+  const renderBadge = () => {
+    // TODO: 装饰器逻辑
+    return null;
+  };
 
   return (
     <div
-      className={cls(styles.tree_node, {
-        renaming: isRenamePrompt,
-        prompt: isRenamePrompt || isNewPrompt,
-        new: isNewPrompt,
-      }, fileOrDir, `depth-${item.depth}`)}
-      data-depth={item.depth}
-      draggable={true}
+      key={item.id}
+      onClick={handleClick}
+    >
+      <div
+        className={cls(
+          styles.treenode,
+          {
+            [styles.mod_focused]: isFocused,
+            [styles.mod_selected]: !isFocused && isSelected,
+          },
+        )}
+        style={fileTreeNodeStyle}
       >
-      {!isNewPrompt && fileOrDir === 'directory' ?
-        <i className={cls('directory-toggle', isDirExpanded ? 'open' : '')} />
-        : null
-      }
-      <span className='file-label'>
-        <i className={cls('file-icon', isNewPrompt ? 'new' : '', fileOrDir)} />
-        <span className='file-name'>
-          {isPrompt && item instanceof PromptHandle
-            ? <><item.ProxiedInput /><span className='prompt-err-msg'></span></>
-            : (item as TreeNode).name
-          }
-        </span>
-      </span>
-    </div>);
+        <div className={cls(styles.file_tree_node_content)}>
+          {(isDirectory && renderFolderToggle(item, handlerTwistieClick))}
+          {renderIcon(item)}
+          <div
+            className={styles.file_tree_node_overflow_wrap}
+          >
+            {renderDisplayName(item)}
+          </div>
+          {renderStatusTail(item)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const FILE_TREE_NODE_HEIGHT = 22;
--- packages/file-tree-next/src/browser/file-tree.tsx
+++ packages/file-tree-next/src/browser/file-tree.tsx
@@ -2,20 +2,37 @@ import * as React from 'react';
 import { observer } from 'mobx-react-lite';
 import { ViewState, useInjectable } from '@ali/ide-core-browser';
 import * as styles from './file-tree.module.less';
-import { RecycleTree, INodeRendererProps, TreeModel } from '@ali/ide-components';
+import { RecycleTree, INodeRendererProps, TreeModel, NodeType, IRecycleTreeHandle } from '@ali/ide-components';
 import { FileTreeNode, FILE_TREE_NODE_HEIGHT } from './file-tree-node';
-import { FileTreeService } from './file-tree.service';
+// import { FileTreeService } from './file-tree.service';
 import { FileTreeModelService } from './services/file-tree-model.service';
+import { LabelService } from '@ali/ide-core-browser/lib/services';
+import { Directory, File } from './file-tree-nodes';
 
 export const FileTree = observer(({
   viewState,
 }: React.PropsWithChildren<{ viewState: ViewState }>) => {
   const [isReady, setIsReady] = React.useState<boolean>(false);
+  const [treeHandle, setTreeHandler] = React.useState<IRecycleTreeHandle>();
   const { width, height } = viewState;
   // const fileTreeService = useInjectable<FileTreeService>(FileTreeService);
   const { treeModel } = useInjectable<FileTreeModelService>(FileTreeModelService);
-  const handleItemClicked = (...args) => {
-    // console.log(args);
+  const labelService = useInjectable<LabelService>(LabelService);
+  const toggleDirectory = (item: Directory) => {
+    if (treeHandle) {
+      if (item.expanded) {
+        treeHandle.collapseNode(item);
+      } else {
+        treeHandle.expandNode(item);
+      }
+    }
+  };
+
+  const handleItemClicked = (ev: React.MouseEvent, item: File | Directory, type: NodeType) => {
+    // TODO: 使用装饰器逻辑来装饰激活项/选中态
+    if (type === NodeType.CompositeTreeNode) {
+      toggleDirectory(item as Directory);
+    }
   };
 
   React.useEffect(() => {
@@ -28,17 +45,26 @@ export const FileTree = observer(({
     setIsReady(true);
   };
 
+  const handleTreeReady = (handle: IRecycleTreeHandle) => {
+    setTreeHandler({
+      ...handle,
+      getModel: () => treeModel,
+    });
+  };
+
   const renderFileTree = () => {
     if (isReady) {
       return <RecycleTree
         height={height}
         width={width}
         itemHeight={FILE_TREE_NODE_HEIGHT}
+        onReady={handleTreeReady}
         model={treeModel}
       >
         {(props: INodeRendererProps) => <FileTreeNode
           item={props.item}
           itemType={props.itemType}
+          labelService={labelService}
           onClick={handleItemClicked}
         />}
       </RecycleTree>;
