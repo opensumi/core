75b3b6e9c1d96f23f10c941c67bd1cb144487cf8
frankLife <shangpo.zw@alibaba-inc.com>
2020-04-01 19:05:29 +0800

    chore: lint

--- tools/electron/src/extension/index.ts
+++ tools/electron/src/extension/index.ts
@@ -1,6 +1,6 @@
-import {extProcessInit} from '@ali/ide-kaitian-extension/lib/hosted/ext.process-base.js'
-import LogServiceClass from './mock-log-service'
+import {extProcessInit} from '@ali/ide-kaitian-extension/lib/hosted/ext.process-base.js';
+import LogServiceClass from './mock-log-service';
 
-(async()=>{
+(async () => {
   await extProcessInit({LogServiceClass});
-})()
+})();
