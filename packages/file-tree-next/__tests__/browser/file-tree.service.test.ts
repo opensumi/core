9222fe07a09a4100cd6448c32d457f4550bdb4b2
erha19 <danwu.wdw@alibaba-inc.com>
2020-04-02 13:22:10 +0800

    test: 添加FileTreeService相关测试用例

---
+++ packages/file-tree-next/__tests__/browser/file-tree.service.test.ts
@@ -0,0 +1,196 @@
+import { MockInjector } from '../../../../tools/dev-tool/src/mock-injector';
+import { FileTreeService } from '../../src/browser/file-tree.service';
+import { createBrowserInjector } from '../../../../tools/dev-tool/src/injector-helper';
+import { IContextKeyService, CorePreferences, Disposable, URI, EDITOR_COMMANDS, FILE_COMMANDS, ILoggerManagerClient } from '@ali/ide-core-browser';
+import { MockContextKeyService } from '@ali/ide-core-browser/lib/mocks/context-key';
+import { IWorkspaceService } from '@ali/ide-workspace';
+import { MockWorkspaceService } from '@ali/ide-workspace/lib/common/mocks';
+import { IFileServiceClient, FileChangeType } from '@ali/ide-file-service';
+import { FileTreeContribution } from '../../src/browser/file-tree-contribution';
+import { IDecorationsService } from '@ali/ide-decoration';
+import { IMainLayoutService } from '@ali/ide-main-layout';
+import { WorkbenchEditorService } from '@ali/ide-editor';
+import { IWindowDialogService, IDialogService, IMessageService } from '@ali/ide-overlay';
+import { IFileTreeAPI } from '../../src/common';
+import { IThemeService } from '@ali/ide-theme';
+
+describe('FileTree Service should be work alone', () => {
+  let injector: MockInjector;
+  let fileTreeService: FileTreeService;
+  let onPreferenceChanged;
+  let mockFileServiceClient;
+  let fileChangeWatcher;
+  beforeEach(() => {
+    injector = createBrowserInjector([]);
+    onPreferenceChanged = jest.fn((valueChangeHandle) => {
+      valueChangeHandle({
+        preferenceName: 'explorer.fileTree.baseIndent',
+        newValue: 6,
+      });
+      valueChangeHandle({
+        preferenceName: 'explorer.fileTree.indent',
+        newValue: 6,
+      });
+      return Disposable.create(() => {});
+    });
+    fileChangeWatcher = {
+      onFilesChanged: jest.fn(() => Disposable.create(() => {})),
+      dispose: () => {},
+    };
+    mockFileServiceClient = {
+      watchFileChanges: jest.fn(() => fileChangeWatcher),
+      dispose: () => {},
+    };
+    injector.overrideProviders(
+      {
+        token: IDecorationsService,
+        useValue: {},
+      },
+      {
+        token: IMainLayoutService,
+        useValue: {},
+      },
+      {
+        token: WorkbenchEditorService,
+        useValue: {},
+      },
+      {
+        token: IWindowDialogService,
+        useValue: {},
+      },
+      {
+        token: IDialogService,
+        useValue: {},
+      },
+      {
+        token: IThemeService,
+        useValue: {},
+      },
+      {
+        token: ILoggerManagerClient,
+        useValue: {},
+      },
+      {
+        token: IFileTreeAPI,
+        useValue: {},
+      },
+      {
+        token: IMessageService,
+        useValue: {},
+      },
+      {
+        token: IContextKeyService,
+        useClass: MockContextKeyService,
+      },
+      {
+        token: IWorkspaceService,
+        useClass: MockWorkspaceService,
+      },
+      {
+        token: CorePreferences,
+        useValue: {
+          'explorer.fileTree.baseIndent': 8,
+          'explorer.fileTree.indent': 8,
+          onPreferenceChanged,
+        },
+      },
+      {
+        token: IFileServiceClient,
+        useValue: mockFileServiceClient,
+      },
+      {
+        token: FileTreeService,
+        useClass: FileTreeService,
+      },
+    );
+    fileTreeService = injector.get(FileTreeService);
+  });
+
+  afterEach(() => {
+    injector.disposeAll();
+    onPreferenceChanged.mockReset();
+    mockFileServiceClient.watchFileChanges.mockReset();
+    fileChangeWatcher.onFilesChanged.mockReset();
+  });
+
+  it('Service should be init correctly', async (done) => {
+    await fileTreeService.init();
+    expect(onPreferenceChanged).toBeCalled();
+    expect(fileTreeService.indent).toBe(6);
+    expect(fileTreeService.baseIndent).toBe(6);
+    done();
+  });
+
+  it('ContextMenuContextKeyService should be existed', () => {
+    expect(!!fileTreeService.contextMenuContextKeyService).toBeTruthy();
+  });
+
+  it('File watch should be work', async (done) => {
+    const workspaceService = injector.get(IWorkspaceService);
+    const testUri = new URI(workspaceService.workspace.uri);
+    fileChangeWatcher.onFilesChanged.mockImplementation((fileChangeHandle) => {
+      fileChangeHandle([
+        {
+          uri: testUri.resolve('test_0'),
+          type: FileChangeType.UPDATED,
+        },
+        {
+          uri: testUri.resolve('test_1'),
+          type: FileChangeType.ADDED,
+        },
+        {
+          uri: testUri.resolve('test_2'),
+          type: FileChangeType.DELETED,
+        },
+      ]);
+      return Disposable.create(() => {});
+    });
+    await fileTreeService.watchFilesChange(testUri);
+    expect(fileChangeWatcher.onFilesChanged).toBeCalledTimes(1);
+    expect(mockFileServiceClient.watchFileChanges).toBeCalled();
+    await fileTreeService.flushEventQueue();
+    done();
+  });
+
+  it('Re-watch should be work while re-connect', async (done) => {
+    const fileTreeContribution = injector.get(FileTreeContribution);
+    const testUri = new URI('file://userhome/test.js');
+    await fileTreeService.watchFilesChange(testUri);
+    await fileTreeContribution.onReconnect();
+    expect(fileChangeWatcher.onFilesChanged).toBeCalledTimes(2);
+    done();
+  });
+
+  it('Commands should be work', () => {
+    const testUri = new URI('file://userhome/test.js');
+    // openAndFixedFile
+    const mockOpenResource = jest.fn();
+    injector.mockCommand(EDITOR_COMMANDS.OPEN_RESOURCE.id, mockOpenResource);
+    fileTreeService.openAndFixedFile(testUri);
+    expect(mockOpenResource).toBeCalledWith(testUri,  { disableNavigate: true, preview: false });
+    // OpenToTheSide
+    fileTreeService.openToTheSide(testUri);
+    expect(mockOpenResource).toBeCalledWith(testUri,  { disableNavigate: true, preview: false });
+    // compare
+    const mockCompare = jest.fn();
+    injector.mockCommand(EDITOR_COMMANDS.COMPARE.id, mockCompare);
+    fileTreeService.compare(testUri, testUri);
+    expect(mockCompare).toBeCalledWith({
+      original: testUri,
+      modified: testUri,
+    });
+    // toggleFilterMode
+    const mockLocation = jest.fn();
+    injector.mockCommand(FILE_COMMANDS.LOCATION.id, mockLocation);
+    fileTreeService.toggleFilterMode();
+    // set filterMode to true
+    fileTreeService.toggleFilterMode();
+    expect(mockLocation).toBeCalledTimes(1);
+    // enableFilterMode
+    fileTreeService.enableFilterMode();
+    expect(fileTreeService.filterMode).toBeTruthy();
+    // locationToCurrentFile
+    fileTreeService.locationToCurrentFile();
+    expect(mockLocation).toBeCalledTimes(2);
+  });
+});
