de8dc76d0c1cf08d720f87198c46b57f998a455f
MMhunter <kengtou.mh@antfin.com>
2020-04-07 22:08:20 +0800

    fix: editor action tooltip 不再使用popover

--- packages/editor/src/browser/tab.view.tsx
+++ packages/editor/src/browser/tab.view.tsx
@@ -5,11 +5,10 @@ import { useInjectable } from '@ali/ide-core-browser/lib/react-hooks';
 import { IResource, ResourceService, IEditorGroup } from '../common';
 import * as styles from './editor.module.less';
 import classnames from 'classnames';
-import { getIcon, MaybeNull, IEventBus, getSlotLocation, ConfigContext, ResizeEvent, URI, localize, makeRandomHexString } from '@ali/ide-core-browser';
+import { getIcon, MaybeNull, IEventBus, getSlotLocation, ConfigContext, ResizeEvent, URI, localize } from '@ali/ide-core-browser';
 // TODO editor 不应该依赖main-layout
 import { Scroll } from './component/scroll/scroll';
 import { GridResizeEvent, IEditorActionRegistry } from './types';
-import { Popover, PopoverTriggerType, PopoverPosition } from '@ali/ide-components';
 
 const pkgName = require('../../package.json').name;
 
@@ -189,31 +188,26 @@ export const EditorActions = observer(({group, hasFocus}: {hasFocus: boolean, gr
         const item = visibleAction.item;
         const icon = <div className={classnames(styles.editor_action, item.iconClass)} title={item.title} key={item.title}
                     onClick={() => item.onClick(group.currentResource, group)} />;
-        if (!item.tip || !visibleAction.tipVisible) {
-          return icon;
-        } else {
-          return <Popover
-            id={'editor_actions_tip_' + makeRandomHexString(5)}
-            title={item.tip}
-            content={<div className={styles.editor_action_tip}>
+        return <div className={classnames(styles.editor_action_wrapper)} >
+          {icon}
+          {
+            (!item.tip || !visibleAction.tipVisible) ? null :
+            <div className={classnames(styles.editor_action_tip, item.tipClass)}>
+              {item.tip}
               <div className={classnames(styles.editor_action_tip_close, getIcon('close'))} onClick={() => visibleAction.closeTip()}></div>
-            </div>}
-            trigger={PopoverTriggerType.program}
-            display={true}
-            popoverClass={classnames(styles.editor_action_tip_wrapper, item.tipClass)}
-            position = {PopoverPosition.bottom}
-          >
-            {icon}
-          </Popover>;
-        }
+            </div>
+          }
+        </div>;
       }) : null
     }
-    <div className={classnames(styles.editor_action, getIcon('ellipsis'))} title={localize('editor.moreActions')}
-      onClick={(event) => {
-        const { x, y } = event.nativeEvent;
-        editorActionRegistry.showMore(x, y, group);
-      }}
-    />
+    <div className={classnames(styles.editor_action_wrapper)} >
+      <div className={classnames(styles.editor_action, getIcon('ellipsis'))} title={localize('editor.moreActions')}
+        onClick={(event) => {
+          const { x, y } = event.nativeEvent;
+          editorActionRegistry.showMore(x, y, group);
+        }}
+      />
+    </div>
   </div>;
 });
 
