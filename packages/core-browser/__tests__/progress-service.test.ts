b66445cd2bef30c2b7509ab19292cd43e81b3059
ensorrow <zheyanglv@qq.com>
2020-04-14 17:41:34 +0800

    chore: progress test

---
+++ packages/core-browser/__tests__/progress-service.test.ts
@@ -0,0 +1,122 @@
+import { ProgressService } from '../src/progress/progress.service';
+import { MockInjector } from '../../../tools/dev-tool/src/mock-injector';
+import { createBrowserInjector } from '../../../tools/dev-tool/src/injector-helper';
+import { IProgressService } from '../src/progress';
+import { ProgressLocation, CommandRegistry } from '@ali/ide-core-common';
+import { StatusBarEntry } from '../src/services';
+
+// https://stackoverflow.com/questions/52177631/jest-timer-and-promise-dont-work-well-settimeout-and-async-function
+function flushPromises() {
+  return new Promise((resolve) => setImmediate(resolve));
+}
+
+describe('progress service test', () => {
+  let service: ProgressService;
+  let injector: MockInjector;
+  const mockEntryMap: Map<string, StatusBarEntry> = new Map();
+
+  beforeAll(() => {
+    injector = createBrowserInjector([]);
+    injector.addProviders(
+      {
+        token: IProgressService,
+        useClass: ProgressService,
+      },
+    );
+    service = injector.get(IProgressService);
+    jest.useFakeTimers();
+    const commandRegistry: CommandRegistry = injector.get(CommandRegistry);
+    commandRegistry.registerCommand({id: 'statusbar.addElement'}, {
+      execute: (id: string, entry: StatusBarEntry) => {
+        mockEntryMap.set(id, entry);
+        return {
+          dispose: () => mockEntryMap.delete(id),
+          update: (entry: StatusBarEntry) => {
+            const original = mockEntryMap.get(id);
+            mockEntryMap.set(id, {
+              ...original,
+              ...entry,
+            });
+          },
+        };
+      },
+    });
+  });
+
+  it('progress with window should work', async (done) => {
+    service.withProgress({
+      location: ProgressLocation.Window,
+      title: 'progressTitle',
+    }, async (progress) => {
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      progress.report({
+        message: 'progressMessage',
+      });
+      await new Promise((resolve) => setTimeout(resolve, 200));
+    });
+    expect(mockEntryMap.get('status.progress')).toBeUndefined();
+    // 低于150ms的进度不展示
+    jest.advanceTimersByTime(150);
+    expect(mockEntryMap.get('status.progress')!.text).toEqual(`$(sync~spin) progressTitle`);
+    jest.advanceTimersByTime(50);
+    await flushPromises();
+    expect(mockEntryMap.get('status.progress')!.text).toEqual(`$(sync~spin) progressTitle: progressMessage`);
+    jest.advanceTimersByTime(200);
+    await flushPromises();
+    expect(mockEntryMap.get('status.progress')).toBeUndefined();
+    done();
+  });
+
+  it('progress with indicator should work', async (done) => {
+    service.registerProgressIndicator('scm');
+    service.withProgress({
+      location: ProgressLocation.Scm,
+      total: 100,
+      delay: 200,
+    }, async (progress) => {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      progress.report({
+        increment: 20,
+      });
+      await new Promise((resolve) => setTimeout(resolve, 200));
+      progress.report({
+        increment: 30,
+      });
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    });
+    const indicator = service.getIndicator('scm');
+    expect(indicator?.progressModel.show).toBeFalsy();
+    expect(indicator?.progressModel.total).toEqual(100);
+    jest.advanceTimersByTime(200);
+    expect(indicator?.progressModel.show).toBeTruthy();
+    jest.advanceTimersByTime(100);
+    await flushPromises();
+    expect(indicator?.progressModel.worked).toEqual(20);
+    jest.advanceTimersByTime(200);
+    await flushPromises();
+    expect(indicator?.progressModel.worked).toEqual(50);
+    jest.advanceTimersByTime(100);
+    await flushPromises();
+    expect(indicator?.progressModel.fade).toBeTruthy();
+    expect(indicator?.progressModel.show).toBeTruthy();
+    expect(indicator?.progressModel.worked).toEqual(100);
+    // fade out when has progress
+    jest.advanceTimersByTime(800);
+    expect(indicator?.progressModel.show).toBeFalsy();
+    done();
+  });
+
+  it('infinite progress with indicator should work', async (done) => {
+    service.registerProgressIndicator('explorer');
+    service.withProgress({
+      location: ProgressLocation.Explorer,
+    }, (_) => new Promise((resolve) => setTimeout(resolve, 400)));
+    const indicator = service.getIndicator('explorer');
+    expect(indicator?.progressModel.total).toBeUndefined();
+    expect(indicator?.progressModel.show).toBeTruthy();
+    jest.advanceTimersByTime(400);
+    await flushPromises();
+    expect(indicator?.progressModel.show).toBeFalsy();
+    done();
+  });
+});
