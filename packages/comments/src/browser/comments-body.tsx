c5e83766610e51e08109e343b9199f20bce904f8
hacke2 <xinglong.wangwxl@alibaba-inc.com>
2020-04-20 20:51:48 +0800

    fix: 使用 shadow-dom 渲染评论内容

--- packages/comments/src/browser/comments-body.tsx
+++ packages/comments/src/browser/comments-body.tsx
@@ -1,17 +1,43 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import * as marked from 'marked';
-import * as styles from './comments.module.less';
+import { markdownCss } from './markdown.style';
+
+const ShadowContent = ({ root, children }) => ReactDOM.createPortal(children, root);
 
 export const CommentsBody: React.FC<{
   body: string;
 }> = React.memo(({ body }) => {
+  const shadowRootRef = React.useRef<HTMLDivElement | null>(null);
+  const [shadowRoot, setShadowRoot] = React.useState<ShadowRoot | null>(null);
+
+  React.useEffect(() => {
+    if (shadowRootRef.current) {
+      const shadowRootElement = shadowRootRef.current.attachShadow({ mode: 'open' });
+      if (!shadowRoot) {
+        setShadowRoot(shadowRootElement);
+      }
+    }
+  }, []);
   return (
-    <div
-      className={styles.comment_item_body}
-      dangerouslySetInnerHTML={{
-        __html: marked(body, {
-          breaks: true,
-        }),
-      }}></div>
+    <div ref={shadowRootRef}>
+      {shadowRoot && (
+        <ShadowContent root={shadowRoot}>
+          <style>{markdownCss}</style>
+          <div dangerouslySetInnerHTML={{
+            __html: marked(body, {
+              gfm: true,
+              tables: true,
+              breaks: false,
+              pedantic: false,
+              sanitize: true,
+              smartLists: true,
+              smartypants: false,
+            }),
+          }}></div>
+        </ShadowContent>
+      )}
+    </div>
+
   );
 });
