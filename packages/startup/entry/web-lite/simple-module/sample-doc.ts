65a059f2dfb0a6e580b72fa693331a17534ce558
vagusxl <vagusxl@gmail.com>
2020-05-27 20:23:59 +0800

    feat: basic doc content provider

--- packages/startup/entry/web-lite/simple-module/sample-doc.ts
+++ packages/startup/entry/web-lite/simple-module/sample-doc.ts
@@ -1,30 +1,24 @@
-import { IEditorDocumentModelContentProvider, IResourceProvider } from '@ali/ide-editor/lib/browser';
-import { Emitter, Event, URI } from '@ali/ide-core-common';
-
-export class SampleDocContentProvider implements IEditorDocumentModelContentProvider {
-  handlesScheme(scheme: string) {
-    return scheme === 'file';
-  }
-
-  provideEditorDocumentModelContent(uri: URI, encoding?: string | undefined) {
-    return uri.toString() + ' mock content provider';
-  }
-
-  isReadonly(uri: URI) {
-    // todo
-    return true;
-  }
-
-  private _onDidChangeTestContent = new Emitter<URI>();
-  public onDidChangeContent: Event<URI> = this._onDidChangeTestContent.event;
-}
+import { IResourceProvider } from '@ali/ide-editor/lib/browser';
+import { URI } from '@ali/ide-core-common';
 
 // TODO: use `FileSystemResourceProvider` instead when ready
 export class SampleResourceProvider implements IResourceProvider {
   readonly scheme = 'file';
   provideResource(uri: URI) {
     return {
-      name: uri.toString(),
+      name: uri.codeUri.path,
+      icon: '', // 依赖 labelService
+      uri,
+      metadata: null,
+    };
+  }
+}
+
+export class AntcodeResourceProvider implements IResourceProvider {
+  readonly scheme = 'antcode';
+  provideResource(uri: URI) {
+    return {
+      name: uri.codeUri.path,
       icon: '', // 依赖 labelService
       uri,
       metadata: null,
--- packages/startup/webpack.lite.config.js
+++ packages/startup/webpack.lite.config.js
@@ -1,2 +1,30 @@
 const { createWebpackConfig } = require('@ali/ide-dev-tool/src/webpack');
-module.exports = createWebpackConfig(__dirname, require('path').join(__dirname, 'entry/web-lite/app.tsx'));
+const webpack = require('webpack');
+
+module.exports = createWebpackConfig(
+  __dirname,
+  require('path').join(__dirname, 'entry/web-lite/app.tsx'),
+  {
+    devServer: {
+      proxy: {
+        '/code-service': {
+          target: 'https://code.alipay.com',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/code-service': '/api'
+          },
+          headers: {
+            'private-token': process.env.ANTCODE_SK || '',
+            host: 'code.alipay.com',
+            origin: 'https://code.alipay.com',
+          },
+        },
+      },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.SCM_PLATFORM': JSON.stringify(process.env.SCM_PLATFORM),
+      }),
+    ]
+  },
+);
