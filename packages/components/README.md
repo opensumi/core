19d16d0fd6f9a919c217d02df84c5d727a1d2a9f
vagusxl <vagusxl@gmail.com>
2020-07-30 17:59:02 +0800

    feat: support createFromIconfontCN

--- packages/components/README.md
+++ packages/components/README.md
@@ -1,5 +1,57 @@
 # @ali/ide-components
 
+## Usage
+
+### Icon/createFromIconfontCN
+```tsx
+import * as React from 'react';
+
+import { createFromIconfontCN } from '@ali/ide-components/lib/icon/iconfont-cn';
+import { Icon } from '@ali/ide-components/lib/icon';
+
+type IconFontMap =
+  'icon-javascript'
+  | 'icon-java'
+  | 'icon-shoppingcart'
+  | 'icon-python';
+
+const IconFont = createFromIconfontCN<IconFontMap>({
+  scriptUrl: [
+    '//at.alicdn.com/t/font_1788044_0dwu4guekcwr.js', // icon-javascript, icon-java, icon-shoppingcart (overrided)
+    '//at.alicdn.com/t/font_1788592_a5xf2bdic3u.js', // icon-shoppingcart, icon-python
+  ],
+});
+
+export const Sample = () => (
+  <div className='icons-list'>
+    <IconFont size='large' icon='icon-javascript' />
+    <IconFont icon='icon-java' onClick={() => console.log('icon java clicked')} />
+    <IconFont loading icon='icon-shoppingcart' />
+    <IconFont disabled size='large' icon='icon-python' />
+    <Icon icon='explorer' />
+    {/* <Icon icon='fanhui' /> */}
+    <Icon icon='shangchuan' />
+  </div>
+);
+```
+
+在 ide-framework 中可通过 appConfig [传入新 Icon/覆盖已存在的 Icon](https://yuque.antfin-inc.com/ide-framework/lsxfi3/mddcy3#m727J)
+```js
+renderApp({
+  // ...
+  iconStyleSheets: [
+    {
+      iconMap: {
+        explorer: 'fanhui', // 这种是覆盖，因此在 Icon 中通过 `explorer` 去取
+        shangchuan: 'shangchuan', // 这种是新增，因此在 Icon 中通过 `shangchuan` 去取
+      },
+      prefix: 'tbe tbe-',
+      cssPath: '//at.alicdn.com/t/font_403404_1qiu0eed62f.css',
+    },
+  ],
+});
+```
+
 ## Attention
 * components 包的定位是直接能对外使用，不限定于在 ide-framework 运行环境的组件
 * 因此需要注意的是，放到这里的组件应该是纯组件
---
+++ packages/components/src/icon/iconfont-cn.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import * as clx from 'classnames';
+import { Icon, IconProp } from './icon';
+import { getIconShapeClxList } from './util';
+
+// source code from '@ant-design/icons'
+const customCache = new Set<string>();
+
+export interface CustomIconOptions {
+  scriptUrl?: string | string[];
+  extraCommonProps?: { [key: string]: any };
+}
+
+/**
+ * 去掉 iconClass 属性
+ * 保留的 icon 属性是为了保持 API 一致性，但是不会传递下去
+ */
+export type IconFontProps<T> = Omit<IconProp<T>, 'iconClass'>;
+
+function isValidCustomScriptUrl(scriptUrl: string): boolean {
+  return Boolean(
+    typeof scriptUrl === 'string'
+      && scriptUrl.length
+      && !customCache.has(scriptUrl),
+  );
+}
+
+function createScriptUrlElements(scriptUrls: string[], index: number = 0): void {
+  const currentScriptUrl = scriptUrls[index];
+  if (isValidCustomScriptUrl(currentScriptUrl)) {
+    const script = document.createElement('script');
+    script.setAttribute('src', currentScriptUrl);
+    script.setAttribute('data-namespace', currentScriptUrl);
+    if (scriptUrls.length > index + 1) {
+      script.onload = () => {
+        createScriptUrlElements(scriptUrls, index + 1);
+      };
+      script.onerror = () => {
+        createScriptUrlElements(scriptUrls, index + 1);
+      };
+    }
+    customCache.add(currentScriptUrl);
+    document.body.appendChild(script);
+  }
+}
+
+const svgBaseProps = {
+  width: '1em',
+  height: '1em',
+  fill: 'currentColor',
+};
+
+export function createFromIconfontCN<T>(options: CustomIconOptions = {}): React.SFC<IconFontProps<T>> {
+  const { scriptUrl, extraCommonProps = {} } = options;
+
+  /**
+   * DOM API required.
+   * Make sure in browser environment.
+   * The Custom Icon will create a <script/>
+   * that loads SVG symbols and insert the SVG Element into the document body.
+   */
+  if (
+    scriptUrl &&
+    typeof document !== 'undefined' &&
+    typeof window !== 'undefined' &&
+    typeof document.createElement === 'function'
+  ) {
+    if (Array.isArray(scriptUrl)) {
+      // 因为iconfont资源会把svg插入before，所以前加载相同type会覆盖后加载，为了数组覆盖顺序，倒叙插入
+      createScriptUrlElements(scriptUrl.reverse());
+    } else {
+      createScriptUrlElements([scriptUrl]);
+    }
+  }
+
+  const Iconfont = React.forwardRef<HTMLSpanElement, IconFontProps<T>>((props, ref) => {
+    const { icon, children, rotate, anim, fill, className = '', ...restProps } = props;
+    const iconShapeOptions = { rotate, anim, fill };
+
+    // children > icon
+    let content: React.ReactNode = null;
+    if (icon) {
+      content = (
+        <svg {...svgBaseProps} focusable='false'>
+          <use xlinkHref={`#${icon}`} />
+        </svg>
+      );
+    }
+    if (children) {
+      content = children;
+    }
+
+    const iconShapeClx = getIconShapeClxList(iconShapeOptions);
+    return (
+      <Icon {...extraCommonProps} {...restProps} className={clx(className, iconShapeClx)} ref={ref}>
+        {content}
+      </Icon>
+    );
+  });
+
+  Iconfont.displayName = 'Iconfont';
+
+  return Iconfont;
+}
