f1e74ec6a773a43981fab331cd6b953d0a54c9dd
vagusxl <vagusxl@gmail.com>
2020-08-10 19:59:09 +0800

    chore: update cdn css

--- packages/core-browser/src/style/icon/ide-iconfont.ts
+++ packages/core-browser/src/style/icon/ide-iconfont.ts
@@ -1,3 +1,3 @@
-export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/font_1432262_n4rpkq5bbjq.css';
+export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/font_1432262_gxosxbfr3ug.css';
 
 export const IDE_ICONFONT_CN_JS = IDE_ICONFONT_CN_CSS.replace(/\.css$/, '.js');
