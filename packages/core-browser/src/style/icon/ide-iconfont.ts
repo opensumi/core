2399b7d8444a0024459e5016880bf3049fdd1ada
vagusxl <vagusxl@gmail.com>
2020-08-26 16:28:14 +0800

    feat: update icons

--- packages/core-browser/src/style/icon/ide-iconfont.ts
+++ packages/core-browser/src/style/icon/ide-iconfont.ts
@@ -1,3 +1,3 @@
-export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/font_1432262_gxosxbfr3ug.css';
+export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/font_1432262_z0juat8ly2g.css';
 
 export const IDE_ICONFONT_CN_JS = IDE_ICONFONT_CN_CSS.replace(/\.css$/, '.js');
