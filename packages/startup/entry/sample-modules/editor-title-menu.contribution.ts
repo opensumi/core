d139c549c3ca5f175d48a37320920e0f89786afd
vagusxl <vagusxl@gmail.com>
2020-10-14 10:08:51 +0800

    fix: normalize style files

--- packages/startup/entry/sample-modules/editor-title-menu.contribution.ts
+++ packages/startup/entry/sample-modules/editor-title-menu.contribution.ts
@@ -1,6 +1,9 @@
 // tslint:disable:no-console
-import { CommandRegistry, CommandContribution, Domain, getIcon } from '@ali/ide-core-browser';
+import { Autowired } from '@ali/common-di';
+import { Disposable, IEventBus } from '@ali/ide-core-common';
+import { ClientAppContribution, IContextKey, IContextKeyService, CommandRegistry, CommandContribution, Domain, getIcon } from '@ali/ide-core-browser';
 import { NextMenuContribution, IMenuRegistry, MenuId } from '@ali/ide-core-browser/lib/menu/next';
+import { EditorGroupChangeEvent } from '@ali/ide-editor/lib/browser';
 
 class WebSCMCommands {
   static Edit = {
@@ -12,8 +15,35 @@ class WebSCMCommands {
   };
 }
 
-@Domain(CommandContribution, NextMenuContribution)
-export class EditorTitleMenuContribution implements CommandContribution, NextMenuContribution {
+const toggledCtx = 'test.ctx.toggled';
+
+@Domain(CommandContribution, NextMenuContribution, ClientAppContribution)
+export class EditorTitleMenuContribution extends Disposable implements CommandContribution, NextMenuContribution, ClientAppContribution {
+  @Autowired(IContextKeyService)
+  private readonly globalCtxKeyService: IContextKeyService;
+
+  @Autowired(IEventBus)
+  private readonly eventBus: IEventBus;
+
+  private toggledCtxKey: IContextKey<boolean>;
+  constructor() {
+    super();
+    this.toggledCtxKey = this.globalCtxKeyService.createKey<boolean>(toggledCtx, false);
+  }
+
+  onStart() {
+    this.addDispose(
+      // 编辑器切换时更新高亮的 contextkey
+      this.eventBus.on(EditorGroupChangeEvent, (e) => {
+        const { newResource } = e.payload;
+        if (!newResource || !newResource.uri) {
+          return;
+        }
+        this.toggledCtxKey.set(newResource.uri.path.toString().endsWith('ts'));
+      }),
+    );
+  }
+
   registerNextMenus(menus: IMenuRegistry): void {
     menus.registerMenuItem(MenuId.EditorTitle, {
       command: {
@@ -31,6 +61,7 @@ export class EditorTitleMenuContribution implements CommandContribution, NextMen
       },
       iconClass: getIcon('save-all'),
       group: 'navigation',
+      toggledWhen: toggledCtx,
     });
   }
 
@@ -44,6 +75,7 @@ export class EditorTitleMenuContribution implements CommandContribution, NextMen
     commands.registerCommand(WebSCMCommands.Save, {
       execute: async (...args) => {
         console.log(args, 'args');
+        this.toggledCtxKey.set(!this.toggledCtxKey.get());
       },
     });
   }
