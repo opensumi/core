9104433db0e6824c48eeb8898f1470ea6962f0f4
hacke2 <xinglong.wangwxl@alibaba-inc.com>
2020-11-09 22:08:09 +0800

    fix: quickopen 实现 canPickMany 参数

---
+++ packages/kaitian-extension/__tests__/hosted/api/vscode/ext.host.quickopen.test.ts
@@ -0,0 +1,66 @@
+
+import { Emitter, Disposable } from '@ali/ide-core-common';
+import { MainThreadAPIIdentifier, ExtHostAPIIdentifier } from '../../../../src/common/vscode';
+import { RPCProtocol } from '@ali/ide-connection';
+import { createBrowserInjector } from '../../../../../../tools/dev-tool/src/injector-helper';
+import { ExtHostQuickOpen } from '../../../../src/hosted/api/vscode/ext.host.quickopen';
+import { MainThreadQuickOpen } from '../../../../src/browser/vscode/api/main.thread.quickopen';
+import { mockService } from '../../../../../../tools/dev-tool/src/mock-injector';
+import { QuickPickService } from '@ali/ide-quick-open';
+import { QuickTitleBar } from '@ali/ide-quick-open/lib/browser/quick-title-bar';
+
+const emitterA = new Emitter<any>();
+const emitterB = new Emitter<any>();
+
+const mockClientA = {
+  send: (msg) => emitterB.fire(msg),
+  onMessage: emitterA.event,
+};
+const mockClientB = {
+  send: (msg) => emitterA.fire(msg),
+  onMessage: emitterB.event,
+};
+
+const rpcProtocolExt = new RPCProtocol(mockClientA);
+const rpcProtocolMain = new RPCProtocol(mockClientB);
+
+let extHost: ExtHostQuickOpen;
+let mainThread: MainThreadQuickOpen;
+
+describe(__filename, () => {
+  const injector = createBrowserInjector([]);
+
+  injector.addProviders({
+    token: QuickPickService,
+    useValue: mockService({
+      // 默认返回第一个
+      show: () => 0,
+    }),
+  }, {
+    token: QuickTitleBar,
+    useValue: mockService({
+      onDidTriggerButton: () => Disposable.NULL,
+    }),
+  });
+  const extHostWorkspace = mockService({});
+  extHost = new ExtHostQuickOpen(rpcProtocolExt, extHostWorkspace);
+  rpcProtocolExt.set(ExtHostAPIIdentifier.ExtHostQuickOpen, extHost);
+
+  mainThread = rpcProtocolMain.set(MainThreadAPIIdentifier.MainThreadQuickOpen, injector.get(MainThreadQuickOpen, [rpcProtocolMain]));
+
+  afterAll(() => {
+    mainThread.dispose();
+  });
+
+  it('get quickpick item', async () => {
+    const item = await extHost.showQuickPick(['a', 'b']);
+    expect(item).toBe('a');
+  });
+
+  it('get quickpick item with canPickMany', async () => {
+    const item = await extHost.showQuickPick(['a', 'b'], {
+      canPickMany: true,
+    });
+    expect(item).toStrictEqual(['a']);
+  });
+});
