532ee7b61182f61d6263af933f71f091190fb7b0
yantze <yutian.yz@alibaba-inc.com>
2020-12-25 17:21:00 +0800

    fix: 修复 electron 窗口特殊情况缩小失效

--- packages/core-common/src/mocks/electron/ipcRenderer.ts
+++ packages/core-common/src/mocks/electron/ipcRenderer.ts
@@ -1,30 +1,33 @@
 import { EventEmitter } from 'events';
 
-export class MockedElectronIpcRenderer  extends EventEmitter implements Electron.IpcRenderer {
-  
+export class MockedElectronIpcRenderer extends EventEmitter implements Electron.IpcRenderer {
+
   invoke(channel: string, ...args: any[]): Promise<any> {
     throw new Error("Method not implemented.");
   }
-  
+
   eventNames() {
     return super.eventNames().map(s => s.toString());
   }
 
-  public targetHost:EventEmitter = new EventEmitter();
+  public targetHost: EventEmitter = new EventEmitter();
 
   send(channel: string, ...args: any[]): void {
-    this.targetHost.emit('send', {channel}, args);
-  }  
-  
+    this.targetHost.emit('send', { channel }, args);
+  }
+
   sendSync(channel: string, ...args: any[]) {
-    this.targetHost.emit('sendSync', {channel}, args);
+    this.targetHost.emit('sendSync', { channel }, args);
   }
   sendTo(webContentsId: number, channel: string, ...args: any[]): void {
-    this.targetHost.emit('sendTo', {webContentsId, channel}, args);
+    this.targetHost.emit('sendTo', { webContentsId, channel }, args);
   }
-  
+
   sendToHost(channel: string, ...args: any[]): void {
     this.targetHost.emit('sendToHost', channel, ...args);
+  }
+
+  postMessage(channel: string, message: any, transfer?: MessagePort[]): void {
   }
 
 }
--- packages/file-scheme/src/browser/external.view.tsx
+++ packages/file-scheme/src/browser/external.view.tsx
@@ -6,11 +6,11 @@ import { IElectronMainUIService } from '@ali/ide-core-common/lib/electron';
 import { INJECTOR_TOKEN } from '@ali/common-di';
 
 export const BinaryEditorComponent: ReactEditorComponent<null> = (props) => {
-  const src: string = props.resource.uri.toString();
+  const srcPath = props.resource.uri.codeUri.fsPath;
   const injector = useInjectable(INJECTOR_TOKEN);
 
   return (<div className={styles.external}>
     {localize('editor.cannotOpenBinary')}
-    { isElectronRenderer() ? <a onClick={() => injector.get(IElectronMainUIService).openItem(props.resource.uri.codeUri.fsPath) }>{localize('editor.openExternal')}</a> : null }
+    { isElectronRenderer() ? <a onClick={() => injector.get(IElectronMainUIService).openPath(srcPath) }>{localize('editor.openExternal')}</a> : null }
   </div>);
 };
