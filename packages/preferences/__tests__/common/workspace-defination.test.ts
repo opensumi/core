12349f0ac7b01cbe4e763a402d2db6e7def45505
erha19 <danwu.wdw@alibaba-inc.com>
2021-02-26 10:24:31 +0800

    PullRequest: 76 fix: 修复工作区路径拼接错误问题

---
+++ packages/preferences/__tests__/common/workspace-defination.test.ts
@@ -0,0 +1,90 @@
+import { URI, FileStat } from '@ali/ide-core-browser';
+import { KAITIAN_MULTI_WORKSPACE_EXT, WorkspaceData } from '@ali/ide-workspace';
+
+describe('WorkspaceData methods', () => {
+
+  it('is method should be work', () => {
+    const workspaceData = {
+      folders: [{
+        path: 'folder1',
+      }],
+      settings: {},
+    };
+    expect(WorkspaceData.is(workspaceData)).toBeTruthy();
+    const unknownData = {
+      test: [],
+    };
+    expect(WorkspaceData.is(unknownData)).toBeFalsy();
+  });
+
+  it('buildWorkspaceData method should be work', () => {
+    const folders = [URI.file('home').toString()];
+    const settings = {
+      hello: 'world',
+    };
+    let workspaceData = WorkspaceData.buildWorkspaceData(folders, settings);
+
+    expect(workspaceData.folders[0].path).toBe(URI.file('home').toString());
+    expect(workspaceData.settings!['hello']).toBe('world');
+    const statFolders = [{
+      uri: URI.file('home').toString(),
+      isDirectory: true,
+      lastModification: new Date().getTime(),
+    } as FileStat];
+    workspaceData = WorkspaceData.buildWorkspaceData(statFolders, settings);
+
+    expect(workspaceData.folders[0].path).toBe(URI.file('home').toString());
+    expect(workspaceData.settings!['hello']).toBe('world');
+  });
+
+  it('transformToRelative method should be work', () => {
+    const workspaceFile = {
+      uri: URI.file('home').resolve(`workspace.${KAITIAN_MULTI_WORKSPACE_EXT}`).toString(),
+      isDirectory: false,
+      lastModification: new Date().getTime(),
+    };
+    const workspaceData = {
+      folders: [
+        {
+          path: URI.file('home').resolve('folder1').toString(),
+        },
+      ],
+      settings: {},
+    };
+    const data = WorkspaceData.transformToRelative(workspaceData as WorkspaceData, workspaceFile);
+
+    expect(data.folders[0].path).toBe('folder1');
+  });
+
+  it('transformToAbsolute method should be work', () => {
+    const workspaceFile = {
+      uri: URI.file('home').resolve(`workspace.${KAITIAN_MULTI_WORKSPACE_EXT}`).toString(),
+      isDirectory: false,
+      lastModification: new Date().getTime(),
+    };
+    const workspaceData = {
+      folders: [
+        {
+          path: 'folder1',
+        },
+      ],
+      settings: {},
+    };
+    let data = WorkspaceData.transformToAbsolute(workspaceData as WorkspaceData, workspaceFile);
+
+    expect(data.folders[0].path).toBe(URI.file('home').resolve('folder1').toString());
+
+    const dotWorkspaceData = {
+      folders: [
+        {
+          path: '.',
+        },
+      ],
+      settings: {},
+    };
+    data = WorkspaceData.transformToAbsolute(dotWorkspaceData as WorkspaceData, workspaceFile);
+
+    expect(data.folders[0].path).toBe(URI.file('home').toString());
+  });
+
+});
