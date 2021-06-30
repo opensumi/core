0e851670003bf81a4c460539639969151df0c4c8
Aaaaash <xubing.bxb@alibaba-inc.com>
2021-06-30 16:43:27 +0800

    PullRequest: 490 fix: 修改插件开发模式下的状态栏背景色

--- packages/theme/src/common/color-tokens/custom/extension.ts
+++ packages/theme/src/common/color-tokens/custom/extension.ts
@@ -1,4 +1,8 @@
 import { localize } from '@ali/ide-core-common';
 import { registerColor } from '../../color-registry';
 
-export const ktStatusBarExtensionDebugginBackground = registerColor('kt.statusBar.extensionDebuggingBackground', { dark: '#FF8F5C', 'light': '#FF8F5C', hc: '#FF8F5C' }, localize('Background of StatusBar while extension is debugging.'));
+export const ktStatusBarExtensionDebugginBackground = registerColor(
+  'kt.statusBar.extensionDebuggingBackground',
+  { dark: '#CC6633', light: '#CC6633', hc: '#CC6633' },
+  localize('Background of StatusBar while extension is debugging.'),
+);
