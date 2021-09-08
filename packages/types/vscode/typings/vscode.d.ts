a24999dfe5a5ac2efcb308d7483b2afbe8899a4a
erha19 <danwu.wdw@alibaba-inc.com>
2021-09-08 16:01:43 +0800

    PullRequest: 716 feat: 适配 VSCode 1.55.0 版本中 TreeView 相关 API

--- packages/types/vscode/typings/vscode.d.ts
+++ packages/types/vscode/typings/vscode.d.ts
@@ -1522,13 +1522,36 @@ declare module 'vscode' {
      * @return Parent of `element`.
      */
     getParent?(element: T): ProviderResult<T>;
+
+    /**
+		 * Called on hover to resolve the {@link TreeItem.tooltip TreeItem} property if it is undefined.
+		 * Called on tree item click/open to resolve the {@link TreeItem.command TreeItem} property if it is undefined.
+		 * Only properties that were undefined can be resolved in `resolveTreeItem`.
+		 * Functionality may be expanded later to include being called to resolve other missing
+		 * properties on selection and/or on open.
+		 *
+		 * Will only ever be called once per TreeItem.
+		 *
+		 * onDidChangeTreeData should not be triggered from within resolveTreeItem.
+		 *
+		 * *Note* that this function is called when tree items are already showing in the UI.
+		 * Because of that, no property that changes the presentation (label, description, etc.)
+		 * can be changed.
+		 *
+		 * @param item Undefined properties of `item` should be set then `item` should be returned.
+		 * @param element The object associated with the TreeItem.
+		 * @param token A cancellation token.
+		 * @return The resolved tree item or a thenable that resolves to such. It is OK to return the given
+		 * `item`. When no result is returned, the given `item` will be used.
+		 */
+		resolveTreeItem?(item: TreeItem, element: T, token: CancellationToken): ProviderResult<TreeItem>;
   }
 
   export class TreeItem {
     /**
-     * A human-readable string describing this item. When `falsy`, it is derived from [resourceUri](#TreeItem.resourceUri).
-     */
-    label?: string;
+		 * A human-readable string describing this item. When `falsy`, it is derived from {@link TreeItem.resourceUri resourceUri}.
+		 */
+		label?: string | TreeItemLabel;
 
     /**
      * Optional id for the tree item that has to be unique across tree. The id is used to preserve the selection and expansion state of the tree item.
@@ -1593,26 +1616,24 @@ declare module 'vscode' {
      */
     contextValue?: string;
 
-    /**
-     * Accessibility information used when screen reader interacts with this tree item.
-     * Generally, a TreeItem has no need to set the `role` of the accessibilityInformation;
-     * however, there are cases where a TreeItem is not displayed in a tree-like way where setting the `role` may make sense.
-     */
+		/**
+		 * Accessibility information used when screen reader interacts with this tree item.
+		 * Generally, a TreeItem has no need to set the `role` of the accessibilityInformation;
+		 * however, there are cases where a TreeItem is not displayed in a tree-like way where setting the `role` may make sense.
+		 */
      accessibilityInformation?: AccessibilityInformation;
 
-    /**
-     * @param label A human-readable string describing this item
-     * @param collapsibleState [TreeItemCollapsibleState](#TreeItemCollapsibleState) of the tree item. Default is [TreeItemCollapsibleState.None](#TreeItemCollapsibleState.None)
-     */
-    /* tslint:disable-next-line */
-    constructor(label: string, collapsibleState?: TreeItemCollapsibleState);
-
-    /**
-     * @param resourceUri The [uri](#Uri) of the resource representing this item.
-     * @param collapsibleState [TreeItemCollapsibleState](#TreeItemCollapsibleState) of the tree item. Default is [TreeItemCollapsibleState.None](#TreeItemCollapsibleState.None)
-     */
-    /* tslint:disable-next-line */
-    constructor(resourceUri: Uri, collapsibleState?: TreeItemCollapsibleState);
+     /**
+      * @param label A human-readable string describing this item
+      * @param collapsibleState {@link TreeItemCollapsibleState} of the tree item. Default is {@link TreeItemCollapsibleState.None}
+      */
+     constructor(label: string | TreeItemLabel, collapsibleState?: TreeItemCollapsibleState);
+ 
+     /**
+      * @param resourceUri The {@link Uri} of the resource representing this item.
+      * @param collapsibleState {@link TreeItemCollapsibleState} of the tree item. Default is {@link TreeItemCollapsibleState.None}
+      */
+     constructor(resourceUri: Uri, collapsibleState?: TreeItemCollapsibleState);
   }
   /**
    * Represents a line of text, such as a line of source code.
