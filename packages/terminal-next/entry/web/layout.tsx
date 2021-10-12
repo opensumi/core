5a5539194cd9a5234c869d3644faa4900fe9ec29
Aaaaash <xubing.bxb@alibaba-inc.com>
2021-10-12 09:50:26 +0800

    PullRequest: 830 chore: 清理一些没用的 entry 代码

--- packages/terminal-next/entry/web/layout.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { BoxPanel } from '@ali/ide-core-browser/lib/components';
-import { SlotRenderer } from '@ali/ide-core-browser/lib/react-providers';
-
-export function DefaultLayout() {
-  return <BoxPanel direction='top-to-bottom'>
-    <SlotRenderer flex={1} slot='terminal' />
-  </BoxPanel>;
-}
