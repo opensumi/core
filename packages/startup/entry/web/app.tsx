7bc5d16faa651c77dad9ee2e978abddf9f2254bd
Aaaaash <xubing.bxb@alibaba-inc.com>
2021-10-12 11:46:06 +0800

    PullRequest: 823 feat: clean startup code

--- packages/startup/entry/web/app.tsx
+++ packages/startup/entry/web/app.tsx
@@ -2,31 +2,21 @@ import { setLocale } from '@ali/ide-monaco/lib/browser/monaco-localize';
 // 这里建议传实际 preferences 的设置项
 // 如果不传则默认会根据 PreferenceScope 的优先级从 LocalStorage 取值
 setLocale('zh-CN');
-
 import '@ali/ide-i18n';
-import { defaultConfig } from '@ali/ide-main-layout/lib/browser/default-config';
-import { renderApp } from './render-app';
-import { CommonBrowserModules } from '../../src/browser/common-modules';
-import { StartupModule } from '../../src/browser';
-
-// 引入公共样式文件
 import '@ali/ide-core-browser/lib/style/index.less';
-// 引入本地icon，不使用cdn版本，与useCdnIcon配套使用
-// import '@ali/ide-core-browser/lib/style/icon.less';
 import { ExpressFileServerModule } from '@ali/ide-express-file-server/lib/browser';
 import { SlotLocation } from '@ali/ide-core-browser';
+import { defaultConfig } from '@ali/ide-main-layout/lib/browser/default-config';
+
+import { renderApp } from './render-app';
+import { CommonBrowserModules } from '../../src/browser/common-modules';
 
 import '../styles.less';
-
-import { SampleModule } from '../sample-modules';
 
 renderApp({
   modules: [
     ...CommonBrowserModules,
     ExpressFileServerModule,
-    StartupModule,
-    // 示例代码
-    SampleModule,
   ],
   layoutConfig: {
     ...defaultConfig,
@@ -45,22 +35,9 @@ renderApp({
     'application.confirmExit': 'never',
     'editor.quickSuggestionsDelay': 100,
     'editor.quickSuggestionsMaxCount': 50,
-    // 'terminal.integrated.shellArgs.linux': ['-l'],
-    // 'editor.forceReadOnly': true,
   },
   defaultPanels: {
     'bottom': '@ali/ide-terminal-next',
     'right': '',
   },
-  // iconStyleSheets: [
-  //   {
-  //     iconMap: {
-  //       eye: 'shake',
-  //     },
-  //     prefix: 'tbe tbe-',
-  //     cssPath: '//at.alicdn.com/t/font_403404_1qiu0eed62f.css',
-  //   },
-  // ],
-  // allowSetDocumentTitleFollowWorkspaceDir: true,
-  // preferenceDirName: '.vsc',
 });
--- packages/startup/entry/web/render-app.tsx
+++ packages/startup/entry/web/render-app.tsx
@@ -1,5 +1,4 @@
 // tslint:disable no-console
-console.log('StartRender', Date.now());
 console.time('Render');
 import { Injector } from '@ali/common-di';
 import { ClientApp, IClientAppOpts } from '@ali/ide-core-browser';
@@ -9,23 +8,21 @@ export async function renderApp(opts: IClientAppOpts) {
   const injector = new Injector();
   opts.workspaceDir = opts.workspaceDir || process.env.WORKSPACE_DIR;
 
-  opts.extensionDir = opts.extensionDir || process.env.EXTENSION_DIR;
   opts.injector = injector;
-  opts.wsPath = process.env.WS_PATH || 'ws://127.0.0.1:8000'; // 代理测试地址: ws://127.0.0.1:8001
+  opts.extensionDir = opts.extensionDir || process.env.EXTENSION_DIR;
+  opts.wsPath = process.env.WS_PATH || 'ws://127.0.0.1:8000';
 
-  opts.extWorkerHost = opts.extWorkerHost || process.env.EXTENSION_WORKER_HOST; // `http://127.0.0.1:8080/kaitian/ext/worker-host.js`; // 访问 Host
-  // 使用不一样的host名称
-  const anotherHostName =
-    process.env.WEBVIEW_HOST ||
-    (window.location.hostname === 'localhost' ? '127.0.0.1' : 'localhost');
+  opts.extWorkerHost = opts.extWorkerHost || process.env.EXTENSION_WORKER_HOST;
+  const anotherHostName = process.env.WEBVIEW_HOST || (window.location.hostname === 'localhost' ? '127.0.0.1' : 'localhost');
   opts.webviewEndpoint = `http://${anotherHostName}:8899`;
-  // opts.editorBackgroundImage = 'https://img.alicdn.com/tfs/TB1Y6vriuL2gK0jSZFmXXc7iXXa-200-200.png';
-  opts.editorBackgroundImage =
-    'https://gw.alipayobjects.com/mdn/rms_104d0d/afts/img/A*j6JzRK8VubgAAAAAAAAAAAAAARQnAQ';
-  // 定制Layout
+  opts.editorBackgroundImage = 'https://gw.alipayobjects.com/mdn/rms_104d0d/afts/img/A*j6JzRK8VubgAAAAAAAAAAAAAARQnAQ';
   opts.layoutComponent = ToolbarActionBasedLayout;
 
-  // KAITIAN 本身应该不需要 loading 动画
+  opts.didRendered = () => {
+    // tslint:disable no-console
+    console.timeEnd('Render');
+  };
+
   const app = new ClientApp(opts);
 
   app.fireOnReload = (forcedReload: boolean) => {
