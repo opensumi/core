557922bab10cd88e7640c669879e38ea9d6d2c0e
Aaaaash <xubing.bxb@alibaba-inc.com>
2021-10-19 16:33:22 +0800

    PullRequest: 866 改进 input、select 等组件的默认样式

--- packages/theme/src/common/color-tokens/custom/input.ts
+++ packages/theme/src/common/color-tokens/custom/input.ts
@@ -1,8 +1,8 @@
 import { localize } from '@ali/ide-core-common';
 import { registerColor, contrastBorder } from '../../color-registry';
-import { inputOptionActiveBorder } from '../input';
+import { inputBorder, inputOptionActiveBorder } from '../input';
 
-export const ktInputBorder = registerColor('kt.input.border', { dark: '#00000000', light: '#00000000', hc: contrastBorder }, localize('ktInputBoxBorder', 'Input box border.'));
+export const ktInputBorder = registerColor('kt.input.border', { dark: inputBorder, light: inputBorder, hc: contrastBorder }, localize('ktInputBoxBorder', 'Input box border.'));
 export const ktInputDisableForeground = registerColor('kt.input.disableForeground', { dark: '#5F656B', light: '#5F656B', hc: null }, localize('ktInputDisableForeground', 'Input box disabled foreground color.'));
 export const ktInputDisableBackground = registerColor('kt.input.disableBackground', { dark: '#5F656B40', light: '#5F656B40', hc: null }, localize('ktInputDisableBackground', 'Input box disabled background color.'));
 export const ktInputSelectionBackground = registerColor('kt.input.selectionBackground', { dark: '#1A66AC', light: '#1A66AC', hc: null }, localize('Input Selection background color.'));
--- packages/theme/src/common/color-tokens/custom/select.ts
+++ packages/theme/src/common/color-tokens/custom/select.ts
@@ -1,10 +1,11 @@
 import { localize } from '@ali/ide-core-common';
 import { registerColor } from '../../color-registry';
 import { inputForeground, inputBackground } from '../input';
+import { settingsSelectBorder } from '../settings';
 
 export const ktSelectForeground = registerColor('kt.select.foreground', { dark: '#D7DBDE', light: inputForeground, hc: null }, localize('ktSelectForeground', 'Select Foreground color.'));
 export const ktSelectBackground = registerColor('kt.select.background', { dark: '#00000040', light: inputBackground, hc: null }, localize('ktSelectBackground', 'Select Background color.'));
-export const ktSelectBorder = registerColor('kt.select.border', { dark: '#00000040', light: '#FFFFFF40', hc: null }, localize('ktSelectBorder', 'Select Border color.'));
+export const ktSelectBorder = registerColor('kt.select.border', { dark: settingsSelectBorder, light: settingsSelectBorder, hc: null }, localize('ktSelectBorder', 'Select Border color.'));
 export const ktSelectPlaceholderForeground = registerColor('kt.select.placeholderForeground', { dark: '#5F656B', light: '#5F656B', hc: null }, localize('ktSelectPlceholder', 'Select Placeholder Foreground color.'));
 export const ktSelectDisableBackground = registerColor('kt.select.disableBackground', { dark: '#5F656B40', light: '#5F656B40', hc: null }, localize('ktSelectDisableBackground', 'Select Disable Background color.'));
 export const ktSelectDisableForeground = registerColor('kt.select.disableForeground', { dark: '#5F656B', light: '#5F656B', hc: null }, localize('ktSelectDisableForeground', 'Select Disable Foreground color.'));
@@ -14,7 +15,7 @@ export const ktSelectOptionActiveBorder = registerColor('kt.selectOption.activeB
 
 /* select dropdown */
 export const ktSelectDropdownForeground = registerColor('kt.selectDropdown.foreground', { dark: '#D7DBDE', light: inputForeground, hc: null }, localize('ktSelectDropdownForeground', 'Select Dropdown Foreground color.'));
-export const ktSelectDropdownBackground = registerColor('kt.selectDropdown.background', { dark: '#35393D', light: inputBackground, hc: null }, localize('ktSelectDropdownBackground', 'Select Dropdown Background color.'));
+export const ktSelectDropdownBackground = registerColor('kt.selectDropdown.background', { dark: inputBackground, light: inputBackground, hc: null }, localize('ktSelectDropdownBackground', 'Select Dropdown Background color.'));
 export const ktSelectDropdownHoverBackground = registerColor('kt.selectDropdown.hoverBackground', { dark: '#5F656B40', light: '#5F656B40', hc: null }, localize('ktSelectDropdownHoverBackground', 'Select Dropdown Hover Background color.'));
 export const ktSelectDropdownSelectionBackground = registerColor('kt.selectDropdown.selectionBackground', { dark: '#203E5A', light: '#5F656B40', hc: null }, localize('ktSelectDropdownSelectionBackground', 'Select Dropdown Selection Background color.'));
 export const ktSelectDropdownTeamForeground = registerColor('kt.selectDropdown.teamForeground', { dark: '#868C91', light: '#999999', hc: null }, localize('ktSelectDropdownSelectionBackground', 'Select Dropdown Selection Background color.'));
