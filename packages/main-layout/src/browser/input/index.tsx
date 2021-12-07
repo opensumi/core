3fefa5374aca61d15b3d9f9b281f55eb5a917ef5
Aaaaash <binshao54@gmail.com>
2021-12-07 20:24:28 +0800

    chore: improve welcome & input render perf

--- packages/main-layout/src/browser/input/index.tsx
+++ packages/main-layout/src/browser/input/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useRef, useEffect } from 'react';
-import { Input } from '@opensumi/ide-components';
+import React, { PropsWithChildren, useRef, useEffect } from 'react';
+import { IInputBaseProps, Input } from '@opensumi/ide-components';
 import { useInjectable } from '@opensumi/ide-core-browser/lib/react-hooks';
 import { IMainLayoutService } from '../../common';
 
-export const AutoFocusedInput = ({ containerId, ...inputProps }) => {
+export const AutoFocusedInput = ({
+  containerId,
+  ...inputProps
+}: PropsWithChildren<{ containerId: string } & IInputBaseProps>) => {
   const layoutService = useInjectable<IMainLayoutService>(IMainLayoutService);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const doFocus = React.useCallback(() => {
     if (inputRef && inputRef.current) {
-      setTimeout(() => {
-        inputRef.current?.focus();
-      }, 0);
+      queueMicrotask(() => inputRef.current?.focus());
     }
   }, [inputRef.current]);
 
