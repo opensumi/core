81409b2eeb07409101be2971f93db0a4a50b785a
Aaaaash <binshao54@gmail.com>
2021-12-17 17:48:55 +0800

    chore: remove cli engine

--- tools/cli-engine/README.md
+++
@@ -1,8 +0,0 @@
-# @opensumi/kaitian-cli-engine
-
-
-## Development
-
-```bash
-DEBUG_ENGINE_PATH=~/your-_folder/ide-framework/tools/cli-engine/lib kaitian dev
-```
--- tools/cli-engine/configs/webpack-util.js
+++
@@ -1,9 +0,0 @@
-exports.lessLoader = (mergeOptions = {}) => ({
-  loader: require.resolve('less-loader'),
-  options: {
-    lessOptions: {
-      javascriptEnabled: true,
-    },
-  },
-  ...mergeOptions,
-});
