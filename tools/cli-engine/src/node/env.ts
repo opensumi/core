01c317e2dbaa0f7aec6a941c99217307e2ea0705
大表哥 <xubing.bxb@alibaba-inc.com>
2021-12-20 10:17:30 +0800

    chore: remove cli engine (#146)

--- tools/cli-engine/src/node/env.ts
+++
@@ -1,6 +0,0 @@
-import * as ip from 'ip';
-import path from 'path';
-import os from 'os';
-
-export const DEV_PATH = path.join(os.homedir(), '.kaitian-dev');
-export const CLIENT_IP = ip.address();
--- tools/cli-engine/src/node/modules.ts
+++
@@ -1,5 +0,0 @@
-import { NodeModule, ConstructorOf } from '@opensumi/ide-core-node';
-import { CommonNodeModules } from '@opensumi/ide-startup/lib/node/common-modules';
-import { ExpressFileServerModule } from '@opensumi/ide-express-file-server';
-
-export const modules: ConstructorOf<NodeModule>[] = [...CommonNodeModules, ExpressFileServerModule];
