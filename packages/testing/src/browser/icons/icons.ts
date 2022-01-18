3c9e85ac8452a05e0ca4907accc8f7d38d624428
John <qingyi.xjh@antgroup.com>
2022-01-18 11:10:47 +0800

    feat: support simple testing decorations (#335)
    * feat: register testing editor contribution
    
    * feat: implement run single test decoration class
    
    * chore: provisional commit
    
    * feat: implement run testing decorations
    
    * chore: optimization code

---
+++ packages/testing/src/browser/icons/icons.ts
@@ -0,0 +1,26 @@
+import { getExternalIcon } from '@opensumi/ide-core-browser';
+import { TestResultState } from '../../common/testCollection';
+
+export const defaultIconColor = 'testing_icon_iconPassed';
+
+export const testStatesToIconColors: { [K in TestResultState]?: string } = {
+  [TestResultState.Errored]: 'testing_icon_iconFailed',
+  [TestResultState.Failed]: 'testing_icon_iconErrored',
+  [TestResultState.Passed]: defaultIconColor,
+  [TestResultState.Queued]: 'testing_icon_iconQueued',
+  [TestResultState.Unset]: 'testing_icon_iconUnset',
+  [TestResultState.Skipped]: 'testing_icon_iconUnset',
+};
+
+export const testingStatesToIcons = new Map<TestResultState, string>([
+  [TestResultState.Errored, getExternalIcon('issues')],
+  [TestResultState.Failed, getExternalIcon('error')],
+  [TestResultState.Passed, getExternalIcon('pass')],
+  [TestResultState.Queued, getExternalIcon('history')],
+  [TestResultState.Running, getExternalIcon('loading')],
+  [TestResultState.Skipped, getExternalIcon('debug-step-over')],
+  [TestResultState.Unset, getExternalIcon('circle-outline')],
+]);
+
+export const testingRunIcon = getExternalIcon('run');
+export const testingRunAllIcon = getExternalIcon('run-all');
