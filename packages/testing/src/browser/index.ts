7d38d9397c58bdba1c24402cae575c9af9692c64
John <qingyi.xjh@antgroup.com>
2022-01-20 14:15:40 +0800

    feat: support testing inline message content (#362)

--- packages/testing/src/browser/index.ts
+++ packages/testing/src/browser/index.ts
@@ -14,6 +14,7 @@ import { TestingPeekOpenerServiceToken } from '../common/testingPeekOpener';
 
 import './icons/icons.less';
 import './outputPeek/test-peek-widget.less';
+import './theme.less';
 
 import { TestingPeekOpenerServiceImpl } from './outputPeek/test-peek-opener.service';
 @Injectable()
