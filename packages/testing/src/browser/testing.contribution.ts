7bcf0c78c85049275ec985d57f4c4419e40c6ae9
John <qingyi.xjh@antgroup.com>
2022-01-27 15:17:34 +0800

    feat: support testing decorations glyph contextmenu (#407)
    * feat: impl basic testing glyph margin contextmenu
    
    * feat: support contextmenu run&debug testing
    
    * fix: contextmenu

--- packages/testing/src/browser/testing.contribution.ts
+++ packages/testing/src/browser/testing.contribution.ts
@@ -28,8 +28,10 @@ import {
 } from '@opensumi/ide-core-browser';
 import {
   ClosePeekTest,
+  DebugTestCommand,
   GoToTestCommand,
   PeekTestError,
+  RuntTestCommand,
   TestingDebugCurrentFile,
   TestingRunCurrentFile,
 } from '../common/commands';
@@ -45,6 +47,7 @@ import { TestingPeekOpenerServiceImpl } from './outputPeek/test-peek-opener.serv
 import { TestServiceImpl } from './test.service';
 import { TestServiceToken } from '../common';
 import { TestRunProfileBitset } from '../common/testCollection';
+import { IMenuRegistry, MenuContribution, MenuId } from '@opensumi/ide-core-browser/lib/menu/next';
 
 @Injectable()
 export class TestingOutputPeekDocumentProvider implements IEditorDocumentModelContentProvider {
@@ -64,9 +67,14 @@ export class TestingOutputPeekDocumentProvider implements IEditorDocumentModelCo
 }
 
 @Injectable()
-@Domain(ClientAppContribution, ComponentContribution, CommandContribution, BrowserEditorContribution)
+@Domain(ClientAppContribution, ComponentContribution, CommandContribution, BrowserEditorContribution, MenuContribution)
 export class TestingContribution
-  implements ClientAppContribution, ComponentContribution, CommandContribution, BrowserEditorContribution
+  implements
+    ClientAppContribution,
+    ComponentContribution,
+    CommandContribution,
+    BrowserEditorContribution,
+    MenuContribution
 {
   @Autowired(TestTreeViewModelToken)
   private readonly testTreeViewModel: ITestTreeViewModel;
@@ -113,6 +121,34 @@ export class TestingContribution
   }
 
   registerCommands(commands: CommandRegistry): void {
+    commands.registerCommand(RuntTestCommand, {
+      execute: async (extId: string) => {
+        const test = this.testTreeViewModel.getTestItem(extId);
+        if (!test) {
+          return;
+        }
+
+        await this.testService.runTests({
+          group: TestRunProfileBitset.Run,
+          tests: [test],
+        });
+      },
+    });
+
+    commands.registerCommand(DebugTestCommand, {
+      execute: async (extId: string) => {
+        const test = this.testTreeViewModel.getTestItem(extId);
+        if (!test) {
+          return;
+        }
+
+        await this.testService.runTests({
+          group: TestRunProfileBitset.Debug,
+          tests: [test],
+        });
+      },
+    });
+
     commands.registerCommand(GoToTestCommand, {
       execute: async (extId: string) => {
         const test = this.testTreeViewModel.getTestItem(extId);
@@ -200,6 +236,19 @@ export class TestingContribution
         }
       }
     };
+  }
+
+  registerMenus(menuRegistry: IMenuRegistry) {
+    menuRegistry.registerMenuItem(MenuId.TestingGlyphMarginContext, {
+      command: RuntTestCommand.id,
+      group: '1_has_decoration',
+      order: 1,
+    });
+    menuRegistry.registerMenuItem(MenuId.TestingGlyphMarginContext, {
+      command: DebugTestCommand.id,
+      group: '1_has_decoration',
+      order: 2,
+    });
   }
 
   registerEditorFeature(registry: IEditorFeatureRegistry) {
