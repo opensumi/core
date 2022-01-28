6096e88c72000da46dec95318dbaa7477aeaaff7
xinglong.wangwxl <xinglong.wangwxl@antgroup.com>
2022-01-28 15:05:04 +0800

    chore: requestWorkspaceTrust api happ impl

--- packages/types/vscode/typings-pending/vscode.proposed.d.ts
+++ packages/types/vscode/typings-pending/vscode.proposed.d.ts
@@ -1,1 +1,23 @@
-declare module 'vscode' {}
+declare module 'vscode' {
+  // #region https://github.com/microsoft/vscode/issues/120173
+  /**
+   * The object describing the properties of the workspace trust request
+   */
+  export interface WorkspaceTrustRequestOptions {
+    /**
+     * Custom message describing the user action that requires workspace
+     * trust. If omitted, a generic message will be displayed in the workspace
+     * trust request dialog.
+     */
+    readonly message?: string;
+  }
+  export namespace workspace {
+    /**
+     * Prompt the user to chose whether to trust the current workspace
+     * @param options Optional object describing the properties of the
+     * workspace trust request.
+     */
+    export function requestWorkspaceTrust(options?: WorkspaceTrustRequestOptions): Thenable<boolean | undefined>;
+  }
+  // #endregion
+}
