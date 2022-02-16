f8c45633e81d5845b3eef20aaad430667fe3eade
qingyi.xjh <qingyi.xjh@antgroup.com>
2022-02-16 16:04:14 +0800

    feat: create debug ansi handle function

--- packages/debug/src/browser/tree/debug-console-tree-node.define.tsx
+++ packages/debug/src/browser/tree/debug-console-tree-node.define.tsx
@@ -7,6 +7,22 @@ import ReactDOM from 'react-dom';
 import cls from 'classnames';
 import { LinkDetector } from '../debug-link-detector';
 
+const getColor = (severity?: MessageType): string => {
+  if (typeof severity === 'undefined') {
+    return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.info);
+  }
+  switch (severity) {
+    case MessageType.Error:
+      return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.error);
+    case MessageType.Warning:
+      return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.warn);
+    case MessageType.Info:
+      return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.info);
+    default:
+      return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.info);
+  }
+};
+
 export class TreeWithLinkWrapper extends React.Component<{ html: HTMLElement; className?: string }> {
   componentDidMount() {
     const container = ReactDOM.findDOMNode(this);
@@ -33,32 +49,17 @@ export class AnsiConsoleNode extends TreeNode {
     // 该节点默认只存在于根节点下
     private readonly _compositeTreeNode: CompositeTreeNode,
     public readonly linkDetector: LinkDetector,
+    private readonly ansiNode?: HTMLSpanElement,
     public readonly severity?: MessageType,
     public readonly source?: DebugProtocol.Source,
     public readonly line?: string | number,
   ) {
     super({} as ITree, _compositeTreeNode);
-    this.linkDetectorHTML = this.linkDetector.linkify(this.description);
+    this.linkDetectorHTML = this.ansiNode ?? this.linkDetector.linkify(this.description);
   }
 
   get name() {
     return `log_${this.id}`;
-  }
-
-  getColor(severity?: MessageType): string {
-    if (typeof severity === 'undefined') {
-      return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.info);
-    }
-    switch (severity) {
-      case MessageType.Error:
-        return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.error);
-      case MessageType.Warning:
-        return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.warn);
-      case MessageType.Info:
-        return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.info);
-      default:
-        return cls(debugConsoleStyles.variable_repl_text, debugConsoleStyles.info);
-    }
   }
 
   get el(): HTMLElement {
@@ -66,6 +67,6 @@ export class AnsiConsoleNode extends TreeNode {
   }
 
   get template(): any {
-    return () => <TreeWithLinkWrapper className={this.getColor(this.severity)} html={this.linkDetectorHTML} />;
+    return () => <TreeWithLinkWrapper className={getColor(this.severity)} html={this.linkDetectorHTML} />;
   }
 }
