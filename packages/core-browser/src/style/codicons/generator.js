f1b8be53f7e289610ed491fdcd4b084aa3a0ecbd
kuiwu <danwu.wdw@alibaba-inc.com>
2022-02-16 22:49:17 +0800

    chore: fix no-console lint error

--- packages/core-browser/src/style/codicons/generator.js
+++ packages/core-browser/src/style/codicons/generator.js
@@ -7,7 +7,7 @@ const nameIt = cssContent.matchAll(/\.codicon-(?<name>[\w-]+)+:before\s*{.+}/g);
 const formattedName = JSON.stringify([...nameIt].map((item) => item.groups.name))
   .replace(/"/g, "'")
   .replace(/,/g, '$& ');
-// tslint:disable-next-line:no-console
+// eslint-disable-next-line no-console
 console.log(formattedName);
 const curFile = path.join(__dirname, 'README.md');
 let curFileContent = fs.readFileSync(curFile, 'utf8');
