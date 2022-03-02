f3a75859f2e5293a4342dfd343d5abe13ea970bc
Dan <danwu.wdw@alibaba-inc.com>
2022-03-02 12:01:16 +0800

    chore: use tag to generate changelog (#516)
    * chore: use tag to generate changelog
    
    * chore: filter useless changelog

--- scripts/changelog/github.ts
+++ scripts/changelog/github.ts
@@ -12,72 +12,47 @@ export function getPullRequestLink(prId: string) {
   return `${GITHUB_URL}/pull/${prId}`;
 }
 
-async function getPrDesc(
-  prIid: string,
-  projectId = 'opensumi/core'
-) {
-  const res = await fetch(
-    `https://api.github.com/repos/${projectId}/pulls/${prIid}`,
-    {
-      method: 'GET',
-      headers: {
-        'Authorization': `token ${process.env.GITHUB_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+async function getPrDesc(prIid: string, projectId = 'opensumi/core') {
+  const res = await fetch(`https://api.github.com/repos/${projectId}/pulls/${prIid}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  });
   const ret = await res.json();
   return ret;
 }
 
-export async function getTagList(
-  per_page: number = 10,
-  projectId = 'opensumi/core'
-) {
-  const res = await fetch(
-    `https://api.github.com/repos/${projectId}/tags?per_page=${per_page}`,
-    {
-      method: 'GET',
-      headers: {
-        'Authorization': `token ${process.env.GITHUB_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+export async function getTagList(per_page: number = 10, projectId = 'opensumi/core') {
+  const res = await fetch(`https://api.github.com/repos/${projectId}/tags?per_page=${per_page}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  });
   const ret = await res.json();
   return ret;
 }
 
-export async function compareCommits(
-  base: string,
-  head: string,
-  projectId = 'opensumi/core'
-) {
-  const res = await fetch(
-    `https://api.github.com/repos/${projectId}/compare/${base}...${head}`,
-    {
-      method: 'GET',
-      headers: {
-        'Authorization': `token ${process.env.GITHUB_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+export async function compareCommits(base: string, head: string, projectId = 'opensumi/core') {
+  const res = await fetch(`https://api.github.com/repos/${projectId}/compare/${base}...${head}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  });
   const ret = await res.json();
   return ret;
 }
 
 /**
- * 从 commit log 按照 github 的特性分离 changelog
+ * 从 commit log 按照 github 的特性分离 Changelog
  */
-export async function extractChangelog(
-  logs: ReadonlyArray<DefaultLogFields>
-): Promise<ICommitLogFields[]> {
-
-  // PullRequest: 20 fix: 修复插件过早注册/替换视图导致的问题
-  const githubPrLogs = logs.filter((n) =>
-    n.message.endsWith(')')
-  );
+export async function extractChangelog(logs: ReadonlyArray<DefaultLogFields>): Promise<ICommitLogFields[]> {
+  const githubPrLogs = logs.filter((n) => n.message.endsWith(')'));
   const result: ICommitLogFields[] = [];
   const regex = /\(#(\d+)\)$/;
   for (const log of githubPrLogs) {
