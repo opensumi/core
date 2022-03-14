6cb6f8da5d64e970cf376fc0747d1a87746f0040
kuiwu <danwu.wdw@alibaba-inc.com>
2022-03-14 20:55:16 +0800

    chore: skip some test

--- packages/terminal-next/__tests__/browser/client.test.ts
+++ packages/terminal-next/__tests__/browser/client.test.ts
@@ -27,7 +27,7 @@ function createDOMContainer() {
   return div;
 }
 
-describe('Terminal Client', () => {
+describe.skip('Terminal Client', () => {
   let client: ITerminalClient;
   let widget: IWidget;
   let proxy: httpProxy;
