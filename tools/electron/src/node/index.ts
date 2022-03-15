60b7e24195c13bcad5af2e3c372aee539e156f26
Aaaaash <binshao54@gmail.com>
2022-03-15 17:58:28 +0800

    fix: import

--- tools/electron/src/node/index.ts
+++ tools/electron/src/node/index.ts
@@ -1,10 +1,8 @@
-import { TopBarModule } from 'modules/topbar';
-
 import { AddonsModule } from '@opensumi/ide-addons/lib/node';
 import { NodeModule, ConstructorOf } from '@opensumi/ide-core-node';
 import { ServerCommonModule } from '@opensumi/ide-core-node';
-import { ExtensionModule } from '@opensumi/ide-extension';
 import { OpenVsxExtensionManagerModule } from '@opensumi/ide-extension-manager';
+import { ExtensionModule } from '@opensumi/ide-extension/lib/node';
 import { FileSchemeNodeModule } from '@opensumi/ide-file-scheme/lib/node';
 import { FileSearchModule } from '@opensumi/ide-file-search';
 import { FileServiceModule } from '@opensumi/ide-file-service/lib/node';
@@ -14,7 +12,6 @@ import { SearchModule } from '@opensumi/ide-search';
 import { TerminalNodePtyModule } from '@opensumi/ide-terminal-next/lib/node';
 
 import { startServer } from './server';
-
 
 export const CommonNodeModules: ConstructorOf<NodeModule>[] = [
   ServerCommonModule,
