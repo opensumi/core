c0cbc056d1434fa03359f2d153da30f6f2a7d870
大表哥 <xubing.bxb@alibaba-inc.com>
2022-03-29 10:31:46 +0800

    feat: update iconfont (#738)

--- packages/core-browser/src/style/icon/ide-iconfont.ts
+++ packages/core-browser/src/style/icon/ide-iconfont.ts
@@ -1,4 +1,4 @@
-export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/font_1432262_8rr0qkgdwud.css';
+export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/font_1432262_kqiwyvii9j9.css';
 
 export const IDE_ICONFONT_CN_JS = IDE_ICONFONT_CN_CSS.replace(/\.css$/, '.js');
 
