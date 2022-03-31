dbf06aeea8474db5a788046caf9ded7ff158e6bb
大表哥 <xubing.bxb@alibaba-inc.com>
2022-03-31 10:55:03 +0800

    feat: add scrollbar for terminal tab

--- packages/terminal-next/src/browser/component/tab.view.tsx
+++ packages/terminal-next/src/browser/component/tab.view.tsx
@@ -1,7 +1,8 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { useInjectable } from '@opensumi/ide-core-browser';
+import { Scroll } from '@opensumi/ide-core-browser/lib/components/scroll';
 
 import { ITerminalGroupViewService, ITerminalRenderProvider, ItemType } from '../../common';
 import { TerminalContextMenuService } from '../terminal.context-menu';
@@ -13,43 +14,46 @@ export default observer(() => {
   const view = useInjectable<ITerminalGroupViewService>(ITerminalGroupViewService);
   const provider = useInjectable<ITerminalRenderProvider>(ITerminalRenderProvider);
   const menuService = useInjectable<TerminalContextMenuService>(TerminalContextMenuService);
+  const tabContainer = useRef<HTMLDivElement | null>();
 
   return (
     <div className={styles.view_container}>
       <div className={styles.tabs}>
-        {view.groups.map((group, index) => {
-          if (!group) {
-            return;
-          }
-          return (
-            <TabItem
-              key={group.id}
-              id={group.id}
-              editable={group.editable}
-              name={group.snapshot}
-              selected={view.currentGroup && view.currentGroup.id === group.id}
-              onInputBlur={() => group.unedit()}
-              onInputEnter={(_: string, name: string) => group.rename(name)}
-              onClick={() => view.selectGroup(index)}
-              onClose={() => view.removeGroup(index)}
-              onContextMenu={(event) => menuService.onTabContextMenu(event, index)}
-              provider={provider}
-            ></TabItem>
-          );
-        })}
+        <Scroll ref={(el) => (el ? (tabContainer.current = el.ref) : null)}>
+          <div className={styles.tab_contents}>
+            {view.groups.map((group, index) => {
+              if (!group) {
+                return;
+              }
+              return (
+                <TabItem
+                  key={group.id}
+                  id={group.id}
+                  editable={group.editable}
+                  name={group.snapshot}
+                  selected={view.currentGroup && view.currentGroup.id === group.id}
+                  onInputBlur={() => group.unedit()}
+                  onInputEnter={(_: string, name: string) => group.rename(name)}
+                  onClick={() => view.selectGroup(index)}
+                  onClose={() => view.removeGroup(index)}
+                  onContextMenu={(event) => menuService.onTabContextMenu(event, index)}
+                  provider={provider}
+                ></TabItem>
+              );
+            })}
+          </div>
+        </Scroll>
       </div>
-      <div className={styles.button}>
-        <TabItem
-          type={ItemType.add}
-          onClick={() => {
-            const index = view.createGroup();
-            const group = view.getGroup(index);
-            view.createWidget(group);
-            view.selectGroup(index);
-          }}
-          provider={provider}
-        />
-      </div>
+      <TabItem
+        type={ItemType.add}
+        onClick={() => {
+          const index = view.createGroup();
+          const group = view.getGroup(index);
+          view.createWidget(group);
+          view.selectGroup(index);
+        }}
+        provider={provider}
+      />
     </div>
   );
 });
