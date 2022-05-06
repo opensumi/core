363f18ce444f660df9b82b7e9830859e2b4dba09
野声 <lijiacheng.ljc@antgroup.com>
2022-05-06 11:51:09 +0800

    chore: fix yargs typings

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,7 +1,7 @@
 import { startFromFolder } from './fn/module';
 import { argv } from 'yargs';
-const folderName = argv.folder as string || 'packages/startup';
-const scriptName = argv.script as string || 'start';
+const folderName = (argv.folder as string) || 'packages/startup';
+const scriptName = (argv.script as string) || 'start';
 
 if (!folderName) {
   throw Error('folderName is required.');
