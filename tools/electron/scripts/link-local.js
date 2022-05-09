f68744a1f8ed8c67587c7ea29ba1af7db98edc51
大表哥 <xubing.bxb@alibaba-inc.com>
2022-05-09 15:49:22 +0800

    chore: remove unused samples code

--- tools/electron/scripts/link-local.js
+++ tools/electron/scripts/link-local.js
@@ -1,28 +1,25 @@
-
 const { resolve, basename, join, dirname } = require('path');
-const { execSync } = require('child_process');
 const { existsSync, symlinkSync, mkdirSync, readdirSync, readFileSync, unlinkSync } = require('fs');
 
-const KTFrameworkDir = resolve('../../');
+const frameworkDir = resolve('../../');
 
 function ensureDirSync(path) {
-  if (!existsSync(path)){
+  if (!existsSync(path)) {
     mkdirSync(path);
   }
 }
 
 function linkNodeModules(moduleDir, targetParent) {
-
   let name = basename(moduleDir);
-  if (name.startsWith('_')){
+  if (name.startsWith('_')) {
     return;
   }
   if (name.startsWith('@') || name === 'node_modules') {
-    const target = join(targetParent,name);
+    const target = join(targetParent, name);
     ensureDirSync(target);
-    readdirSync(moduleDir).forEach(dir => {
-      linkNodeModules(join(moduleDir, dir), target)
-    })
+    readdirSync(moduleDir).forEach((dir) => {
+      linkNodeModules(join(moduleDir, dir), target);
+    });
   } else {
     const target = join(targetParent, name);
     if (!existsSync(target)) {
@@ -34,24 +31,20 @@ function linkNodeModules(moduleDir, targetParent) {
 
       symlinkSync(moduleDir, target);
     }
-
   }
-
 }
 
-
-linkNodeModules(join(KTFrameworkDir, '/node_modules'), resolve('./'));
-
+linkNodeModules(join(frameworkDir, '/node_modules'), resolve('./'));
 
 function linkPackages(packagesDir, targetParent) {
-  readdirSync(packagesDir).forEach(packageName => {
+  readdirSync(packagesDir).forEach((packageName) => {
     if (packageName.startsWith('.')) {
       return;
     }
-    if (!existsSync(join(packagesDir, packageName ,'package.json'))) {
+    if (!existsSync(join(packagesDir, packageName, 'package.json'))) {
       return;
     }
-    const name = JSON.parse(readFileSync(join(packagesDir, packageName ,'package.json'),'utf8').toString()).name;
+    const name = JSON.parse(readFileSync(join(packagesDir, packageName, 'package.json'), 'utf8').toString()).name;
     const target = join(targetParent, name);
     try {
       unlinkSync(target); //失效link
@@ -60,7 +53,7 @@ function linkPackages(packagesDir, targetParent) {
     }
     ensureDirSync(dirname(target));
     symlinkSync(join(packagesDir, packageName), target);
-  })
+  });
 }
 
-linkPackages(join(KTFrameworkDir, '/packages'), resolve('./node_modules'));
+linkPackages(join(frameworkDir, '/packages'), resolve('./node_modules'));
