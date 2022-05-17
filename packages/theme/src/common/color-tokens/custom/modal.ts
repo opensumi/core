791c8e924dba77389260223abf0bbe40f256c329
iamxukai <85668115+iamxukai@users.noreply.github.com>
2022-05-17 14:51:16 +0800

    style: use color tokens to replace color values (#1022)
    * style: use color tokens to replace color values
    
    * chore: remove editor success foreground
    
    * style: move separator and icon color style
    
    * style: use color tokens to replace color values

--- packages/theme/src/common/color-tokens/custom/modal.ts
+++ packages/theme/src/common/color-tokens/custom/modal.ts
@@ -1,16 +1,22 @@
 import { localize } from '@opensumi/ide-core-common';
 
 import { registerColor } from '../../color-registry';
-import { NOTIFICATIONS_BACKGROUND, NOTIFICATIONS_FOREGROUND } from '../notification';
+import {
+  NOTIFICATIONS_BACKGROUND,
+  NOTIFICATIONS_ERROR_ICON_FOREGROUND,
+  NOTIFICATIONS_FOREGROUND,
+  NOTIFICATIONS_INFO_ICON_FOREGROUND,
+  NOTIFICATIONS_WARNING_ICON_FOREGROUND,
+} from '../notification';
 
 export const ktModalForeground = registerColor(
   'kt.modal.foreground',
-  { light: NOTIFICATIONS_FOREGROUND, dark: '#D7DBDE', hc: null },
+  { light: NOTIFICATIONS_FOREGROUND, dark: NOTIFICATIONS_FOREGROUND, hc: null },
   localize('ktModalForeground', 'Modal Foreground color.'),
 );
 export const ktModalBackground = registerColor(
   'kt.modal.background',
-  { light: NOTIFICATIONS_BACKGROUND, dark: '#35393D', hc: null },
+  { light: NOTIFICATIONS_BACKGROUND, dark: NOTIFICATIONS_BACKGROUND, hc: null },
   localize('ktModalBackground', 'Modal Background color.'),
 );
 
@@ -21,17 +27,17 @@ export const ktModalSeparatorBackground = registerColor(
 );
 export const ktModalErrorIconForeground = registerColor(
   'kt.modalErrorIcon.foreground',
-  { light: '#DB4345', dark: '#DB4345', hc: null },
+  { light: NOTIFICATIONS_ERROR_ICON_FOREGROUND, dark: NOTIFICATIONS_ERROR_ICON_FOREGROUND, hc: null },
   localize('ktModalErrorIconForeground', 'Modal Error Icon Foreground Color.'),
 );
 export const ktModalWarningIconForeground = registerColor(
   'kt.modalWarningIcon.foreground',
-  { light: '#DB9238', dark: '#DB9238', hc: null },
+  { light: NOTIFICATIONS_WARNING_ICON_FOREGROUND, dark: NOTIFICATIONS_WARNING_ICON_FOREGROUND, hc: null },
   localize('ktModalWarningIconForeground', 'Modal Warning Icon Foreground Color.'),
 );
 export const ktModalInfoIconForeground = registerColor(
   'kt.modalInfoIcon.foreground',
-  { light: '#DB4345', dark: '#DB4345', hc: null },
+  { light: NOTIFICATIONS_INFO_ICON_FOREGROUND, dark: NOTIFICATIONS_INFO_ICON_FOREGROUND, hc: null },
   localize('ktModalInfoIconForeground', 'Modal Info Icon Foreground Color.'),
 );
 export const ktModalSuccessIconForeground = registerColor(
