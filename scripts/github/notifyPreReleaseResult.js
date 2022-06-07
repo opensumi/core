9cc7aea6e606f064286b604f88cd9e9b61f796c0
Artin <lijiacheng.ljc@antgroup.com>
2022-06-07 23:21:27 +0800

    ci: fix workflow (#1183)
    * chore: fix workflow
    
    * chore: fix workflow

--- scripts/github/notifyPreReleaseResult.js
+++ scripts/github/notifyPreReleaseResult.js
@@ -3,7 +3,7 @@ const { createVersionText } = require('./helpers');
 module.exports = async ({ github, context, core }) => {
   const issueBody = createVersionText('Pre-Release', process.env.CURRENT_VERSION);
 
-  github.rest.issues.createComment({
+  await github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
