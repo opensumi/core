40e8c26c0170a407e72b7a85309cc9492c655173
野声 <lijiacheng.ljc@antgroup.com>
2022-06-09 18:31:19 +0800

    chore: update start electron

--- scripts/fn/shell.ts
+++ scripts/fn/shell.ts
@@ -1,7 +1,6 @@
 import { command } from 'execa';
 
 export async function run(cmd: string) {
-  // tslint:disable-next-line
   console.log(`[RUN]: ${cmd}`);
   return command(cmd, { stdio: 'inherit', shell: true });
 }
