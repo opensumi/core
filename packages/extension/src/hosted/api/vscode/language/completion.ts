63f59c5beeb2438b451d9da55db50f19b8a5f28e
John <qingyi.xjh@antgroup.com>
2022-06-13 12:00:24 +0800

    chore: revert suggest dto insertText field (#1187)

--- packages/extension/src/hosted/api/vscode/language/completion.ts
+++ packages/extension/src/hosted/api/vscode/language/completion.ts
@@ -88,22 +88,19 @@ export class CompletionAdapter {
         [ISuggestDataDtoField.kind]: item.kind ? Converter.CompletionItemKind.from(item.kind) : undefined,
         [ISuggestDataDtoField.sortText]: item.sortText,
         [ISuggestDataDtoField.filterText]: item.filterText,
+        [ISuggestDataDtoField.documentation]: item.documentation,
         x: [pid, i] as [number, number],
       };
 
-      // 如果不支持 resolveCompletionItem 则全量返回
-      if (typeof this.delegate.resolveCompletionItem !== 'function') {
-        if (item.textEdit) {
-          dto[ISuggestDataDtoField.insertText] = item.textEdit.newText;
-        } else if (typeof item.insertText === 'string') {
-          dto[ISuggestDataDtoField.insertText] = item.insertText;
-        } else if (item.insertText instanceof SnippetString) {
-          dto[ISuggestDataDtoField.insertText] = item.insertText.value;
-          dto[ISuggestDataDtoField.insertTextRules] = CompletionItemInsertTextRule.InsertAsSnippet;
-        } else {
-          dto[ISuggestDataDtoField.insertText] = typeof item.label === 'string' ? item.label : item.label.label;
-        }
-        dto[ISuggestDataDtoField.documentation] = item.documentation;
+      if (item.textEdit) {
+        dto[ISuggestDataDtoField.insertText] = item.textEdit.newText;
+      } else if (typeof item.insertText === 'string') {
+        dto[ISuggestDataDtoField.insertText] = item.insertText;
+      } else if (item.insertText instanceof SnippetString) {
+        dto[ISuggestDataDtoField.insertText] = item.insertText.value;
+        dto[ISuggestDataDtoField.insertTextRules] = CompletionItemInsertTextRule.InsertAsSnippet;
+      } else {
+        dto[ISuggestDataDtoField.insertText] = typeof item.label === 'string' ? item.label : item.label.label;
       }
 
       const range = this.convertRange(item, inserting, replacing);
