d312ccc6650cdd7660b2122e0f0cee0a741d2765
hacke2 <xinglong.wangwxl@antgroup.com>
2022-06-13 15:48:52 +0800

    feat: release 时生成 manifest 文件 (#1199)

---
+++ scripts/generate-manifest-json.ts
@@ -0,0 +1,25 @@
+import { readFileSync, writeFileSync, ensureFileSync } from 'fs-extra';
+import { generateManifest } from './manifest';
+import Package, { readAllMainPackages } from './pkg';
+import { argv } from 'yargs';
+import { join } from 'path';
+
+// npm run manifest -- -v=2.19.0
+const version = argv.v as string;
+const localManifest = join(__dirname, '../packages/types/manifest.json');
+if (!version) {
+  throw new Error('version is required');
+}
+
+const packages: Package[] = readAllMainPackages();
+generateManifest(packages, version).then((manifest) => updateFileWithDispose(localManifest, manifest));
+
+function updateFileWithDispose(filePath: string, content: any | ((original: string) => any)) {
+  ensureFileSync(filePath);
+  const original = readFileSync(filePath, { encoding: 'utf8' });
+  let newContent = content;
+  if (typeof content === 'function') {
+    newContent = content(original);
+  }
+  writeFileSync(filePath, `${JSON.stringify(newContent, null, 2)}\n`);
+}
