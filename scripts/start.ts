878e7263cac7ba8b7cc4117afd75401d50d46021
野声 <lijiacheng.ljc@antgroup.com>
2022-06-17 11:41:57 +0800

    chore: update types

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,5 +1,6 @@
 import { startFromFolder } from './fn/module';
-import { argv } from 'yargs';
+import { argv } from '../packages/core-common/src/node/cli';
+
 const folderName = (argv.folder as string) || 'packages/startup';
 const scriptName = (argv.script as string) || 'start';
 
