257c7a04f4cea356c0fb349afbd741d35be941bb
野声 <lijiacheng.ljc@antgroup.com>
2022-06-21 14:46:18 +0800

    chore: update run-tests

--- scripts/run-tests.ts
+++ scripts/run-tests.ts
@@ -1,26 +1,23 @@
 import { argv } from '../packages/core-common/src/node/cli';
-import { runTest } from './jest/runTest';
 
+import { runTest } from './jest/runTest';
 import { getShardPackages } from './jest/shard';
 
-let pkgs = getShardPackages();
+const pkgs = getShardPackages();
 
 const packagesDirNames = pkgs.map((pkg) => pkg.dirname);
 
-const main = async () => {
-  console.log(`current jest modules:`, packagesDirNames);
+if ((argv as any).strictPromise) {
+  process.env['EXIT_ON_UNHANDLED_REJECTION'] = 'true';
+}
 
-  if ((argv as any).strictPromise) {
-    process.env['EXIT_ON_UNHANDLED_REJECTION'] = 'true';
-  }
+console.log(`current jest modules:`, packagesDirNames);
 
-  const v = await runTest(packagesDirNames);
+runTest(packagesDirNames).then((v) => {
   console.log('测试是否成功:', v.results.success);
   // 如果一秒后进程还没退出
   setTimeout(() => {
     console.log('1s 后强行退出');
     process.exit(v.results.success ? 0 : 1);
-  }, 1000);
-};
-
-main();
+  }, 3000);
+});
