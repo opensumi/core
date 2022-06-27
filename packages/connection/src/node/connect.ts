32f609eeb3eb7e9790b9a5158842bc68e458397d
Lam Tang <tangyenan@gmail.com>
2022-06-27 15:48:11 +0800

    feat: 捕获MessageConnection的消息

--- packages/connection/src/node/connect.ts
+++ packages/connection/src/node/connect.ts
@@ -6,7 +6,34 @@ import {
   createMessageConnection,
 } from '@opensumi/vscode-jsonrpc/lib/node/main';
 
-
 export function createSocketConnection(socket: net.Socket) {
-  return createMessageConnection(new SocketMessageReader(socket), new SocketMessageWriter(socket));
+  // return createMessageConnection(new SocketMessageReader(socket), new SocketMessageWriter(socket));
+
+  const messageConnection = createMessageConnection(new SocketMessageReader(socket), new SocketMessageWriter(socket));
+
+  const messageConnectionProxy = new Proxy(messageConnection, {
+    get(target, prop) {
+      if (prop === 'sendRequest' || prop === 'sendNotification') {
+        return function (...args: any) {
+          // 注意这是common/xxx，所以要同时考虑在browser和在node的情况，node是没有window的
+          if (typeof window !== 'undefined' && window.__OPENSUMI_DEVTOOL_EVENT_SOURCE_TOKEN__) {
+            window.__OPENSUMI_DEVTOOL_EVENT_SOURCE_TOKEN__.traffic.send([prop, ...args]);
+          }
+          return target[prop].apply(target, [...args]);
+        };
+      }
+
+      if (prop === 'onRequest' || prop === 'onNotification') {
+        return function (...args: any) {
+          if (typeof window !== 'undefined' && window.__OPENSUMI_DEVTOOL_EVENT_SOURCE_TOKEN__) {
+            window.__OPENSUMI_DEVTOOL_EVENT_SOURCE_TOKEN__.traffic.receive([prop, ...args]);
+          }
+          return target[prop].apply(target, [...args]);
+        };
+      }
+      return target[prop];
+    },
+  });
+
+  return messageConnectionProxy;
 }
