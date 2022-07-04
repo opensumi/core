436180314a4b1c739c440b54969d6660ec399c46
qingyi.xjh <qingyi.xjh@antgroup.com>
2022-07-04 11:53:47 +0800

    feat: implement viewsProxies contribute code snippet

---
+++ packages/extension/src/browser/sumi/contributes/views-proxies.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@opensumi/di';
+import { localize } from '@opensumi/ide-core-common';
+
+import { VSCodeContributePoint, Contributes } from '../../../common';
+
+@Injectable()
+@Contributes('viewsProxies')
+export class ViewsProxiesContributionPoint extends VSCodeContributePoint<{ [key in string]: string }> {
+
+  static schema = {
+    type: 'array',
+    markdownDescription: localize('kaitianContributes.viewsProxies'),
+    defaultSnippets: [
+      {
+        body: ["${1}"]
+      }
+    ],
+    items: {
+      type: 'string'
+    }
+  };
+
+  contribute() {
+    // do nothing
+  }
+}
