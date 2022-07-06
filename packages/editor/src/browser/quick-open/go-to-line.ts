2432cde6090e43beb7a077d2257b79e7b1877e34
RetroX <retrox.jcy@antgroup.com>
2022-07-06 17:54:44 +0800

    chore: monaco adaptation v4 (#1284)

--- packages/editor/src/browser/quick-open/go-to-line.ts
+++ packages/editor/src/browser/quick-open/go-to-line.ts
@@ -10,7 +10,7 @@ import { QuickOpenItem, PrefixQuickOpenService, QuickOpenModel, Mode } from '@op
 import { Event as MonacoEvent } from '@opensumi/ide-monaco/lib/browser/monaco-api/types';
 import { IRange as IMonacoRange } from '@opensumi/monaco-editor-core/esm/vs/editor/common/core/range';
 import { IEditor as IMonacoCodeEditor } from '@opensumi/monaco-editor-core/esm/vs/editor/common/editorCommon';
-import { AbstractGotoLineQuickAccessProvider } from '@opensumi/monaco-editor-core/esm/vs/editor/contrib/quickAccess/gotoLineQuickAccess';
+import { AbstractGotoLineQuickAccessProvider } from '@opensumi/monaco-editor-core/esm/vs/editor/contrib/quickAccess/browser/gotoLineQuickAccess';
 import * as monaco from '@opensumi/monaco-editor-core/esm/vs/editor/editor.api';
 
 import { WorkbenchEditorService } from '../types';
