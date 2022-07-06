d0fe352f52abc908389550659f6c5149d259f626
situ2001 <yongcong2001@outlook.com>
2022-07-06 10:33:36 +0800

    chore: clean up

--- packages/collaboration/__tests__/node/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { CollaborationServerModule } from '../../src/node';
-
-describe('template test', () => {
-  it('TemplateUpperNameModule', () => {
-    const cls = new CollaborationServerModule();
-    expect(cls.providers).toEqual([]);
-  });
-});
--- packages/collaboration/src/node/index.ts
+++
@@ -1,7 +0,0 @@
-import { Provider, Injectable } from '@opensumi/di';
-import { NodeModule } from '@opensumi/ide-core-node';
-
-@Injectable()
-export class CollaborationServerModule extends NodeModule {
-  providers: Provider[] = [];
-}
