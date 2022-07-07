c991594c49e636a0844d2c88bf4f2f695da17cee
retrox.jcy <retrox.jcy@antgroup.com>
2022-07-07 17:53:07 +0800

    chore: delete old codes

--- packages/extension/src/hosted/api/vscode/language/inlay-hints.ts
+++ packages/extension/src/hosted/api/vscode/language/inlay-hints.ts
@@ -20,12 +20,6 @@ export class InlayHintsAdapter {
     private readonly _commands: CommandsConverter,
   ) {}
 
-  async provideInlayHints1(resource: Uri, range: IRange, token: CancellationToken) {
-    const doc = this._documents.getDocument(resource);
-    const value = await this._provider.provideInlayHints(doc!, typeConvert.Range.to(range), token);
-    return value ? { hints: value.map(typeConvert.InlayHint.from) } : undefined;
-  }
-
   async provideInlayHints(resource: Uri, ran: IRange, token: CancellationToken): Promise<IInlayHintsDto | undefined> {
     const doc = this._documents.getDocument(resource);
     const range = typeConvert.Range.to(ran);
