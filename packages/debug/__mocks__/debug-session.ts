09a63faadcc1704f7676b616de33606d59f13a71
大表哥 <xubing.bxb@alibaba-inc.com>
2022-07-13 11:17:55 +0800

    fix: context key types

--- packages/debug/__mocks__/debug-session.ts
+++ packages/debug/__mocks__/debug-session.ts
@@ -28,13 +28,20 @@ export class MockDebugSession implements IDebugSession {
     this.disposable.push(this._onCurrentThreadChange);
     this.disposable.push(this._onDidInvalidateMemory);
   }
-  capabilities: DebugProtocol.Capabilities;
-  onDidInvalidateMemory: Event<DebugProtocol.MemoryEvent>;
 
-  readMemory(memoryReference: string, offset: number, count: number): Promise<DebugProtocol.ReadMemoryResponse | undefined> {
+  readMemory(
+    memoryReference: string,
+    offset: number,
+    count: number,
+  ): Promise<DebugProtocol.ReadMemoryResponse | undefined> {
     throw new Error('Method not implemented.');
   }
-  writeMemory(memoryReference: string, offset: number, data: string, allowPartial?: boolean | undefined): Promise<DebugProtocol.WriteMemoryResponse | undefined> {
+  writeMemory(
+    memoryReference: string,
+    offset: number,
+    data: string,
+    allowPartial?: boolean | undefined,
+  ): Promise<DebugProtocol.WriteMemoryResponse | undefined> {
     throw new Error('Method not implemented.');
   }
 
