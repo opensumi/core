226962969802e90d10217c2b44580ea6800d3e2f
situ2001 <yongcong2001@outlook.com>
2022-07-17 18:01:55 +0800

    chore: remove unused log

--- packages/collaboration/src/browser/collaboration.contribution.ts
+++ packages/collaboration/src/browser/collaboration.contribution.ts
@@ -6,16 +6,13 @@ import {
   KeybindingWeight,
   PreferenceService,
 } from '@opensumi/ide-core-browser';
-import { CommandContribution, CommandRegistry, Domain, ILogger, PreferenceScope } from '@opensumi/ide-core-common';
+import { CommandContribution, CommandRegistry, Domain } from '@opensumi/ide-core-common';
 
 import { ICollaborationService } from '../common';
 import { REDO, UNDO } from '../common/commands';
 
 @Domain(ClientAppContribution, KeybindingContribution, CommandContribution)
 export class CollaborationContribution implements ClientAppContribution, KeybindingContribution, CommandContribution {
-  @Autowired(ILogger)
-  private logger: ILogger;
-
   @Autowired(ICollaborationService)
   private collaborationService: ICollaborationService;
 
@@ -23,10 +20,7 @@ export class CollaborationContribution implements ClientAppContribution, Keybind
   private preferenceService: PreferenceService;
 
   onDidStart() {
-    this.logger.log('Collaboration Contribution initialized');
-    this.logger.log('preference got', this.preferenceService.get('editor.askIfDiff'));
     if (this.preferenceService.get('editor.askIfDiff') === true) {
-      this.logger.log('Set ask diff to false');
       this.preferenceService.set('editor.askIfDiff', false);
     }
     this.collaborationService.initialize();
@@ -34,7 +28,6 @@ export class CollaborationContribution implements ClientAppContribution, Keybind
 
   onStop() {
     if (this.preferenceService.get('editor.askIfDiff') === false) {
-      this.logger.log('Set ask diff to true');
       this.preferenceService.set('editor.askIfDiff', true);
     }
     this.collaborationService.destroy();
