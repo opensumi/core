743352751357aa61c7f79d789a3f9374ea043cbc
John <qingyi.xjh@antgroup.com>
2022-07-19 15:16:29 +0800

    fix: contribute json schema of undefined (#1375)

--- packages/extension/src/browser/extension.contribution.ts
+++ packages/extension/src/browser/extension.contribution.ts
@@ -149,7 +149,7 @@ export class ExtensionClientAppContribution implements ClientAppContribution {
       const contributeName = Reflect.getMetadata(CONTRIBUTE_NAME_KEY, contributeCls);
       this.extensionsPointService.registerExtensionPoint({
         extensionPoint: contributeName,
-        jsonSchema: contributeCls.schema,
+        jsonSchema: contributeCls.schema || {},
         frameworkKind: ['vscode', 'opensumi'],
       });
     }
@@ -158,7 +158,7 @@ export class ExtensionClientAppContribution implements ClientAppContribution {
       const contributeName = Reflect.getMetadata(CONTRIBUTE_NAME_KEY, contributeCls);
       this.extensionsPointService.registerExtensionPoint({
         extensionPoint: contributeName,
-        jsonSchema: contributeCls.schema,
+        jsonSchema: contributeCls.schema || {},
         frameworkKind: ['opensumi'],
       });
     }
