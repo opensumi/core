2857da08d24d27095997ffa18e5313f67f162e57
sinoon <sinoon1218@gmail.com>
2022-08-09 13:44:08 +0800

    fix: fix eslint error

--- packages/monaco/src/browser/monaco-snippet-suggest-provider.ts
+++ packages/monaco/src/browser/monaco-snippet-suggest-provider.ts
@@ -326,8 +326,8 @@ export class MonacoSnippetSuggestion implements monaco.languages.CompletionItem 
   readonly detail: string;
   readonly sortText: string;
   readonly noAutoAccept = true;
-  readonly type: 'snippet' = 'snippet';
-  readonly snippetType: 'textmate' = 'textmate';
+  readonly type = 'snippet' as const;
+  readonly snippetType = 'textmate' as const;
   readonly kind = monaco.languages.CompletionItemKind.Snippet;
   range: IRange;
 
