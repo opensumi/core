b4e794366cbbbd563bce37c127b6209cdf421978
徐凯 <493709361@qq.com>
2022-08-14 22:44:34 +0800

    feat: shortcut adaptaion of ctrlcmd+f1

--- packages/markers/src/browser/markers-contribution.ts
+++ packages/markers/src/browser/markers-contribution.ts
@@ -1,5 +1,12 @@
 import { Autowired } from '@opensumi/di';
-import { ComponentContribution, ComponentRegistry, Domain, Logger } from '@opensumi/ide-core-browser';
+import {
+  CommandContribution,
+  CommandRegistry,
+  ComponentContribution,
+  ComponentRegistry,
+  Domain,
+  Logger,
+} from '@opensumi/ide-core-browser';
 import { IMainLayoutService, MainLayoutContribution } from '@opensumi/ide-main-layout';
 
 import { IMarkerService, MARKER_CONTAINER_ID } from '../common';
@@ -9,8 +16,14 @@ import { MarkerService } from './markers-service';
 import { MarkerPanel } from './markers-tree.view';
 import Messages from './messages';
 
-@Domain(ComponentContribution, MainLayoutContribution)
-export class MarkersContribution implements ComponentContribution, MainLayoutContribution {
+export namespace MARKER_COMMANDS {
+  export const SHOW_ERRORS_WARNINGS = {
+    id: 'workbench.action.showErrorsWarnings',
+  };
+}
+
+@Domain(CommandContribution, ComponentContribution, MainLayoutContribution)
+export class MarkersContribution implements CommandContribution, ComponentContribution, MainLayoutContribution {
   @Autowired()
   logger: Logger;
 
@@ -43,6 +56,18 @@ export class MarkersContribution implements ComponentContribution, MainLayoutCon
         containerId: MARKER_CONTAINER_ID,
         activateKeyBinding: 'ctrlcmd+shift+m',
         titleComponent: MarkerFilterPanel,
+      },
+    );
+  }
+
+  registerCommands(commands: CommandRegistry): void {
+    commands.registerCommand(
+      { id: MARKER_COMMANDS.SHOW_ERRORS_WARNINGS.id },
+      {
+        execute: () => {
+          const tabbarHandler = this.mainlayoutService.getTabbarHandler(MARKER_CONTAINER_ID);
+          tabbarHandler?.activate();
+        },
       },
     );
   }
