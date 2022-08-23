124726aebd3c48afae663d02df9e6d217a2fba56
situ2001 <yongcong2001@outlook.com>
2022-08-23 15:18:27 +0800

    refactor: move constants

--- packages/collaboration/src/common/commands.ts
+++ packages/collaboration/src/common/commands.ts
@@ -1,18 +1,15 @@
 import { Command } from '@opensumi/ide-core-common';
 
-const category = 'Collaboration';
+const COMMAND_CATEGORY = 'Collaboration';
 
 export const UNDO: Command = {
   id: 'collaboration.undo',
   label: 'collaboration.undo', // TODO i18n
-  category,
+  category: COMMAND_CATEGORY,
 };
 
 export const REDO: Command = {
   id: 'collaboration.redo',
   label: 'collaboration.redo', // TODO i18n
-  category,
+  category: COMMAND_CATEGORY,
 };
-
-export const Y_REMOTE_SELECTION = 'yRemoteSelection';
-export const Y_REMOTE_SELECTION_HEAD = 'yRemoteSelectionHead';
