1eb6cffd58257a79281d85b8b10afc4bd7772892
songhn233 <songhn233@gmail.com>
2022-09-06 04:09:07 +0800

    feat: import electron for clipboard & render

--- packages/core-browser/src/services/electron-clipboard.service.ts
+++ packages/core-browser/src/services/electron-clipboard.service.ts
@@ -1,29 +1,35 @@
-import { Injectable } from '@opensumi/di';
+import { Injectable, Autowired } from '@opensumi/di';
 import { IClipboardService, CLIPBOARD_FILE_TOKEN } from '@opensumi/ide-core-common';
 import { URI } from '@opensumi/ide-core-common';
+import { IElectronMainUIService } from '@opensumi/ide-core-common/lib/electron';
 
 export { CLIPBOARD_FILE_TOKEN };
 
-const { clipboard } = require('electron');
 export const INativeClipboardService = Symbol('INativeClipboardService');
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface INativeClipboardService extends IClipboardService {}
+
 @Injectable()
-export class ElectronClipboardService implements IClipboardService {
+export class ElectronClipboardService implements INativeClipboardService {
+  @Autowired(IElectronMainUIService)
+  private electronMainUIService: IElectronMainUIService;
+
   async writeText(text: string): Promise<void> {
-    return clipboard.writeText(text);
+    return await this.electronMainUIService.writeClipboardText(text);
   }
   async readText(): Promise<string> {
-    return clipboard.readText();
+    return await this.electronMainUIService.readClipboardText();
   }
   async writeResources(resources: URI[], field = CLIPBOARD_FILE_TOKEN): Promise<void> {
     try {
       const buffer = Buffer.from(JSON.stringify(resources), 'utf8');
-      return clipboard.writeBuffer(field, Buffer.from(buffer));
+      return await this.electronMainUIService.writeClipboardBuffer(field, Buffer.from(buffer));
     } catch {}
   }
   async readResources(field = CLIPBOARD_FILE_TOKEN): Promise<URI[]> {
     try {
-      const list = clipboard.readBuffer(field).toJSON().data;
+      const list = Buffer.from(await this.electronMainUIService.readClipboardBuffer(field)).toJSON().data;
       if (
         !Array.isArray(list) ||
         !list.length ||
--- packages/core-browser/src/services/index.ts
+++ packages/core-browser/src/services/index.ts
@@ -2,6 +2,7 @@ export * from './label-service';
 export * from './storage-service';
 export * from './status-bar-service';
 export * from './clipboard.service';
+export * from './electron-clipboard.service';
 export * from './external-uri.service';
 export * from './credentials-service';
 export * from './crypto-service';
