466d8ef93a7641f6992a773d2e3e087e06460915
songhn233 <songhn233@gmail.com>
2022-09-12 05:05:37 +0800

    test: clipboard resourse

--- packages/core-browser/__tests__/services/clipboard.service.test.ts
+++ packages/core-browser/__tests__/services/clipboard.service.test.ts
@@ -1,4 +1,4 @@
-import { ILoggerManagerClient } from '@opensumi/ide-core-common';
+import { URI } from '@opensumi/ide-core-common';
 
 import { createBrowserInjector } from '../../../../tools/dev-tool/src/injector-helper';
 import { MockInjector } from '../../../../tools/dev-tool/src/mock-injector';
@@ -16,5 +16,14 @@ describe('clipboard service test', () => {
     await clipboardService.writeText('test');
     const text = await clipboardService.readText();
     expect(text).toBe('test');
+  });
+
+  it('read resouce', async () => {
+    expect(await clipboardService.readResources()).toEqual([]);
+    await clipboardService.writeResources([new URI('test')]);
+    await clipboardService.writeResources([undefined] as any);
+    expect(await clipboardService.hasResources()).toBeTruthy();
+    const resources = await clipboardService.readResources();
+    expect(resources?.[0].codeUri.path).toEqual('/test');
   });
 });
