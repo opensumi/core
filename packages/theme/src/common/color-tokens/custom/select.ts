d3c774e90bbf0b40e62dc284f88f3cd08c286782
Dan <danwu.wdw@alibaba-inc.com>
2022-09-20 21:40:27 +0800

    style: improve select component and global color tokens style (#1683)

--- packages/theme/src/common/color-tokens/custom/select.ts
+++ packages/theme/src/common/color-tokens/custom/select.ts
@@ -1,18 +1,33 @@
 import { localize } from '@opensumi/ide-core-common';
 
-import { registerColor } from '../../utils';
-import { dropdownBackground, dropdownBorder } from '../dropdown';
-import { inputForeground, inputBackground } from '../input';
+import { darken, lighten, registerColor } from '../../utils';
+import { dropdownBackground, dropdownBorder, dropdownForeground } from '../dropdown';
+import {
+  inputForeground,
+  inputBackground,
+  inputPlaceholderForeground,
+  inputOptionActiveBorder,
+  inputOptionActiveBackground,
+} from '../input';
+import {
+  listActiveSelectionBackground,
+  listActiveSelectionForeground,
+  listErrorForeground,
+  listHoverBackground,
+  listInactiveSelectionBackground,
+  listInactiveSelectionForeground,
+  listWarningForeground,
+} from '../list-tree';
 import { settingsSelectBorder } from '../settings';
 
 export const ktSelectForeground = registerColor(
   'kt.select.foreground',
-  { dark: '#D7DBDE', light: inputForeground, hc: null },
+  { dark: inputForeground, light: inputForeground, hc: inputForeground },
   localize('ktSelectForeground', 'Select Foreground color.'),
 );
 export const ktSelectBackground = registerColor(
   'kt.select.background',
-  { dark: '#00000040', light: inputBackground, hc: null },
+  { dark: inputBackground, light: inputBackground, hc: inputBackground },
   localize('ktSelectBackground', 'Select Background color.'),
 );
 export const ktSelectBorder = registerColor(
@@ -22,40 +37,53 @@ export const ktSelectBorder = registerColor(
 );
 export const ktSelectPlaceholderForeground = registerColor(
   'kt.select.placeholderForeground',
-  { dark: '#5F656B', light: '#5F656B', hc: null },
+  { dark: inputPlaceholderForeground, light: inputPlaceholderForeground, hc: inputPlaceholderForeground },
   localize('ktSelectPlaceholder', 'Select Placeholder Foreground color.'),
 );
 export const ktSelectDisableBackground = registerColor(
   'kt.select.disableBackground',
-  { dark: '#5F656B40', light: '#5F656B40', hc: null },
+  {
+    dark: listInactiveSelectionBackground,
+    light: listInactiveSelectionBackground,
+    hc: listInactiveSelectionBackground,
+  },
   localize('ktSelectDisableBackground', 'Select Disable Background color.'),
 );
 export const ktSelectDisableForeground = registerColor(
   'kt.select.disableForeground',
-  { dark: '#5F656B', light: '#5F656B', hc: null },
+  {
+    dark: listInactiveSelectionForeground,
+    light: listInactiveSelectionForeground,
+    hc: listInactiveSelectionForeground,
+  },
   localize('ktSelectDisableForeground', 'Select Disable Foreground color.'),
 );
-export const ktSelectWarningColor = registerColor(
-  'kt.select.warningColor',
-  { dark: '#D77915', light: '#D77915', hc: null },
-  localize('ktSelectWarningColor', 'Select Warning Color.'),
+export const ktSelectWarningForeground = registerColor(
+  'kt.select.warningForeground',
+  { dark: listWarningForeground, light: listWarningForeground, hc: listWarningForeground },
+  localize('ktSelectWarningForeground', 'Select Warning Foreground.'),
+);
+export const ktSelectErrorForeground = registerColor(
+  'kt.select.warningForeground',
+  { dark: listErrorForeground, light: listErrorForeground, hc: listErrorForeground },
+  localize('ktSelectErrorForeground', 'Select Error Foreground.'),
 );
 
 export const ktSelectOptionActiveBackground = registerColor(
   'selectOption.activeBackground',
-  { dark: '#00000040', light: '#FFFFFF40', hc: null },
+  { dark: inputOptionActiveBackground, light: inputOptionActiveBackground, hc: inputOptionActiveBackground },
   localize('ktSelectOptionActiveBackground', 'Select Option Active Background color.'),
 );
 export const ktSelectOptionActiveBorder = registerColor(
   'kt.selectOption.activeBorder',
-  { dark: '#167CDB', light: '#167CDB', hc: null },
+  { dark: inputOptionActiveBorder, light: inputOptionActiveBorder, hc: inputOptionActiveBorder },
   localize('ktSelectOptionActiveBorder', 'Select Option Active Border color.'),
 );
 
 /* select dropdown */
 export const ktSelectDropdownForeground = registerColor(
   'kt.selectDropdown.foreground',
-  { dark: '#D7DBDE', light: inputForeground, hc: null },
+  { dark: dropdownForeground, light: dropdownForeground, hc: dropdownForeground },
   localize('ktSelectDropdownForeground', 'Select Dropdown Foreground color.'),
 );
 export const ktSelectDropdownBackground = registerColor(
@@ -65,16 +93,21 @@ export const ktSelectDropdownBackground = registerColor(
 );
 export const ktSelectDropdownHoverBackground = registerColor(
   'kt.selectDropdown.hoverBackground',
-  { dark: '#5F656B40', light: '#5F656B40', hc: null },
+  { dark: listHoverBackground, light: listHoverBackground, hc: listHoverBackground },
   localize('ktSelectDropdownHoverBackground', 'Select Dropdown Hover Background color.'),
 );
 export const ktSelectDropdownSelectionBackground = registerColor(
   'kt.selectDropdown.selectionBackground',
-  { dark: '#203E5A', light: '#5F656B40', hc: null },
+  { dark: listActiveSelectionBackground, light: listActiveSelectionBackground, hc: listActiveSelectionBackground },
   localize('ktSelectDropdownSelectionBackground', 'Select Dropdown Selection Background color.'),
+);
+export const ktSelectDropdownSelectionForeground = registerColor(
+  'kt.selectDropdown.selectionForeground',
+  { dark: listActiveSelectionForeground, light: listActiveSelectionForeground, hc: listActiveSelectionForeground },
+  localize('ktSelectDropdownSelectionForeground', 'Select Dropdown Selection Foreground color.'),
 );
 export const ktSelectDropdownTeamForeground = registerColor(
   'kt.selectDropdown.teamForeground',
-  { dark: '#868C91', light: '#999999', hc: null },
+  { dark: darken(ktSelectDropdownBackground, 0.2), light: lighten(ktSelectDropdownBackground, 0.2), hc: null },
   localize('ktSelectDropdownSelectionBackground', 'Select Dropdown Selection Background color.'),
 );
