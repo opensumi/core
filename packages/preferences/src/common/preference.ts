a665716445c04805596b01e27bde9f4897939954
野声 <lijiacheng.ljc@antgroup.com>
2022-09-20 17:08:14 +0800

    feat: make more readable

--- packages/preferences/src/common/preference.ts
+++ packages/preferences/src/common/preference.ts
@@ -2,14 +2,35 @@ import { IPreferenceViewDesc, localize } from '@opensumi/ide-core-browser';
 
 export const PREF_SCHEME = 'pref';
 
+export const knownTermMappings = new Map<string, string>();
+knownTermMappings.set('power shell', 'PowerShell');
+knownTermMappings.set('powershell', 'PowerShell');
+knownTermMappings.set('javascript', 'JavaScript');
+knownTermMappings.set('typescript', 'TypeScript');
+
+['css', 'html', 'scss', 'less', 'json', 'js', 'ts', 'ie', 'id', 'php', 'scm', 'npm'].forEach((v) => {
+  knownTermMappings.set(v, v.toUpperCase());
+});
+
+function transformFirstTerm(term: string): string {
+  const termLowerCase = term.toLowerCase();
+  if (knownTermMappings.has(termLowerCase)) {
+    return knownTermMappings.get(termLowerCase)!;
+  }
+  return toNormalCase(term);
+}
+
 export function toPreferenceReadableName(name: string) {
   const parts = name.split('.');
-  let result = toNormalCase(parts[0]);
+  let result = transformFirstTerm(parts[0]);
   if (parts[1]) {
     result += ' > ' + toNormalCase(parts[1]);
   }
   if (parts[2]) {
     result += ' : ' + toNormalCase(parts[2]);
+  }
+  if (parts.slice(3).length > 0) {
+    result += '. ' + parts.slice(3).join(' ');
   }
   return result;
 }
