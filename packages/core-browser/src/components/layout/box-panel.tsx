477b555b9af90c52abdef350dfccf8792f614ab9
野声 <lijiacheng.ljc@antgroup.com>
2022-09-22 15:25:06 +0800

    feat: use data attributes

--- packages/core-browser/src/components/layout/box-panel.tsx
+++ packages/core-browser/src/components/layout/box-panel.tsx
@@ -5,14 +5,46 @@ import { Layout } from './layout';
 import styles from './styles.module.less';
 
 export interface IChildComponentProps {
+  /**
+   * 创建的 div 标签样式中要使用的 flex 属性
+   */
   flex?: number;
+  /**
+   * 创建的 div 标签样式中要使用的默认宽度
+   */
   defaultSize?: number;
   id: string;
   overflow: string;
+  /**
+   * 创建的 div 标签样式中要使用的 zIndex 属性
+   */
+  zIndex?: number;
+  /**
+   * 创建的 div 标签样式中要使用的 z-index 属性
+   */
+  'z-index'?: number;
+  /**
+   * 创建的 div 标签样式中要使用的 backgroundColor 属性
+   */
+  backgroundColor?: string;
+
+  // 以上都是老代码，直接取 props 的相关属性太直接了
+  // 优雅一些的做法是使用 data- 标签
+  /**
+   * 创建的 div 标签要使用的 id
+   */
+  'data-wrapper-id'?: string;
+  /**
+   * 创建的 div 标签要使用的 className
+   */
+  'data-wrapper-class'?: string;
 }
 
 type ChildComponent = React.ReactElement<IChildComponentProps>;
 
+/**
+ * 包裹放入其中的元素，并为每个元素创建一个 div
+ */
 export const BoxPanel: React.FC<{
   children?: ChildComponent | ChildComponent[];
   className?: string;
@@ -21,7 +53,7 @@ export const BoxPanel: React.FC<{
   zIndex?: number;
 }> = ({ className, children = [], direction = 'left-to-right', ...restProps }) => {
   // convert children to list
-  const arrayChildren = React.Children.toArray(children);
+  const arrayChildren = React.Children.toArray(children) as ChildComponent[];
 
   return (
     <div
@@ -32,7 +64,8 @@ export const BoxPanel: React.FC<{
       {arrayChildren.map((child, index) => (
         <div
           key={index}
-          className={clsx(styles.wrapper, child['props']?.className)}
+          id={child['props']?.['data-wrapper-id']}
+          className={clsx(styles.wrapper, child['props']?.['data-wrapper-class'])}
           style={
             child['props']
               ? {
