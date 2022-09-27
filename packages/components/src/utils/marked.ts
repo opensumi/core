5752352af15023838b1b71da3fb50bbfa5eb8c62
yantze <yutian.yz@alibaba-inc.com>
2022-09-27 15:27:15 +0800

    fix: preferences extension section render fail

--- packages/components/src/utils/marked.ts
+++ packages/components/src/utils/marked.ts
@@ -1,11 +1,20 @@
-import { marked } from 'marked';
+import { marked, Renderer } from 'marked';
 
-export const createMarkedRenderer = () => new marked.Renderer();
+export const createMarkedRenderer = () => new Renderer();
 
 export const parseMarkdown = (
   value: string,
   options?: marked.MarkedOptions,
   callback?: (error: any, parseResult: string) => void,
-) => marked.parse(value, options, callback);
+) => {
+  if (!callback) {
+    return marked.parse(value, options);
+  }
+  if (options) {
+    marked.parse(value, options, callback);
+  } else {
+    marked.parse(value, callback);
+  }
+};
 
 export const toMarkdownHtml = (value: string, options?: marked.MarkedOptions) => marked(value, options);
