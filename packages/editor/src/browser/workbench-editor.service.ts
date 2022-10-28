3c90309aca22a1e7245d4ed9fd7050a9b1115f50
大表哥 <xubing.bxb@alibaba-inc.com>
2022-10-28 16:24:38 +0800

    refactor: parallel open resource (#1873)

--- packages/editor/src/browser/workbench-editor.service.ts
+++ packages/editor/src/browser/workbench-editor.service.ts
@@ -2004,9 +2004,17 @@ export class EditorGroup extends WithEventBus implements IGridEditorGroup {
   async restoreState(state: IEditorGroupState) {
     this._restoringState = true;
     this.previewURI = state.uris[state.previewIndex] ? new URI(state.uris[state.previewIndex]) : null;
-    for (const uri of state.uris) {
-      await this.doOpen(new URI(uri), { disableNavigate: true, backend: true, preview: false, deletedPolicy: 'skip' });
-    }
+    await Promise.all(
+      state.uris.map(async (uri) => {
+        await this.doOpen(new URI(uri), {
+          disableNavigate: true,
+          backend: true,
+          preview: false,
+          deletedPolicy: 'skip',
+        });
+      }),
+    );
+
     let targetUri: URI | undefined;
     if (state.current) {
       targetUri = new URI(state.current);
