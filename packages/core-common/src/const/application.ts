0789aac8a3800f22ebcba1ad396e5f42c8365ec0
Dan <danwu.wdw@alibaba-inc.com>
2022-11-02 16:18:19 +0800

    chore: use stable extension marketplace registry (#1886)

--- packages/core-common/src/const/application.ts
+++ packages/core-common/src/const/application.ts
@@ -2,5 +2,5 @@ export const DEFAULT_APPLICATION_NAME = 'OpenSumi';
 export const DEFAULT_APPLICATION_DESKTOP_HOST = 'desktop';
 export const DEFAULT_APPLICATION_WEB_HOST = 'web';
 export const DEFAULT_URI_SCHEME = 'sumi';
-export const DEFAULT_OPENVSX_REGISTRY = 'https://marketplace.smartide.cn'; // China Mirror
-// export const DEFAULT_OPENVSX_REGISTRY = 'https://open-vsx.org'; // Official Registry
+// export const DEFAULT_OPENVSX_REGISTRY = 'https://marketplace.smartide.cn'; // China Mirror
+export const DEFAULT_OPENVSX_REGISTRY = 'https://open-vsx.org'; // Official Registry
