88ec3137e6acd48a2ffe6f2c2b01c167073fef72
野声 <lijiacheng.ljc@antgroup.com>
2022-11-11 00:34:31 +0800

    ci: do not cache windows

--- scripts/actions/cache.js
+++ scripts/actions/cache.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const { TEMP_DIR } = require('./constants');
+try {
+  if (!fs.existsSync(TEMP_DIR)) {
+    fs.mkdirSync(TEMP_DIR);
+  }
 
-if (!fs.existsSync(TEMP_DIR)) {
-  fs.mkdirSync(TEMP_DIR);
+  fs.copyFileSync('yarn.lock', path.join(TEMP_DIR, 'yarn.lock'));
+} catch (e) {
+  console.log('err', e);
 }
-
-fs.copyFileSync('yarn.lock', path.join(TEMP_DIR, 'yarn.lock'));
--- scripts/actions/restore-cache.js
+++ scripts/actions/restore-cache.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const { TEMP_DIR } = require('./constants');
+try {
+  if (!fs.existsSync(TEMP_DIR)) {
+    fs.mkdirSync(TEMP_DIR);
+  }
 
-if (!fs.existsSync(TEMP_DIR)) {
-  fs.mkdirSync(TEMP_DIR);
+  fs.copyFileSync(path.join(TEMP_DIR, 'yarn.lock'), 'yarn.lock');
+} catch (e) {
+  console.log('err', e);
 }
-
-fs.copyFileSync(path.join(TEMP_DIR, 'yarn.lock'), 'yarn.lock');
