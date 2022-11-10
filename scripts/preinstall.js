70188ad49009db85efcc7b224a51bae732def73d
野声 <lijiacheng.ljc@antgroup.com>
2022-11-10 23:54:58 +0800

    chore: use yarn@3

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,6 +1,7 @@
 let err = false;
 
-if (!/yarn[\w-.]*\.c?js$|yarnpkg$/.test(process.env['npm_execpath'])) {
+console.log(`🚀 ~ file: preinstall.js ~ line 4 ~ process.env['npm_execpath']`, process.env['npm_execpath']);
+if (!/yarn$|yarn[\w-.]*\.c?js$|yarnpkg$/.test(process.env['npm_execpath'])) {
   console.error('\033[1;31mPlease use yarn to install dependencies.\033[0;0m');
   err = true;
 }
