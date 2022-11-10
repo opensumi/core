9fee10459c1f459a9ba6cfdf47cefca7e0ab5d9f
野声 <lijiacheng.ljc@antgroup.com>
2022-11-10 22:01:24 +0800

    build: add preinstall check

---
+++ scripts/preinstall.js
@@ -0,0 +1,11 @@
+let err = false;
+
+if (!/yarn[\w-.]*\.c?js$|yarnpkg$/.test(process.env['npm_execpath'])) {
+  console.error('\033[1;31mPlease use yarn to install dependencies.\033[0;0m');
+  err = true;
+}
+
+if (err) {
+  console.error('');
+  process.exit(1);
+}
