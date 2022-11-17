04d01b4d9026d6c07a685bd18311a3a3eba550fa
yantze <yutian.yz@alibaba-inc.com>
2022-11-17 21:53:54 +0800

    refactor: remove vscode-languageserver-protocol

--- packages/extension/src/common/vscode/model.api.ts
+++ packages/extension/src/common/vscode/model.api.ts
@@ -2,6 +2,7 @@ import type vscode from 'vscode';
 import { SymbolInformation } from 'vscode-languageserver-types';
 
 import {
+  isString,
   Uri as URI,
   IRange,
   IDisposable,
@@ -610,6 +611,56 @@ export interface SignatureHelpResult extends IDisposable {
 export interface RenameLocation {
   range: Range;
   text: string;
+}
+
+/**
+ * A document filter denotes a document by different properties like
+ * the {@link TextDocument.languageId language}, the {@link Uri.scheme scheme} of
+ * its resource, or a glob-pattern that is applied to the {@link TextDocument.fileName path}.
+ *
+ * Glob patterns can have the following syntax:
+ * - `*` to match one or more characters in a path segment
+ * - `?` to match on one character in a path segment
+ * - `**` to match any number of path segments, including none
+ * - `{}` to group sub patterns into an OR expression. (e.g. `**​/*.{ts,js}` matches all TypeScript and JavaScript files)
+ * - `[]` to declare a range of characters to match in a path segment (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
+ * - `[!...]` to negate a range of characters to match in a path segment (e.g., `example.[!0-9]` to match on `example.a`, `example.b`, but not `example.0`)
+ *
+ * @sample A language filter that applies to typescript files on disk: `{ language: 'typescript', scheme: 'file' }`
+ * @sample A language filter that applies to all package.json paths: `{ language: 'json', pattern: '**package.json' }`
+ */
+export type TextDocumentFilter =
+  | {
+      /** A language id, like `typescript`. */
+      language: string;
+      /** A Uri {@link Uri.scheme scheme}, like `file` or `untitled`. */
+      scheme?: string;
+      /** A glob pattern, like `*.{ts,js}`. */
+      pattern?: string;
+    }
+  | {
+      /** A language id, like `typescript`. */
+      language?: string;
+      /** A Uri {@link Uri.scheme scheme}, like `file` or `untitled`. */
+      scheme: string;
+      /** A glob pattern, like `*.{ts,js}`. */
+      pattern?: string;
+    }
+  | {
+      /** A language id, like `typescript`. */
+      language?: string;
+      /** A Uri {@link Uri.scheme scheme}, like `file` or `untitled`. */
+      scheme?: string;
+      /** A glob pattern, like `*.{ts,js}`. */
+      pattern: string;
+    };
+
+/**
+ * From vscode-languageserver-node/protocol/src/common/protocol.ts
+ */
+export function isDocumentFilter(value: any): value is TextDocumentFilter {
+  const candidate: TextDocumentFilter = value;
+  return isString(candidate.language) || isString(candidate.scheme) || isString(candidate.pattern);
 }
 
 export interface Rejection {
