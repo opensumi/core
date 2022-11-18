4a583ea12b25b0a24aa4c3634215864134437ebe
野声 <lijiacheng.ljc@antgroup.com>
2022-11-18 15:28:47 +0800

    ci: update wait script

--- tools/playwright/scripts/wait.sh
+++ tools/playwright/scripts/wait.sh
@@ -2,15 +2,13 @@
 attempt_counter=0
 max_attempts=10
 
-printf 'waiting for http://localhost:8080 respond'
-
 until $(curl --output /dev/null --silent --head --fail http://localhost:8080); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "\nMax attempts reached"
       exit 1
     fi
 
-    printf '.'
+    printf 'waiting for http://localhost:8080 respond.\n'
     attempt_counter=$(($attempt_counter+1))
     sleep 2
 done
