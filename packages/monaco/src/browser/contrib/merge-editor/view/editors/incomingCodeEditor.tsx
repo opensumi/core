3cb84f4ea13ee307c4993ee833a2b517c028a934
John <qingyi.xjh@antgroup.com>
2022-11-18 11:12:40 +0800

    feat: implement inner changes decoration render (#1987)
    * fix: result editor view line widget render error
    
    * feat: implement inner changes char decoration render
    
    * chore: improve code

--- packages/monaco/src/browser/contrib/merge-editor/view/editors/incomingCodeEditor.tsx
+++ packages/monaco/src/browser/contrib/merge-editor/view/editors/incomingCodeEditor.tsx
@@ -1,6 +1,6 @@
 import { Injectable, Autowired, Injector, INJECTOR_TOKEN } from '@opensumi/di';
 import { Range } from '@opensumi/monaco-editor-core/esm/vs/editor/common/core/range';
-import { LineRange, LineRangeMapping } from '@opensumi/monaco-editor-core/esm/vs/editor/common/diff/linesDiffComputer';
+import { LineRangeMapping } from '@opensumi/monaco-editor-core/esm/vs/editor/common/diff/linesDiffComputer';
 import { IStandaloneEditorConstructionOptions } from '@opensumi/monaco-editor-core/esm/vs/editor/standalone/browser/standaloneCodeEditor';
 
 import {
@@ -10,6 +10,7 @@ import {
   MergeEditorDecorations,
 } from '../../model/decorations';
 import { GuidelineWidget } from '../../model/line';
+import { LineRange } from '../../model/line-range';
 import { flatInnerModified, flatModified, flatOriginal, flatInnerOriginal } from '../../utils';
 
 import { BaseCodeEditor } from './baseCodeEditor';
@@ -20,42 +21,7 @@ export class IncomingCodeEditor extends BaseCodeEditor {
     return { readOnly: true };
   }
 
-  private rangeMapping: LineRangeMapping[] = [];
-
-  protected prepareRenderDecorations(
-    ranges: LineRange[],
-    innerChanges: Range[],
-  ): [IRenderChangesInput[], IRenderInnerChangesInput[]] {
-    const [originalRanges, innerOriginalRanges] = [
-      flatOriginal(this.rangeMapping),
-      flatInnerOriginal(this.rangeMapping),
-    ];
-    const length = ranges.length;
-
-    const changesResult: IRenderChangesInput[] = [];
-    const innerChangesResult: IRenderInnerChangesInput[] = [];
-
-    for (let i = 0; i < length; i++) {
-      if (ranges[i].isEmpty && !originalRanges[i].isEmpty) {
-        changesResult.push({
-          ranges: ranges[i],
-          type: 'remove',
-        });
-      } else if (!ranges[i].isEmpty && originalRanges[i].isEmpty) {
-        changesResult.push({
-          ranges: ranges[i],
-          type: 'insert',
-        });
-      } else {
-        changesResult.push({
-          ranges: ranges[i],
-          type: 'modify',
-        });
-      }
-    }
-
-    return [changesResult, innerChangesResult];
-  }
+  protected computeResultRangeMapping: LineRangeMapping[] = [];
 
   protected getRetainDecoration(): IDiffDecoration[] {
     return [];
@@ -66,10 +32,10 @@ export class IncomingCodeEditor extends BaseCodeEditor {
   }
 
   public inputDiffComputingResult(changes: LineRangeMapping[]): void {
-    this.rangeMapping = changes;
+    this.computeResultRangeMapping = changes;
 
     const [c, i] = [flatModified(changes), flatInnerModified(changes)];
     this.renderDecorations(c, i);
-    this.rangeMapping = [];
+    this.computeResultRangeMapping = [];
   }
 }
