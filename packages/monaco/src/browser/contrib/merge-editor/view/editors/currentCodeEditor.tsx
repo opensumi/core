7a725a81fc82ef29a94ced760fb3fe16ad244209
John <qingyi.xjh@antgroup.com>
2022-11-23 11:29:41 +0800

    feat: render conflict actions (#2002)
    * feat: implement conflict actions base code
    
    * chore: improve code

--- packages/monaco/src/browser/contrib/merge-editor/view/editors/currentCodeEditor.tsx
+++ packages/monaco/src/browser/contrib/merge-editor/view/editors/currentCodeEditor.tsx
@@ -1,4 +1,5 @@
 import { Injectable } from '@opensumi/di';
+import { IEditorMouseEvent, MouseTargetType } from '@opensumi/monaco-editor-core/esm/vs/editor/browser/editorBrowser';
 import { Margin } from '@opensumi/monaco-editor-core/esm/vs/editor/browser/viewParts/margin/margin';
 import { Range } from '@opensumi/monaco-editor-core/esm/vs/editor/common/core/range';
 import { LineRangeMapping } from '@opensumi/monaco-editor-core/esm/vs/editor/common/diff/linesDiffComputer';
@@ -7,19 +8,21 @@ import { IStandaloneEditorConstructionOptions } from '@opensumi/monaco-editor-co
 
 import { IDiffDecoration } from '../../model/decorations';
 import { LineRange } from '../../model/line-range';
-import { EditorViewType } from '../../types';
+import { ACCEPT_CURRENT, CONFLICT_ACTIONS_ICON, EditorViewType, IGNORE } from '../../types';
 import { flatInnerOriginal, flatOriginal } from '../../utils';
 import { GuidelineWidget } from '../guideline-widget';
 
 import { BaseCodeEditor } from './baseCodeEditor';
 
+// 用来寻址点击事件时的标记
+const ADDRESSING_TAG_CLASSNAME = 'ADDRESSING_TAG_CLASSNAME_';
 
 @Injectable({ multiple: false })
 export class CurrentCodeEditor extends BaseCodeEditor {
   public computeResultRangeMapping: LineRangeMapping[] = [];
 
   protected getMonacoEditorOptions(): IStandaloneEditorConstructionOptions {
-    return { readOnly: true };
+    return { readOnly: true, lineNumbersMinChars: 5 };
   }
 
   protected getRetainDecoration(): IDiffDecoration[] {
@@ -32,6 +35,28 @@ export class CurrentCodeEditor extends BaseCodeEditor {
 
   protected override prepareRenderDecorations(ranges: LineRange[], innerChanges: Range[][]) {
     return super.prepareRenderDecorations(ranges, innerChanges, 1);
+  }
+
+  private onActionsClick(e: IEditorMouseEvent): void {
+    const element = e.target.element!;
+
+    if (element.classList.contains(ACCEPT_CURRENT)) {
+      const toArry = Array.from(element.classList);
+      const find = toArry.find((c) => c.startsWith(ADDRESSING_TAG_CLASSNAME));
+      if (find) {
+        const posiLine = Number(find.replace(ADDRESSING_TAG_CLASSNAME, ''));
+        if (typeof posiLine === 'number') {
+          // not implement
+        }
+      }
+
+      return;
+    }
+
+    if (element.classList.contains(IGNORE)) {
+      // not implement
+      return;
+    }
   }
 
   public getMonacoDecorationOptions(
@@ -49,9 +74,42 @@ export class CurrentCodeEditor extends BaseCodeEditor {
   public inputDiffComputingResult(changes: LineRangeMapping[]): void {
     this.computeResultRangeMapping = changes;
 
-    const [c, i] = [flatOriginal(changes), flatInnerOriginal(changes)];
+    const [ranges, innerRanges] = [flatOriginal(changes), flatInnerOriginal(changes)];
 
-    this.renderDecorations(c, i);
+    this.renderDecorations(ranges, innerRanges);
+
+    this.registerActionsProvider({
+      provideActionsItems: () =>
+        ranges.map((range) => {
+          const posiMark = `${ADDRESSING_TAG_CLASSNAME}${range.startLineNumber}`;
+          return {
+            range,
+            decorationOptions: {
+              description: 'current editor view conflict actions',
+              glyphMarginClassName: CONFLICT_ACTIONS_ICON.RIGHT + ` offset-left ${posiMark}`,
+              marginClassName: CONFLICT_ACTIONS_ICON.CLOSE + ` ${posiMark}`,
+            },
+          };
+        }),
+      mouseDownGuard: (e: IEditorMouseEvent) => {
+        /**
+         * 注: 由于 current view 视图已经将 margin 区域和 code 区域交换了
+         * 导致 editor mousedown 在处理点击事件的时候无法通过内部逻辑找到 target type 和 position 等信息
+         * 进而导致没法知道点击了哪个位置上的 actions 图标
+         * 所以这里通过 ADDRESSING_TAG_CLASSNAME 标志符加 lineNumber 给 className 类名的方式，来找到具体点击了哪个 actions
+         */
+        if (!(e.target.type === MouseTargetType.UNKNOWN) || e.event.rightButton) {
+          return false;
+        }
+
+        if (!e.target.element) {
+          return false;
+        }
+
+        return true;
+      },
+      onActionsClick: this.onActionsClick,
+    });
 
     this.layout();
   }
