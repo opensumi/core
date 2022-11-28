6202bea49e79a826a3e088157476dca20aa6ba2f
kuiwu <danwu.wdw@alibaba-inc.com>
2022-11-28 18:38:25 +0800

    fix: support onSaveCodeActions with ESLint extension

--- packages/editor/src/browser/doc-model/saveParticipants.ts
+++ packages/editor/src/browser/doc-model/saveParticipants.ts
@@ -6,16 +6,21 @@ import {
   OnEvent,
   PreferenceService,
   CommandService,
-  MonacoService,
   ServiceNames,
   ILogger,
   MonacoOverrideServiceRegistry,
-  Progress,
+  IProgress,
+  ProgressLocation,
+  IProgressStep,
+  formatLocalize,
 } from '@opensumi/ide-core-browser';
+import { IProgressService } from '@opensumi/ide-core-browser/lib/progress';
+import {} from '@opensumi/ide-extension/lib/common/vscode/converter';
 import { ResourceEdit } from '@opensumi/ide-monaco/lib/browser/monaco-api';
 import { languageFeaturesService } from '@opensumi/ide-monaco/lib/browser/monaco-api/languages';
 import { ITextModel } from '@opensumi/ide-monaco/lib/browser/monaco-api/types';
 import * as languages from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages';
+import { CodeActionProvider } from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages';
 import {
   getCodeActions,
   CodeActionItem,
@@ -33,26 +38,26 @@ import { EditorDocumentModelWillSaveEvent, IEditorDocumentModelService } from '.
 @Injectable()
 export class CodeActionOnSaveParticipant extends WithEventBus {
   @Autowired(PreferenceService)
-  preferenceService: PreferenceService;
-
-  @Autowired(MonacoService)
-  monacoService: MonacoService;
+  private readonly preferenceService: PreferenceService;
 
   @Autowired(MonacoOverrideServiceRegistry)
   private readonly overrideServiceRegistry: MonacoOverrideServiceRegistry;
 
+  @Autowired(CommandService)
+  private readonly commandService: CommandService;
+
+  @Autowired(IEditorDocumentModelService)
+  private readonly docService: IEditorDocumentModelService;
+
+  @Autowired(ILogger)
+  private readonly logger: ILogger;
+
+  @Autowired(IProgressService)
+  private readonly progressService: IProgressService;
+
   get bulkEditService(): any {
     return this.overrideServiceRegistry.getRegisteredService(ServiceNames.BULK_EDIT_SERVICE);
   }
-
-  @Autowired(CommandService)
-  commandService: CommandService;
-
-  @Autowired(IEditorDocumentModelService)
-  docService: IEditorDocumentModelService;
-
-  @Autowired(ILogger)
-  logger: ILogger;
 
   activate() {
     // noop
@@ -65,66 +70,108 @@ export class CodeActionOnSaveParticipant extends WithEventBus {
       return;
     }
 
-    const preferenceActions = this.preferenceService.get<{
-      [prop: string]: any;
-    }>('editor.codeActionsOnSave', undefined, e.payload.uri.toString(), e.payload.language);
-    if (!preferenceActions) {
-      return undefined;
-    }
-    const codeActionsOnSave: CodeActionKind[] = [];
+    return this.progressService.withProgress(
+      {
+        title: formatLocalize('editor.saveActions.saveing', e.payload.uri.displayName),
+        location: ProgressLocation.Notification,
+      },
+      async (progress) => {
+        const preferenceActions = this.preferenceService.get<
+          | {
+              [prop: string]: any;
+            }
+          | string[]
+        >('editor.codeActionsOnSave', undefined, e.payload.uri.toString(), e.payload.language);
+        if (!preferenceActions) {
+          return undefined;
+        }
 
-    for (const k of Object.keys(preferenceActions).map((p) => new CodeActionKind(p))) {
-      // source.fixAll
-      // source.fixAll.eslint
-      // source.fixAll.stylelint
-      if (CodeActionKind.SourceFixAll.equals(k) || CodeActionKind.SourceFixAll.contains(k)) {
-        codeActionsOnSave.push(CodeActionKind.SourceFixAll);
-      }
+        const codeActions = Array.isArray(preferenceActions) ? preferenceActions : Object.keys(preferenceActions);
+        const codeActionsOnSave: CodeActionKind[] = codeActions.map((p) => new CodeActionKind(p));
 
-      // source.organizeImports
-      if (CodeActionKind.SourceOrganizeImports.equals(k)) {
-        codeActionsOnSave.push(CodeActionKind.SourceOrganizeImports);
-      }
-    }
+        if (!Array.isArray(preferenceActions)) {
+          codeActionsOnSave.sort((a, b) => {
+            if (CodeActionKind.SourceFixAll.contains(a)) {
+              if (CodeActionKind.SourceFixAll.contains(b)) {
+                return 0;
+              }
+              return -1;
+            }
+            if (CodeActionKind.SourceFixAll.contains(b)) {
+              return 1;
+            }
+            return 0;
+          });
+        }
+        if (codeActionsOnSave.length === 0) {
+          return;
+        }
 
-    if (codeActionsOnSave.length === 0) {
-      return;
-    }
+        const modelRef = this.docService.getModelReference(e.payload.uri, 'codeActionOnSave');
+        if (!modelRef) {
+          return;
+        }
 
-    const modelRef = this.docService.getModelReference(e.payload.uri, 'codeActionOnSave');
-    if (!modelRef) {
-      return;
-    }
+        const model = modelRef.instance.getMonacoModel();
 
-    const model = modelRef.instance.getMonacoModel();
+        const tokenSource = new monaco.CancellationTokenSource();
 
-    const tokenSource = new monaco.CancellationTokenSource();
+        const timeout = this.preferenceService.get<number>(
+          'editor.codeActionsOnSaveTimeout',
+          undefined,
+          e.payload.uri.toString(),
+          e.payload.language,
+        );
 
-    const timeout = this.preferenceService.get<number>(
-      'editor.codeActionsOnSaveTimeout',
-      undefined,
-      e.payload.uri.toString(),
-      e.payload.language,
-    );
+        const excludedActions = Array.isArray(preferenceActions)
+          ? []
+          : Object.keys(preferenceActions)
+              .filter((x: string) => preferenceActions[x] === false)
+              .map((x: string) => new CodeActionKind(x));
 
-    return Promise.race([
-      new Promise<void>((_resolve, reject) =>
-        setTimeout(() => {
+        return Promise.race([
+          new Promise<void>((_resolve, reject) =>
+            setTimeout(() => {
+              tokenSource.cancel();
+              reject('codeActionsOnSave timeout');
+            }, timeout),
+          ),
+          this.applyOnSaveActions(model, codeActionsOnSave, excludedActions, progress, tokenSource.token),
+        ]).finally(() => {
           tokenSource.cancel();
-          reject('codeActionsOnSave timeout');
-        }, timeout),
-      ),
-      this.applyOnSaveActions(model, codeActionsOnSave, tokenSource.token),
-    ]).finally(() => {
-      tokenSource.cancel();
-      modelRef.dispose();
-    });
+          modelRef.dispose();
+        });
+      },
+    );
   }
 
-  private async applyOnSaveActions(model: ITextModel, actions: CodeActionKind[], token: monaco.CancellationToken) {
+  private async applyOnSaveActions(
+    model: ITextModel,
+    actions: CodeActionKind[],
+    excludes: readonly CodeActionKind[],
+    progress: IProgress<IProgressStep>,
+    token: monaco.CancellationToken,
+  ) {
+    const getActionProgress = new (class implements IProgress<CodeActionProvider> {
+      private _names = new Set<string>();
+      private _report(): void {
+        progress.report({
+          message: formatLocalize(
+            'editor.saveActions.gettingCodeAction',
+            [...this._names].map((name) => `'${name}'`).join(', '),
+          ),
+        });
+      }
+      report(provider: CodeActionProvider) {
+        if (provider.displayName && !this._names.has(provider.displayName)) {
+          this._names.add(provider.displayName);
+          this._report();
+        }
+      }
+    })();
     for (const codeActionKind of actions) {
       try {
-        const actionsToRun = await this.getActionsToRun(model, codeActionKind, token);
+        const actionsToRun = await this.getActionsToRun(model, codeActionKind, excludes, getActionProgress, token);
 
         await this.applyCodeActions(actionsToRun.validActions);
       } catch (e) {
@@ -147,17 +194,23 @@ export class CodeActionOnSaveParticipant extends WithEventBus {
     }
   }
 
-  private async getActionsToRun(model: ITextModel, codeActionKind: CodeActionKind, token: monaco.CancellationToken) {
+  private async getActionsToRun(
+    model: ITextModel,
+    codeActionKind: CodeActionKind,
+    excludes: readonly CodeActionKind[],
+    progress: IProgress<CodeActionProvider>,
+    token: monaco.CancellationToken,
+  ) {
     return getCodeActions(
       languageFeaturesService.codeActionProvider,
       model,
       model.getFullModelRange(),
       {
         type: languages.CodeActionTriggerType.Auto,
-        filter: { include: codeActionKind, includeSourceActions: true },
+        filter: { include: codeActionKind, excludes, includeSourceActions: true },
         triggerAction: CodeActionTriggerSource.OnSave,
       },
-      Progress.None,
+      progress,
       token,
     );
   }
--- packages/i18n/src/common/zh-CN.lang.ts
+++ packages/i18n/src/common/zh-CN.lang.ts
@@ -501,6 +501,8 @@ export const localizationBundle = {
     'preference.item.notValid': '{0} 不是有效选项',
 
     'editor.saveAll': '保存全部',
+    'editor.saveActions.gettingCodeAction': '从 {0} 中获取 CodeAction',
+    'editor.saveActions.saveing': '保存 "{0}"',
 
     'editor.configuration.wrapTab': '控制当编辑器 Tab 超过可用空间时，是否使用换行来代替滚动模式。',
     'editor.configuration.enablePreviewFromCodeNavigation': '控制当代码导航从其出发时，编辑器是否仍处于预览模式。',
