d006457eea8589c23a17b066213990563bfc2345
kuiwu <danwu.wdw@alibaba-inc.com>
2022-11-29 14:29:13 +0800

    chore: show unhandledRejection error

--- jest.setup.base.js
+++ jest.setup.base.js
@@ -1,11 +1,3 @@
-process.on('unhandledRejection', (error) => {
-  // eslint-disable-next-line no-console
-  console.error('unhandledRejection', error);
-  if (process.env.EXIT_ON_UNHANDLED_REJECTION) {
-    process.exit(1); // To exit with a 'failure' code
-  }
-});
-
 // Do not log message on GitHub Actions.
 // Because these logs will affect the detection of real problems.
 const _console = global.console;
@@ -20,5 +12,12 @@ global.console = process.env.CI
       timeEnd: () => {},
     }
   : _console;
+
+process.on('unhandledRejection', (error) => {
+  _console.error('unhandledRejection', error);
+  if (process.env.EXIT_ON_UNHANDLED_REJECTION) {
+    process.exit(1); // To exit with a 'failure' code
+  }
+});
 
 process.env.IS_JEST_TEST = true;
