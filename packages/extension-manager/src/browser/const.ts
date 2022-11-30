a2f04f09f7ffe4e9f38fba5f9414cc0bfbfb2ffb
o_0 <sijie.zsj@alibaba-inc.com>
2022-11-30 17:12:52 +0800

    feat: remove default extension image (#2030)

--- packages/extension-manager/src/browser/const.ts
+++ packages/extension-manager/src/browser/const.ts
@@ -1,5 +1,3 @@
 import { EXTENSION_CONTAINER_ID } from '@opensumi/ide-core-browser/lib/common/container-id';
 export const OPEN_VSX_EXTENSION_MANAGER_CONTAINER_ID = EXTENSION_CONTAINER_ID;
 export const EXTENSION_SCHEME = 'extension';
-export const DEFAULT_EXTENSION_ICON_URL =
-  'https://marketplace.opentrscdn.com/static/A_TKtCQIToMwgAAAAAAAAAAABkARQnAQ.png';
