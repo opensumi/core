ff303dd9b1c27bee165253246d4f57b5509d4585
kuiwu <danwu.wdw@alibaba-inc.com>
2022-12-01 14:57:01 +0800

    chore: fix test

--- packages/utils/src/encoding.ts
+++ packages/utils/src/encoding.ts
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+// @ts-ignore
 import iconv from 'iconv-lite';
 
 import { BinaryBuffer } from './buffer';
