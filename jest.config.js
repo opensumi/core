f1dc265eb5cb631d1852bcb7c0a2b54332485cc9
kuiwu <danwu.wdw@alibaba-inc.com>
2022-12-05 11:10:54 +0800

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,22 @@ const baseConfig = {
   preset: 'ts-jest',
   testRunner: 'jest-jasmine2',
   resolver: '<rootDir>/tools/dev-tool/src/jest-resolver.js',
-  // coverageProvider: process.env.JEST_COVERAGE_PROVIDER || 'v8',
+  coverageProvider: process.env.JEST_COVERAGE_PROVIDER || 'v8',
   // https://dev.to/vantanev/make-your-jest-tests-up-to-20-faster-by-changing-a-single-setting-i36
   maxWorkers: '50%',
+  collectCoverageFrom: [
+    'packages/*/src/**/*.ts',
+    '!packages/**/*.contribution.ts',
+    // 部分contribution文件为-contribution结尾
+    '!packages/**/*-contribution.ts',
+    '!packages/startup/**/*.ts',
+    // Test 功能暂未完成
+    '!packages/testing/**/*.ts',
+    // CLI 不需要测试
+    '!packages/remote-cli/**/*.ts',
+    '!packages/core-electron-main/**/*.ts',
+    '!packages/*/src/electron-main/**/*.ts',
+  ],
   moduleNameMapper: {
     ...tsModuleNameMapper,
     '.*\\.(css|less)$': '<rootDir>/tools/dev-tool/src/mock-exports.js',
@@ -28,6 +41,14 @@ const baseConfig = {
     '/packages/terminal-next/__tests__/browser/render.test.ts',
   ],
   modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/tools/workspace/'],
+  coveragePathIgnorePatterns: [
+    '/dist/',
+    '/node_modules/',
+    '/mocks/',
+    '/tools/template/',
+    '/tools/workspace/',
+    '/packages/startup/entry',
+  ],
   coverageThreshold: {
     global: {
       branches: 0,
@@ -51,7 +72,7 @@ module.exports = {
       testMatch: [
         // 有个 webview 的 case 应该放在 electron 下测，也会被第一条规则匹配到
         // - packages/webview/__tests__/webview/webview.channel.test.ts
-        '**/packages/*/__test(s)?__/!(browser)/**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/packages/*/__test?(s)__/!(browser)/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/packages/{core-common,core-electron-main,core-node,electron-basic,utils,i18n}/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
         // exclude 的要放最后
         '!**/packages/{components,core-browser}/__tests__/**',
@@ -74,8 +95,8 @@ module.exports = {
       },
       setupFiles: ['./jest.setup.jsdom.js'],
       testMatch: [
-        '**/packages/*/__test(s)?__/browser/**/?(*.)+(spec|test).[jt]s?(x)',
-        '**/packages/*/__test(s)?__/common/**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/packages/*/__test?(s)__/browser/**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/packages/*/__test?(s)__/common/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/tools/*/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/packages/extension/__tests__/hosted/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/packages/{components,core-browser,core-common}/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
