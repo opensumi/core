37ae89cf7f1ede604d5183cc0f0bd4061f023b0c
大表哥 <xubing.bxb@alibaba-inc.com>
2022-12-16 14:04:51 +0800

    fix: active embedded language on demand

--- packages/editor/src/browser/monaco-contrib/tokenizer/textmate-tokenizer.ts
+++ packages/editor/src/browser/monaco-contrib/tokenizer/textmate-tokenizer.ts
@@ -7,22 +7,6 @@ import {
 } from '@opensumi/monaco-editor-core/esm/vs/editor/common/encodedTokenAttributes';
 import * as monaco from '@opensumi/monaco-editor-core/esm/vs/editor/editor.api';
 
-/** ******************************************************************************
- * Copyright (C) 2018 Ericsson and others.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License v. 2.0 are satisfied: GNU General Public License, version 2
- * with the GNU Classpath Exception which is available at
- * https://www.gnu.org/software/classpath/license.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
- ********************************************************************************/
-
 export class TokenizerState implements monaco.languages.IState {
   constructor(public readonly ruleStack: StackElement) {}
 
