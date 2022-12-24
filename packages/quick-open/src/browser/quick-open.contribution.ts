724c31877a79fba2228e209883a44dd70329c17c
Aaaaash <binshao54@gmail.com>
2022-12-24 20:40:51 +0800

    fix: quick open argument

--- packages/quick-open/src/browser/quick-open.contribution.ts
+++ packages/quick-open/src/browser/quick-open.contribution.ts
@@ -55,7 +55,7 @@ export class QuickOpenFeatureContribution
 
   registerCommands(commands: CommandRegistry): void {
     commands.registerCommand(QUICK_OPEN_COMMANDS.OPEN, {
-      execute: (prefix = '>') => this.prefixQuickOpenService.open(prefix),
+      execute: () => this.prefixQuickOpenService.open('>'),
     });
     commands.registerCommand(QUICK_OPEN_COMMANDS.OPEN_OUTLINE, {
       execute: () => this.prefixQuickOpenService.open('@'),
