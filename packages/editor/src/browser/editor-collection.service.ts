7a8cb37cece55d54d29b358e892befe0a5c27bc9
大表哥 <xubing.bxb@alibaba-inc.com>
2022-12-28 13:40:23 +0800

    fix: unnecessary editor update options

--- packages/editor/src/browser/editor-collection.service.ts
+++ packages/editor/src/browser/editor-collection.service.ts
@@ -10,6 +10,7 @@ import {
   ISelection,
   Disposable,
   objects,
+  isEmptyObject,
 } from '@opensumi/ide-core-common';
 import { Emitter } from '@opensumi/ide-core-common';
 import type {
@@ -261,24 +262,7 @@ export abstract class BaseMonacoEditorWrapper extends Disposable implements IEdi
   constructor(public readonly monacoEditor: IMonacoCodeEditor, private type: EditorType) {
     super();
     this.decorationApplier = this.injector.get(MonacoEditorDecorationApplier, [this.monacoEditor]);
-    this.addDispose(
-      this.monacoEditor.onDidChangeModel(() => {
-        this._editorOptionsFromContribution = {};
-        const uri = this.currentUri;
-        if (uri) {
-          Promise.resolve(this.editorFeatureRegistry.runProvideEditorOptionsForUri(uri)).then((option) => {
-            if (!this.currentUri || !uri.isEqual(this.currentUri)) {
-              return; // uri可能已经变了
-            }
-            if (option && Object.keys(option).length > 0) {
-              this._editorOptionsFromContribution = option;
-              this._doUpdateOptions();
-            }
-          });
-        }
-        this._doUpdateOptions();
-      }),
-    );
+    this.addDispose(this.monacoEditor.onDidChangeModel(this.onDidChangeModel.bind(this)));
     this.addDispose(
       this.monacoEditor.onDidChangeModelLanguage(() => {
         this._doUpdateOptions();
@@ -292,6 +276,26 @@ export abstract class BaseMonacoEditorWrapper extends Disposable implements IEdi
         }
       }),
     );
+  }
+
+  private async onDidChangeModel() {
+    const now = Date.now();
+    this._editorOptionsFromContribution = {};
+    const uri = this.currentUri;
+    if (uri) {
+      Promise.resolve(this.editorFeatureRegistry.runProvideEditorOptionsForUri(uri)).then((options) => {
+        if (!this.currentUri || !uri.isEqual(this.currentUri)) {
+          return; // uri可能已经变了
+        }
+
+        if (options && Object.keys(options).length > 0) {
+          this._editorOptionsFromContribution = options;
+          if (!isEmptyObject(this._editorOptionsFromContribution)) {
+            this._doUpdateOptions();
+          }
+        }
+      });
+    }
   }
 
   public getType() {
@@ -490,7 +494,7 @@ export class BrowserCodeEditor extends BaseMonacoEditorWrapper implements ICodeE
     }
   }
 
-  async open(documentModelRef: IEditorDocumentModelRef): Promise<void> {
+  open(documentModelRef: IEditorDocumentModelRef): void {
     this.saveCurrentState();
     this._currentDocumentModelRef = documentModelRef;
     const model = this.currentDocumentModel!.getMonacoModel();
@@ -684,7 +688,7 @@ export class BrowserDiffEditor extends Disposable implements IDiffEditor {
       change.originalEndLineNumber,
       change.modifiedStartLineNumber,
       change.modifiedEndLineNumber,
-      change.charChanges?.map((charChange) => ([
+      change.charChanges?.map((charChange) => [
         charChange.originalStartLineNumber,
         charChange.originalStartColumn,
         charChange.originalEndLineNumber,
@@ -693,7 +697,7 @@ export class BrowserDiffEditor extends Disposable implements IDiffEditor {
         charChange.modifiedStartColumn,
         charChange.modifiedEndLineNumber,
         charChange.modifiedEndColumn,
-      ])),
+      ]),
     ]);
   }
 
