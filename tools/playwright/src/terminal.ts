392f36633163c2001c082609ff33411ccbfb95c0
Dan <danwu.wdw@alibaba-inc.com>
2023-01-03 10:30:44 +0800

    test: add Javascript Debug Terminal test case (#2150)

--- tools/playwright/src/terminal.ts
+++ tools/playwright/src/terminal.ts
@@ -1,5 +1,8 @@
 import { OpenSumiApp } from './app';
+import { OpenSumiContextMenu } from './context-menu';
 import { OpenSumiPanel } from './panel';
+
+type TerminalType = 'bash' | 'zsh' | 'Javascript Debug Terminal';
 
 export class OpenSumiTerminal extends OpenSumiPanel {
   constructor(app: OpenSumiApp) {
@@ -18,5 +21,30 @@ export class OpenSumiTerminal extends OpenSumiPanel {
     }
     await this.page.keyboard.type(text);
     await this.app.page.keyboard.press('Enter');
+  }
+
+  async createTerminalByType(type: TerminalType) {
+    const buttonWrapper = await this.view?.$('[class*="item_wrapper__"]');
+    const buttons = await buttonWrapper?.$$('.kaitian-icon');
+    if (!buttons) {
+      return;
+    }
+    let button;
+    for (const item of buttons) {
+      const title = await item.getAttribute('title');
+      if (title === 'Create terminal by type') {
+        button = item;
+        break;
+      }
+    }
+    if (!button) {
+      return;
+    }
+    await button.click();
+    const menu = new OpenSumiContextMenu(this.app);
+    await menu.waitForVisible();
+    await menu.clickMenuItem(type);
+
+    await this.app.page.waitForTimeout(1000);
   }
 }
