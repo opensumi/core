0a60475397178b32f3f1126d9012f92e81ee53d7
wangxiaojuan <wangxiaojuan19@163.com>
2023-02-10 13:11:08 +0800

    fix: change the text to upper-case

--- packages/i18n/src/common/en-US.lang.ts
+++ packages/i18n/src/common/en-US.lang.ts
@@ -73,7 +73,7 @@ export const localizationBundle = {
     'file.filetree.openTerminalWithPath': 'Open In Integrated Terminal',
     'file.tooltip.symbolicLink': 'Symbolic Link',
     'file.resource-deleted': '(Deleted)',
-    'file.revealInExplorer': 'Reveal in file tree',
+    'file.revealInExplorer': 'Reveal in File Tree',
 
     'menu-bar.title.file': 'File',
     'menu-bar.title.edit': 'Edit',
