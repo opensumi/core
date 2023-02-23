a4541b28869c387c6207488c55bd31eb7b09e74c
wangxiaojuan <wangxiaojuan19@163.com>
2023-02-23 09:47:43 +0800

    fix: 根据PR修改代码逻辑问题

--- packages/editor/src/browser/menu/breadcrumbs.menus.ts
+++ packages/editor/src/browser/menu/breadcrumbs.menus.ts
@@ -17,20 +17,13 @@ export class BreadCrumbsMenuService {
   @Autowired(ICtxMenuRenderer)
   private readonly ctxMenuRenderer: ICtxMenuRenderer;
 
-  private _groupContextKeyService;
-
   show(x: number, y: number, group: EditorGroup, uri: URI) {
-    let titleContext;
-    if (!this._groupContextKeyService) {
-      titleContext = group.contextKeyService.createScoped();
-    }
+    const titleContext = group.contextKeyService;
     const menus = this.ctxMenuService.createMenu({
       id: MenuId.BreadcrumbsTitleContext,
       contextKeyService: titleContext,
     });
     const menuNodes = menus.getMergedMenuNodes();
-    menus.dispose();
-    titleContext.dispose();
 
     this.ctxMenuRenderer.show({
       anchor: { x, y },
