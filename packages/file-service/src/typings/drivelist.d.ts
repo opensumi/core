6f624f42c1145d139d460022ccd3d058fb6e91f0
guqiankun.gqk <guqiankun.gqk@antgroup.com>
2023-02-23 16:14:30 +0800

    chore: remove drivelist

--- packages/file-service/src/typings/drivelist.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'drivelist' {
-  interface Drivelist {
-    list(cb: (error: Error, drives: ({ readonly mountpoints: { readonly path: string }[] })[]) => void): void;
-  }
-
-  const drivelist: Drivelist;
-
-  export = drivelist;
-}
