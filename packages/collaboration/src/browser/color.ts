3c00b2a245bb2bcd19ac62a2bccb7af3adaa6293
winjo <winjowind@163.com>
2023-02-28 18:47:08 +0800

    fix(collaboration): change default color (#2348)

--- packages/collaboration/src/browser/color.ts
+++ packages/collaboration/src/browser/color.ts
@@ -1,4 +1,16 @@
-const color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violent'];
+// 默认配置色
+const defaultColors: [string, string][] = [
+  ['#000000', '#FFB900'],
+  ['#000000', '#00B294'],
+  ['#000000', '#FFF100'],
+  ['#000000', '#B4A0FF'],
+  ['#000000', '#BAD80A'],
+  ['#000000', '#FF8C00'],
+  ['#FFFFFF', '#107C10'],
+  ['#FFFFFF', '#B4009E'],
+  ['#FFFFFF', '#E3008C'],
+  ['#FFFFFF', '#5C2D91'],
+];
 
 // get color by clientID
-export const getColorByClientID = (id: number): string => color[id % color.length];
+export const getColorByClientID = (id: number): [string, string] => defaultColors[id % defaultColors.length];
