74b1abb4cbdfee67157dc2eed932bb286c945ea7
yantze <yutian.yz@alibaba-inc.com>
2023-03-24 10:14:19 +0800

    chore: delete files to the trash by default

--- packages/file-service/src/browser/file-service-client.ts
+++ packages/file-service/src/browser/file-service-client.ts
@@ -396,7 +396,7 @@ export class FileServiceClient implements IFileServiceClient {
   }
 
   async delete(uriString: string, options?: FileDeleteOptions) {
-    if (this.appConfig.isElectronRenderer) {
+    if (this.appConfig.isElectronRenderer && options?.moveToTrash !== false) {
       const uri = new URI(uriString);
       if (uri.scheme === Schemes.file) {
         return (this.injector.get(IElectronMainUIService) as IElectronMainUIService).moveToTrash(uri.codeUri.fsPath);
