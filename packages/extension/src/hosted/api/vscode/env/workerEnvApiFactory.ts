843532345dda8d67ccf9a620f0ca6a6717db6bdf
winjo <winjo.gwj@antgroup.com>
2023-03-30 20:49:02 +0800

    feat: work host env 增加 language

--- packages/extension/src/hosted/api/vscode/env/workerEnvApiFactory.ts
+++ packages/extension/src/hosted/api/vscode/env/workerEnvApiFactory.ts
@@ -2,13 +2,18 @@ import type vscode from 'vscode';
 
 import { IRPCProtocol } from '@opensumi/ide-connection/lib/common/rpcProtocol';
 
-import { MainThreadAPIIdentifier, IMainThreadEnv } from '../../../../common/vscode';
+import { MainThreadAPIIdentifier, IMainThreadEnv, IExtHostEnv } from '../../../../common/vscode';
 
 export function createWorkerHostEnvAPIFactory(
   rpcProtocol: IRPCProtocol,
-): Pick<typeof vscode.env, 'clipboard' | 'openExternal'> {
+  extHostEnv: IExtHostEnv,
+): Pick<typeof vscode.env, 'clipboard' | 'openExternal' | 'language'> {
   const mainThreadEnvProxy: IMainThreadEnv = rpcProtocol.getProxy(MainThreadAPIIdentifier.MainThreadEnv);
   return {
+    // TODO: initWorkerThreadAPIProxy 有时序问题，没有等待 worker host api 准备好就发送 port 了，因此不能先取值
+    get language() {
+      return extHostEnv.getEnvValues().language;
+    },
     clipboard: {
       readText: () => mainThreadEnvProxy.$clipboardReadText(),
       writeText: (text: string) => mainThreadEnvProxy.$clipboardWriteText(text),
