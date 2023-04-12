9dedaa7b971225fd89bcfd5a5f211e2d9550d729
qingyi.xjh <qingyi.xjh@antgroup.com>
2023-04-12 11:42:16 +0800

    chore: improve code

--- packages/debug/src/browser/preferences/launch.view.tsx
+++ packages/debug/src/browser/preferences/launch.view.tsx
@@ -21,27 +21,9 @@ import { launchExtensionSchemaUri } from '../../common/debug-schema';
 
 import styles from './launch.module.less';
 
-export const LaunchViewContainer = () => (
-  <ComponentContextProvider value={{ getIcon, localize }}>
-    <div className={styles.launch_container}>
-      <SplitPanel
-        id='launch-container'
-        resizeHandleClassName={styles.devider}
-        className={styles.launch_panel}
-        direction={EDirection.LeftToRight}
-      >
-        <LaunchIndexs data-sp-defaultSize={240} data-sp-minSize={150} />
-        <LaunchBody data-sp-flex={1} />
-      </SplitPanel>
-    </div>
-  </ComponentContextProvider>
-);
-
-const LaunchIndexs = () => {
+export const LaunchViewContainer = () => {
   const schemaRegistry = useInjectable<IJSONSchemaRegistry>(IJSONSchemaRegistry);
-  const [menuOpen, setMenuOpen] = React.useState<boolean>(false);
-  const [snippetItems, setSnippetItems] = useState<string[]>([]);
-  const [configurationItems, setConfigurationItems] = useState<string[]>([]);
+  const [snippetItems, setSnippetItems] = useState<IJSONSchemaSnippet[]>([]);
 
   useEffect(() => {
     const disposed = schemaRegistry.onDidChangeSchema((uri: string) => {
@@ -68,11 +50,52 @@ const LaunchIndexs = () => {
       'items',
       'defaultSnippets',
     ] as (keyof IJSONSchema)[]);
-    setSnippetItems(snippets.filter((s) => s.label).map((s) => s.label!));
+    setSnippetItems(snippets.filter((s) => s.label));
   };
 
+  const onSelectedConfiguration = (current: string) => {};
+
+  return (
+    <ComponentContextProvider value={{ getIcon, localize }}>
+      <div className={styles.launch_container}>
+        <SplitPanel
+          id='launch-container'
+          resizeHandleClassName={styles.devider}
+          className={styles.launch_panel}
+          direction={EDirection.LeftToRight}
+        >
+          <LaunchIndexs
+            data-sp-defaultSize={240}
+            data-sp-minSize={150}
+            snippetItems={snippetItems}
+            onSelectedConfiguration={onSelectedConfiguration}
+          />
+          <LaunchBody data-sp-flex={1} />
+        </SplitPanel>
+      </div>
+    </ComponentContextProvider>
+  );
+};
+
+const LaunchIndexs = ({
+  snippetItems,
+  onSelectedConfiguration,
+}: {
+  snippetItems: IJSONSchemaSnippet[];
+  onSelectedConfiguration: (data: string) => void;
+}) => {
+  const [menuOpen, setMenuOpen] = React.useState<boolean>(false);
+  const [configurationItems, setConfigurationItems] = useState<string[]>([]);
+  const [currentCheckItem, setCurrentCheckItem] = useState<string>();
+
+  useEffect(() => {
+    if (currentCheckItem) {
+      onSelectedConfiguration(currentCheckItem);
+    }
+  }, [currentCheckItem]);
+
   const template = ({ data }) => (
-    <div className={cls(styles.configuration_item)}>
+    <div className={cls(styles.configuration_item)} onClick={() => setCurrentCheckItem(data)}>
       <div className={styles.configuration_wrapper}>
         <span className={styles.configuration_description}>{data}</span>
       </div>
@@ -91,23 +114,28 @@ const LaunchIndexs = () => {
     setMenuOpen(visible);
   };
 
-  const snippetMenu = useMemo(() => (
+  const snippetMenu = useMemo(
+    () => (
       <MenuActionList
-        data={snippetItems.map((label) => new LabelMenuItemNode(label))}
+        data={snippetItems.map((item) => new LabelMenuItemNode(item.label!))}
         afterClick={(item: LabelMenuItemNode) => handleMenuItemClick(item)}
       />
-    ), [snippetItems, configurationItems]);
+    ),
+    [snippetItems, configurationItems],
+  );
 
   return (
     <div className={styles.launch_indexes_container}>
       <AutoSizer className={styles.configuration_items_box}>
-        {({ width, height }) => configurationItems.length === 0 ? (
+        {({ width, height }) =>
+          configurationItems.length === 0 ? (
             <div style={{ width }} className={styles.not_configuration_content}>
               {localize('debug.action.no.configuration')}
             </div>
           ) : (
             <RecycleList width={width} height={height} data={configurationItems} template={template} />
-          )}
+          )
+        }
       </AutoSizer>
       <div className={styles.foot_box}>
         <Button
