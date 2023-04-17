db74351080d47e018998e1baa8aaf3d24b6e2f23
Yiming Pan <perfectpan325@gmail.com>
2023-04-17 10:20:13 +0800

    fix: remove the marked.js warning about sanitize and add sanitizer (#2591)

--- packages/components/src/markdown/index.tsx
+++ packages/components/src/markdown/index.tsx
@@ -1,3 +1,4 @@
+import { sanitize } from 'dompurify';
 import { marked, Renderer } from 'marked';
 import React from 'react';
 
@@ -19,7 +20,7 @@ export class DefaultMarkedRenderer extends Renderer {
 }
 
 export function Markdown(props: IMarkdownProps) {
-  const parseMarkdown = (text: string, renderer: any) => marked.parse(text, { renderer });
+  const parseMarkdown = (text: string, renderer: any) => sanitize(marked.parse(text, { renderer }));
 
   const [htmlContent, setHtmlContent] = React.useState(parseMarkdown(props.value, props.renderer));
 
