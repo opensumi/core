a1ca8e1eca6ac6cc77c0564d81b6ae0f257dcb4a
shilin8805 <shilin@vip.qq.com>
2023-05-08 13:52:46 +0800

    fix(debug): fix debug breakpoint in empty line display repeat

--- packages/debug/src/browser/editor/debug-model.ts
+++ packages/debug/src/browser/editor/debug-model.ts
@@ -354,7 +354,7 @@ export class DebugModel implements IDebugModel {
    */
   protected createBreakpointDecoration(breakpoint: IDebugBreakpoint): monaco.editor.IModelDeltaDecoration {
     const lineNumber = breakpoint.raw.line;
-    const column = breakpoint.raw.column || 1;
+    const column = breakpoint.raw.column || 0;
     const range = new monaco.Range(lineNumber, column, lineNumber, column + 1);
     return {
       range,
