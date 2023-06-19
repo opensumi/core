6741e5dbbf377ea477485a1a9b79a72ec0a4f01f
qingyi.xjh <qingyi.xjh@antgroup.com>
2023-06-19 20:39:37 +0800

    feat: support goto button

--- packages/editor/src/browser/merge-editor/MergeEditorFloatComponents.tsx
+++ packages/editor/src/browser/merge-editor/MergeEditorFloatComponents.tsx
@@ -1,15 +1,22 @@
 import React, { useCallback } from 'react';
 
 import { Button } from '@opensumi/ide-components';
-import { localize } from '@opensumi/ide-core-browser';
+import { CommandService, SCM_COMMANDS, URI, localize } from '@opensumi/ide-core-browser';
 import { useInjectable } from '@opensumi/ide-core-browser';
 
 import styles from '../editor.module.less';
+import { ReactEditorComponent } from '../types';
 
-export const MergeEditorFloatComponents = (_: React.HtmlHTMLAttributes<HTMLDivElement>) => {
-  const handleOpenMergeEditor = useCallback(() => {
-    // not implemented
-  }, []);
+export const MergeEditorFloatComponents: ReactEditorComponent<{ uri: URI }> = ({ resource }) => {
+  const commandService = useInjectable<CommandService>(CommandService);
+
+  const handleOpenMergeEditor = useCallback(async () => {
+    const { uri } = resource;
+
+    [SCM_COMMANDS.GIT_OPEN_MERGE_EDITOR, SCM_COMMANDS._GIT_OPEN_MERGE_EDITOR].forEach(({ id: command }) => {
+      commandService.tryExecuteCommand(command, uri);
+    });
+  }, [resource]);
 
   return (
     <div className={styles.merge_editor_float_container}>
