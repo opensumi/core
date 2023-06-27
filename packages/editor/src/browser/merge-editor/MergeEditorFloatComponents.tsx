a3e89bdc6de6856fd99ecddd94f92ce4afc9d261
qingyi.xjh <qingyi.xjh@antgroup.com>
2023-06-27 16:12:48 +0800

    fix: improve code

--- packages/editor/src/browser/merge-editor/MergeEditorFloatComponents.tsx
+++ packages/editor/src/browser/merge-editor/MergeEditorFloatComponents.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 
 import { Button } from '@opensumi/ide-components';
-import { CommandService, SCM_COMMANDS, URI, localize } from '@opensumi/ide-core-browser';
+import { CommandRegistry, CommandService, SCM_COMMANDS, URI, localize } from '@opensumi/ide-core-browser';
 import { useInjectable } from '@opensumi/ide-core-browser';
 
 import styles from '../editor.module.less';
@@ -9,12 +9,15 @@ import { ReactEditorComponent } from '../types';
 
 export const MergeEditorFloatComponents: ReactEditorComponent<{ uri: URI }> = ({ resource }) => {
   const commandService = useInjectable<CommandService>(CommandService);
+  const commandRegistry = useInjectable<CommandRegistry>(CommandRegistry);
 
   const handleOpenMergeEditor = useCallback(async () => {
     const { uri } = resource;
 
     [SCM_COMMANDS.GIT_OPEN_MERGE_EDITOR, SCM_COMMANDS._GIT_OPEN_MERGE_EDITOR].forEach(({ id: command }) => {
-      commandService.tryExecuteCommand(command, uri);
+      if (commandRegistry.getCommand(command) && commandRegistry.isEnabled(command)) {
+        commandService.executeCommand(command, uri);
+      }
     });
   }, [resource]);
 
