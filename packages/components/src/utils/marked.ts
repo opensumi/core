efa0149d9d018e30c7760c7c87eaa445543717d9
pipiiiiii <yanxinyuu@icloud.com>
2023-08-02 16:05:05 +0800

    fix: not resolve eacape in message

--- packages/components/src/utils/marked.ts
+++ packages/components/src/utils/marked.ts
@@ -20,3 +20,12 @@ export const parseMarkdown = (
 };
 
 export const toMarkdownHtml = (value: string, options?: IMarkedOptions) => marked(value, options);
+
+export const parseWithoutEscape = (token: marked.Token) => {
+  // 这里兼容下 vscode 的写法，vscode 这里没有处理 markdown 语法
+  if (token.type === 'escape') {
+    token.text = token.raw;
+  }
+
+  return token;
+};
