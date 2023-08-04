261f271f9b98d7852be10809b8231cb2ae1d4b2b
pipiiiiii <yanxinyuu@icloud.com>
2023-08-04 10:27:38 +0800

    feat: 支持 ai 修改配置

--- packages/startup/entry/sample-modules/ai-chat/ai-chat.service.ts
+++ packages/startup/entry/sample-modules/ai-chat/ai-chat.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@opensumi/di';
-import { Emitter, Event } from '@opensumi/ide-core-common';
-import { AISerivceType } from '@opensumi/ide-startup/lib/common/index';
+import { Injectable, Autowired } from '@opensumi/di';
+import { PreferenceService } from '@opensumi/ide-core-browser';
+import { Emitter, Event, CommandService } from '@opensumi/ide-core-common';
+import { AISerivceType, AiGPTBackSerivcePath } from '@opensumi/ide-startup/lib/common/index';
 
 const aiSearchKey = '/search ';
 const aiSearchCodeKey = '/searchcode ';
@@ -8,6 +9,16 @@ const aiSumiKey = '/sumi';
 
 @Injectable()
 export class AiChatService {
+
+  @Autowired(AiGPTBackSerivcePath)
+  aiBackService: any;
+
+  @Autowired(CommandService)
+  protected readonly commandService: CommandService;
+
+  @Autowired(PreferenceService)
+  protected preferenceService: PreferenceService;
+
   private readonly _onChatMessageLaunch = new Emitter<string | React.ReactNode>();
   public readonly onChatMessageLaunch: Event<string | React.ReactNode> = this._onChatMessageLaunch.event;
 
@@ -31,5 +42,45 @@ export class AiChatService {
     }
 
     return { type, message };
+  }
+
+  public async messageWithSumi(input: string) {
+    const messageWithPrompt = `You are a developer proficient in vscode extension development. I will ask you some questions about extension development. If a certain problem can be solved using a Command, please provide the command. If it's related to modifying configurations, please specify the category and identifier of the configuration item, along with an example code.
+    An example question is as follow: “修改字体大小为 20 像素”.
+    And then, give me an answer such as: “
+    ConfigCategory: editor
+    ConfigKey: fontSize
+    ConfigParams: 16
+    Example:
+    \`\`\`
+    const config = vscode.workspace.getConfiguration('editor');
+    config.update('fontSize', 16, vscode.ConfigurationTarget.Global);
+    \`\`\`
+    ”
+    Another example is : “唤起弹窗”
+    And answer such as :”
+    Command: workbench.action.openGlobalKeybindings
+    Example:
+    \`\`\`
+    vscode.workspace.executeCommand('workbench.action.openGlobalKeybindings')
+    \`\`\`
+    ”
+    (You need to distinguish between whether it's a Command or a Config in your answers and provide the corresponding format. Simply provide content similar to the examples given without the need for explanations.)
+    My question is: ${input}`;
+
+    const res = await this.aiBackService.aiCommonRequest(messageWithPrompt);
+
+    const commandReg = /Command:\s*(?<command>\S+)\s*\n*Example:\s*```(?<example>[\s\S]+)```/i;
+    const command = commandReg.exec(res);
+    if (command) {
+      this.commandService.tryExecuteCommand(command.groups?.command!);
+    }
+
+    const configReg = /ConfigCategory:\s*(?<category>\S+)\s*\n*ConfigKey:\s*(?<key>\S+)\s*\n*ConfigParams:\s*"?(?<params>[^"\n]+)"?\s*\n*Example:\s*\n*```(?<example>[\s\S]+)```/i;
+    const config = configReg.exec(res);
+    if (config) {
+      const { category, key, params } = config.groups || {};
+      this.preferenceService.set(`${category}.${key}`, params);
+    }
   }
 }
