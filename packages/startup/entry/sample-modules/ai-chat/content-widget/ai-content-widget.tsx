69dd579335e887d563e92d7f4d9e3dca8ff69b7c
qingyi.xjh <qingyi.xjh@antgroup.com>
2023-08-10 10:23:14 +0800

    chore: 实现 ai content widget

---
+++ packages/startup/entry/sample-modules/ai-chat/content-widget/ai-content-widget.tsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Injectable, Autowired } from '@opensumi/di';
+import { Disposable, Emitter, Event } from '@opensumi/ide-core-common';
+import type { ICodeEditor as IMonacoCodeEditor } from '@opensumi/ide-monaco/lib/browser/monaco-api/types';
+import * as monaco from '@opensumi/monaco-editor-core/esm/vs/editor/editor.api';
+import { AppConfig, ConfigProvider, getExternalIcon } from '@opensumi/ide-core-browser';
+import { AiInput } from '../components/AIInput';
+import { AIImprove } from '../components/AIImprove';
+
+export interface IAiContentWidget extends monaco.editor.IContentWidget {
+  show: (options?: ShowAiContentOptions | undefined) => void;
+  hide: (options?: ShowAiContentOptions | undefined) => void;
+}
+
+export interface ShowAiContentOptions {
+  /**
+   * 选中区域
+   */
+  selection: monaco.Range;
+}
+
+@Injectable({ multiple: true })
+export class AiContentWidget extends Disposable implements IAiContentWidget {
+
+  @Autowired(AppConfig)
+  private configContext: AppConfig;
+
+  allowEditorOverflow?: boolean | undefined = true;
+  suppressMouseDown?: boolean | undefined = false;
+
+  private domNode: HTMLElement;
+  protected options: ShowAiContentOptions | undefined;
+
+  private readonly _onSelectChange = new Emitter<string>();
+  public readonly onSelectChange: Event<string> = this._onSelectChange.event;
+
+  constructor(private readonly editor: IMonacoCodeEditor) {
+    super();
+
+    this.hide();
+    this.renderView();
+  }
+
+  override dispose(): void {
+    this.hide()
+    super.dispose();
+  }
+
+  private renderView(): void {
+    ReactDOM.render(
+      <ConfigProvider value={this.configContext}>
+        <div>
+          <div className={'ai-shortcuts'}>
+            <AIImprove onClick={(title) => {
+              console.log('title:>>>>', title)
+              this._onSelectChange.fire(title);
+            }} lists={[
+              {
+                title: '给出测试用例',
+                iconClass: getExternalIcon('git-pull-request')
+              },
+              {
+                title: '｜',
+                iconClass: ''
+              },
+              {
+                title: '优化一下代码',
+                iconClass: getExternalIcon('git-pull-request')
+              },
+              {
+                title: '｜',
+                iconClass: ''
+              },
+              {
+                title: '添加注释',
+                iconClass: getExternalIcon('git-pull-request')
+              },
+              {
+                title: '｜',
+                iconClass: ''
+              },
+              {
+                title: '检查代码是否有问题',
+                iconClass: getExternalIcon('git-pull-request')
+              }
+            ]}/>
+          </div>
+          <div className={'ai-input'}>
+            <AiInput onValueChange={(value) => {
+              this._onSelectChange.fire(value);
+            }}/>
+          </div>
+        </div>
+      </ConfigProvider>,
+      this.getDomNode(),
+    );
+    this.layoutContentWidget();
+  }
+
+  async show(options?: ShowAiContentOptions | undefined): Promise<void> {
+    if (!options) {
+      return;
+    }
+
+    if (this.options && this.options.selection.equalsRange(options.selection)) {
+      return;
+    }
+
+    this.options = options;
+
+    this.editor.addContentWidget(this);
+  }
+
+  hide: (options?: ShowAiContentOptions | undefined) => void = () => {
+    this.options = undefined;
+    this.editor.removeContentWidget(this);
+  };
+
+  getId(): string {
+    return 'AI-Content-Widget';
+  }
+
+  protected layoutContentWidget(): void {
+    this.editor.layoutContentWidget(this);
+  }
+
+  getDomNode(): HTMLElement {
+    if (!this.domNode) {
+      this.domNode = document.createElement('div');
+      this.domNode.classList.add(this.getId());
+    }
+    return this.domNode;
+  }
+
+  getPosition(): monaco.editor.IContentWidgetPosition | null {
+    const endPosition = this.options && this.options.selection.getEndPosition();
+    return endPosition
+      ? {
+          // @ts-ignore
+          position: new monaco.Position(endPosition.lineNumber, this.options?.selection.startColumn),
+          preference: [
+            monaco.editor.ContentWidgetPositionPreference.BELOW,
+          ],
+        }
+      : null;
+  }
+}
