666747f2324ebc7df47a56acb7d1118840f3d821
pipiiiiii <yanxinyuu@icloud.com>
2023-08-15 15:53:06 +0800

    fix: fix

--- packages/startup/entry/sample-modules/ai-chat/ai-chat.service.ts
+++ packages/startup/entry/sample-modules/ai-chat/ai-chat.service.ts
@@ -83,54 +83,81 @@ export class AiChatService {
   }
 
   public async messageWithSumi(input: string) {
-    const messageWithPrompt = `You are a developer proficient in vscode extension development.I will ask you some questions about extension development.
-    If a certain problem can be solved using a Command, please provide the command.
-    If it's related to modifying configurations, please specify the category and identifier of the configuration item, along with an example code.
-    An example question is as follow: “修改字体大小为 20 像素”.
-    And then, give me an answer such as: “
-    ConfigCategory: editor
-    ConfigKey: fontSize
-    ConfigParams: 16
-    Example:
-    \`\`\`
-    const config = vscode.workspace.getConfiguration('editor');
-    config.update('fontSize', 16, vscode.ConfigurationTarget.Global);
-    \`\`\`
-    ”
-    Another example is : “唤起弹窗”
-    And answer such as :”
-    Command: workbench.action.openGlobalKeybindings
-    Example:
-    \`\`\`
-    vscode.workspace.executeCommand('workbench.action.openGlobalKeybindings')
-    \`\`\`
-    ”
-    (You need to distinguish between whether it's a Command or a Config in your answers and provide the corresponding format. Simply provide content similar to the examples given without the need for explanations.)
+    // const messageWithPrompt = `You are a developer proficient in vscode extension development.I will ask you some questions about extension development.
+    // If a certain problem can be solved using a Command, please provide the command.
+    // If it's related to modifying configurations, please specify the category and identifier of the configuration item, along with an example code.
+    // An example question is as follow: “修改字体大小为 20 像素”.
+    // And then, give me an answer such as: “
+    // ConfigCategory: editor
+    // ConfigKey: fontSize
+    // ConfigParams: 16
+    // Example:
+    // \`\`\`
+    // const config = vscode.workspace.getConfiguration('editor');
+    // config.update('fontSize', 16, vscode.ConfigurationTarget.Global);
+    // \`\`\`
+    // ”
+    // Another example is : “唤起弹窗”
+    // And answer such as :”
+    // Command: workbench.action.openGlobalKeybindings
+    // Example:
+    // \`\`\`
+    // vscode.workspace.executeCommand('workbench.action.openGlobalKeybindings')
+    // \`\`\`
+    // ”
+    // (You need to distinguish between whether it's a Command or a Config in your answers and provide the corresponding format. Simply provide content similar to the examples given without the need for explanations.)
+    // My question is: ${input}`;
+    const messageWithPrompt = `
+    You are a professional vscode plugin developer, and I have some questions about plugin development to ask you. Please provide API and give example codes with javascript.
+    An example question is as follow: "修改字体大小为 20 像素"
+    And then, give me an answer such as: "
+      API: vscode.workspace.getConfiguration
+      Example:
+      \`\`\`
+          const config = vscode.workspace.getConfiguration('editor');
+          config.update('fontSize', 20, vscode.ConfigurationTarget.Global);
+      \`\`\`
+    "
+    (Please just provide example code and API, do not give other words)
     My question is: ${input}`;
 
     const res = await this.aiBackService.aiGPTcompletionRequest(messageWithPrompt);
 
     console.log('aiCodeGPTcompletionRequest: >>>> ', res);
 
+    const exampleReg = /(Example:)?\n*```(javascript)?\n?(?<example>[\s\S]+)```/i;
+    const example = exampleReg.exec(res.data);
+    if (example) {
+      try {
+        await this.aiBackService.writeFile(example.groups?.example);
+        await this.extensionManagementService.postChangedExtension(false, await this.aiBackService.getExtensionPath());
+      } catch {
+        console.log('error');
+      }
+    }
+
     // const commandReg = /Command:\s*(?<command>\S+)\s*\n*Example:\s*```\n?(?<example>[\s\S]+)\n```/i;
-    // const command = commandReg.exec(res);
+    // const command = commandReg.exec(res.data);
     // if (command) {
-    //   await this.removeOldExtension();
     //   try {
     //     await this.commandService.executeCommand(command.groups?.command!);
     //   } catch {
-
+    //     await this.aiBackService.writeFile(command.groups?.example);
+    //     await this.extensionManagementService.postChangedExtension(false, await this.aiBackService.getExtensionPath());
     //   }
     // }
 
-    const configReg = /ConfigCategory:\s*(?<category>\S+)\s*\n*ConfigKey:\s*(?<key>\S+)\s*\n*ConfigParams:\s*"?(?<params>[^"\n]+)"?\s*\n*Example:\s*\n*```(?<example>[^`]+)```/i;
-    const config = configReg.exec(res.data);
-    if (config) {
-      const { category, key, params, example } = config.groups || {};
-      this.preferenceService.set(`${category}.${key}`, params);
-      await this.aiBackService.writeFile(example);
-      await this.extensionManagementService.postChangedExtension(false, await this.aiBackService.getExtensionPath());
-    }
+    // const configReg = /ConfigCategory:\s*(?<category>\S+)\s*\n*ConfigKey:\s*(?<key>\S+)\s*\n*ConfigParams:\s*"?(?<params>[^"\n]+)"?\s*\n*Example:\s*\n*```(?<example>[^`]+)```/i;
+    // const config = configReg.exec(res.data);
+    // if (config) {
+    //   const { category, key, params, example } = config.groups || {};
+    //   this.preferenceService.set(`${category}.${key}`, params);
+    //   await this.aiBackService.writeFile(example);
+    //   await this.extensionManagementService.postChangedExtension(false, await this.aiBackService.getExtensionPath());
+    // }
+    setTimeout(() => {
+      this.removeOldExtension();
+    }, 10000);
   }
 
   public async removeOldExtension() {
