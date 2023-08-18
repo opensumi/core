10218539a5d565d1f8d1d8513f7211773a01a049
kuiwu <danwu.wdw@alibaba-inc.com>
2023-08-18 10:42:31 +0800

    chore: add vscode.l10n api interface

--- packages/types/vscode/typings/vscode.proposed.inlineCompletionsAdditions.d.ts
+++ packages/types/vscode/typings/vscode.proposed.inlineCompletionsAdditions.d.ts
@@ -1,12 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 declare module 'vscode' {
-
 	// https://github.com/microsoft/vscode/issues/124024 @hediet @alexdima
-
 	export interface InlineCompletionItem {
 		/**
 		 * If set to `true`, unopened closing brackets are removed and unclosed opening brackets are closed.
--- packages/types/vscode/typings/vscode.proposed.scm.d.ts
+++ packages/types/vscode/typings/vscode.proposed.scm.d.ts
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 /**
  * This is the place for API experiments and proposals.
  * These API are NOT stable and subject to change. They are only available in the Insiders
@@ -15,7 +10,6 @@
  */
 
 declare module 'vscode' {
-
   export interface SourceControl {
 
     /**
