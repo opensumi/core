3eb1e744397c4fc392e594d48b5fc2f2c59c74bb
kanno <812137533@qq.com>
2023-10-13 18:21:27 +0800

    fix: error color

--- packages/core-common/src/log.ts
+++ packages/core-common/src/log.ts
@@ -231,7 +231,7 @@ export class DebugLog implements IDebugLog {
       black: '\x1b[30m',
       red: '\x1b[31m',
       green: '\x1b[32m',
-      yellow: '\x1b3[33m',
+      yellow: '\x1b[33m',
       blue: '\x1b[34m',
       magenta: '\x1b[35m',
       cyan: '\x1b[36m',
