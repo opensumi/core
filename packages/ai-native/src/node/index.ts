6e743c94be83f26b310d7742f39524b92ba61395
qingyi.xjh <qingyi.xjh@antgroup.com>
2023-11-10 13:45:12 +0800

    fix: ci

--- packages/ai-native/src/node/index.ts
+++ packages/ai-native/src/node/index.ts
@@ -3,15 +3,15 @@ import { NodeModule } from '@opensumi/ide-core-node';
 
 import { AiGPTBackSerivcePath, AiGPTBackSerivceToken, IAiChatService } from '../common';
 
-import { AiNativeBackService } from './ai-gpt.back.service';
+// import { AiNativeBackService } from './ai-gpt.back.service';
 
 @Injectable()
 export class AiNativeModule extends NodeModule {
   providers: Provider[] = [
     {
       token: AiGPTBackSerivceToken,
-      useClass: AiNativeBackService,
-      // useClass: class {},
+      // useClass: AiNativeBackService,
+      useClass: class {},
     },
   ];
 
