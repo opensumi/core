e208dea2d1fa3e13f762c0831776fc277e708a56
Bryce <31676999+AhkunTa@users.noreply.github.com>
2023-11-17 12:59:07 +0800

    feat: add menu bar slot (#3177)

--- packages/ai-native/src/browser/override/layout/layout-config.ts
+++ packages/ai-native/src/browser/override/layout/layout-config.ts
@@ -15,3 +15,6 @@ export const AiChatLayoutConfig = {
     modules: [Ai_CHAT_CONTAINER_VIEW_ID],
   },
 };
+
+export const AI_MENU_BAR_RIGHT = 'ai_menu_bar_right';
+export const AI_MENU_BAR_LEFT = 'ai_menu_bar_left';
