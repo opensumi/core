a7a1543a1542a69e2c83c4d038635c14751cb793
kuiwu <danwu.wdw@alibaba-inc.com>
2023-12-01 17:21:19 +0800

    chore: add monaco test

--- jest.config.js
+++ jest.config.js
@@ -116,9 +116,9 @@ module.exports = {
         '**/packages/output/__test?(s)__/browser/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/packages/terminal-next/__test?(s)__/browser/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/packages/extension-storage/__test?(s)__/browser/**/?(*.)+(spec|test).[jt]s?(x)',
-
-        // '**/packages/debug/__test?(s)__/browser/**/?(*.)+(spec|test).[jt]s?(x)',
-        // '**/packages/*/__test?(s)__/common/**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/packages/monaco/__tests__/browser/textmate.test.ts',
+        // '**/packages/*/__test?(s)__/browser/**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/packages/*/__test?(s)__/common/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/tools/*/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/packages/extension/__tests__/hosted/**/?(*.)+(spec|test).[jt]s?(x)',
         '**/packages/{components,core-browser,core-common}/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
