345816ee7f194128b370ba73ce7654ec956a0392
John <qingyi.xjh@antgroup.com>
2024-01-23 20:20:55 +0800

    feat: support 3-way stop widget (#3297)
    * feat: support 3-way stop widget
    
    * feat: support 3-way stop widget

---
+++ packages/monaco/src/browser/contrib/merge-editor/widget/stop-widget.tsx
@@ -0,0 +1,25 @@
+import { Injectable } from '@opensumi/di';
+import { IAiInlineResultIconItemsProps } from '@opensumi/ide-core-browser/lib/components/ai-native/inline-chat/result';
+
+import { ResolveResultWidget } from './resolve-result-widget';
+
+@Injectable({ multiple: true })
+export class StopWidget extends ResolveResultWidget {
+  override isRenderThumbs(): boolean {
+    return false;
+  }
+
+  override iconItems(): IAiInlineResultIconItemsProps[] {
+    return [
+      {
+        icon: 'circle-pause',
+        text: '停止',
+        onClick: () => {},
+      },
+    ];
+  }
+
+  override id(): string {
+    return `${super.id()}_stop_${this.uid}`;
+  }
+}
