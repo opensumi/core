77fec8292b859b07d1177709c831a291694353f2
lijiacheng.ljc <lijiacheng.ljc@antgroup.com>
2024-02-23 17:07:12 +0800

    chore: remove unused code

--- packages/core-common/src/di-helper/domain-helper.ts
+++ packages/core-common/src/di-helper/domain-helper.ts
@@ -13,36 +13,3 @@ export function Domain(...domains: Domain[]) {
     markInjectable(target, opts);
   };
 }
-
-const domainMap = new Map<Domain, ConstructorOf<any>>();
-
-/**
- * 带全局记录的 Domain 装饰器
- * @param domain
- */
-export function EffectDomain(domain: Domain) {
-  return (target: ConstructorOf<any>) => {
-    const opts = getInjectableOpts(target) || {};
-    opts.domain = domain;
-    markInjectable(target, opts);
-
-    const tmp = domainMap.get(domain);
-    if (!tmp) {
-      domainMap.set(domain, target);
-    }
-  };
-}
-
-export function getDomainConstructors(...domains: Domain[]) {
-  const constructorSet = new Set<ConstructorOf<any>>();
-  for (const domain of domains) {
-    const constructor = domainMap.get(domain);
-    if (constructor) {
-      constructorSet.add(constructor);
-    } else {
-      // eslint-disable-next-line no-console
-      console.error(`Unable to retrieve the Constructor for ${String(domain)}`);
-    }
-  }
-  return Array.from(constructorSet);
-}
