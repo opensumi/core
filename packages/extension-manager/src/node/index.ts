43a7d2b7f412fc921b703ffa8eda77bc59265db1
boke <sijie.zsj@antgroup.com>
2024-03-05 19:15:04 +0800

    feat: replace marketplace to alipay cloudide marketplace

--- packages/extension-manager/src/node/index.ts
+++ packages/extension-manager/src/node/index.ts
@@ -2,13 +2,13 @@ import { Injectable, Provider } from '@opensumi/di';
 import { NodeModule } from '@opensumi/ide-core-node';
 
 import {
-  IOpentrsMarketplaceService,
+  IAlipayCloudMarketplaceService,
   IOpenvsxMarketplaceService,
   VSXExtensionBackSerivceToken,
   VSXExtensionServicePath,
 } from '../common';
 
-import { OpentrsMarketplaceService, OpenvsxMarketplaceService } from './marketplace';
+import { AlipayCloudMarketplaceService, OpenvsxMarketplaceService } from './marketplace';
 import { VSXExtensionService } from './vsx-extension.service';
 
 @Injectable()
@@ -19,8 +19,8 @@ export class OpenVsxExtensionManagerModule extends NodeModule {
       useClass: VSXExtensionService,
     },
     {
-      token: IOpentrsMarketplaceService,
-      useClass: OpentrsMarketplaceService,
+      token: IAlipayCloudMarketplaceService,
+      useClass: AlipayCloudMarketplaceService,
     },
     {
       token: IOpenvsxMarketplaceService,
