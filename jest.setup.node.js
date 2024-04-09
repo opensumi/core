cf87ba07493147b982b9542ffdf9e2594f3a99a6
野声 <lijiacheng.ljc@antgroup.com>
2024-04-09 22:46:22 +0800

    fix: set max listener to 100

--- jest.setup.node.js
+++ jest.setup.node.js
@@ -101,5 +101,4 @@ Object.defineProperty(window, 'matchMedia', {
   })),
 });
 
-// https://stackoverflow.com/a/44143119/9443819
-process.setMaxListeners(0);
+require('events').EventEmitter.defaultMaxListeners = 100;
