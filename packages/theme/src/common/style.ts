ca5988c9bfdfdfe209014a791b1e8dc11b00eea1
野声 <lijiacheng.ljc@antgroup.com>
2024-04-10 14:41:17 +0800

    fix: dispose css rule

--- packages/theme/src/common/style.ts
+++ packages/theme/src/common/style.ts
@@ -1,9 +1,25 @@
 import { IDisposable } from '@opensumi/ide-core-common';
 
-export interface ICSSStyleService {
-  addClass(classname: string, style: Partial<CSSStyleDeclaration>): IDisposable;
+export interface IStyleInsertResult extends IDisposable {
+  index: number;
+}
 
-  removeClass(classname: string);
+export interface IStyleSheet {
+  insertSelector(selector: string, rule: string): IStyleInsertResult;
+  deleteRule(index: number): void;
+}
+
+export interface ICSSStyleService {
+  /**
+   * @deprecated pls use
+   */
+  addClass(classname: string, style: Partial<CSSStyleDeclaration>): IDisposable;
+  /**
+   * @deprecated
+   */
+  removeClass(classname: string): void;
+
+  acquire(key: string): IStyleSheet;
 }
 
 export const ICSSStyleService = Symbol('ICSSStyleService');
