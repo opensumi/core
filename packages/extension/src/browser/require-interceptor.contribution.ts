7e5c4c2747ba738296c0a1b97f234d41edbe5e78
野声 <lijiacheng.ljc@antgroup.com>
2024-04-23 10:18:18 +0800

    fix: require interceptor handle react error (#3570)

--- packages/extension/src/browser/require-interceptor.contribution.ts
+++ packages/extension/src/browser/require-interceptor.contribution.ts
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 
 import { Domain } from '@opensumi/ide-core-browser';
 
@@ -11,6 +12,12 @@ import {
 
 import { createBrowserApi } from './sumi-browser';
 
+// `react-dom/18.2.0/umd/react-dom.production.min.js` is do the same thing, it export both `react-dom` and `react-dom/client`.
+const umdReactDOM = {
+  ...ReactDOM,
+  ...ReactDOMClient,
+};
+
 @Domain(RequireInterceptorContribution)
 export class BrowserRequireInterceptorContribution implements RequireInterceptorContribution {
   registerRequireInterceptor(registry: IRequireInterceptorService<IBrowserRequireInterceptorArgs>): void {
@@ -21,7 +28,12 @@ export class BrowserRequireInterceptorContribution implements RequireInterceptor
 
     registry.registerRequireInterceptor({
       moduleName: 'ReactDOM',
-      load: () => ReactDOM,
+      load: () => umdReactDOM,
+    });
+
+    registry.registerRequireInterceptor({
+      moduleName: 'ReactDOMClient',
+      load: () => ReactDOMClient,
     });
 
     registry.registerRequireInterceptor({
