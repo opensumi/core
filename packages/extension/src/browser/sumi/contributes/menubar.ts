794ad232530815d737804229194d6de5ab2113a7
野声 <lijiacheng.ljc@antgroup.com>
2024-04-23 20:05:57 +0800

    feat: render ext icon in menubar

--- packages/extension/src/browser/sumi/contributes/menubar.ts
+++ packages/extension/src/browser/sumi/contributes/menubar.ts
@@ -1,9 +1,12 @@
 import { Autowired, Injectable } from '@opensumi/di';
+import { getIcon } from '@opensumi/ide-components';
 import { IMenuRegistry } from '@opensumi/ide-core-browser/lib/menu/next';
 import { IJSONSchema, LifeCyclePhase, localize } from '@opensumi/ide-core-common';
+import { IIconService, IconType } from '@opensumi/ide-theme';
 
 import { Contributes, LifeCycle, VSCodeContributePoint } from '../../../common';
 import { IContributeMenubarItem } from '../../../common/sumi/extension';
+import { AbstractExtInstanceManagementService } from '../../types';
 
 export type KtMenubarsSchema = IContributeMenubarItem[];
 
@@ -13,6 +16,12 @@ export type KtMenubarsSchema = IContributeMenubarItem[];
 export class MenubarsContributionPoint extends VSCodeContributePoint<KtMenubarsSchema> {
   @Autowired(IMenuRegistry)
   private readonly menuRegistry: IMenuRegistry;
+
+  @Autowired(AbstractExtInstanceManagementService)
+  protected readonly extensionManageService: AbstractExtInstanceManagementService;
+
+  @Autowired(IIconService)
+  protected readonly iconService: IIconService;
 
   static schema: IJSONSchema = {
     description: localize('sumiContributes.menubars'),
@@ -52,12 +61,22 @@ export class MenubarsContributionPoint extends VSCodeContributePoint<KtMenubarsS
   contribute() {
     for (const contrib of this.contributesMap) {
       const { extensionId, contributes } = contrib;
+      const extension = this.extensionManageService.getExtensionInstanceByExtId(extensionId);
+      if (!extension) {
+        continue;
+      }
+
       for (const menubarItem of contributes) {
+        const iconClass = extension.icon
+          ? this.iconService.fromIcon('', extension.icon, IconType.Background)
+          : getIcon('default-menu-icon');
+
         this.addDispose(
           this.menuRegistry.registerMenubarItem(menubarItem.id, {
             label: this.getLocalizeFromNlsJSON(menubarItem.title, extensionId),
             order: menubarItem.order,
             nativeRole: menubarItem.nativeRole,
+            iconClass,
           }),
         );
       }
