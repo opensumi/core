517c360f80086b4792a5f555394efc640aa5fa40
野声 <lijiacheng.ljc@antgroup.com>
2024-04-29 15:45:35 +0800

    chore: update code

--- packages/ai-native/src/browser/layout/layout-config.ts
+++ packages/ai-native/src/browser/layout/layout-config.ts
@@ -1,13 +1,5 @@
 import { DESIGN_MENU_BAR_LEFT, DESIGN_MENU_BAR_RIGHT } from '@opensumi/ide-design';
 
-import { AI_CHAT_CONTAINER_ID, AI_CHAT_VIEW_ID } from '../../common';
-
-export const AIChatLayoutConfig = {
-  [AI_CHAT_VIEW_ID]: {
-    modules: [AI_CHAT_CONTAINER_ID],
-  },
-};
-
 /**
  * @deprecated Use {@link DESIGN_MENU_BAR_RIGHT} instead
  */
