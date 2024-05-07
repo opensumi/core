199effa6740ad4fbf250a33401222eb13658b6a7
野声 <lijiacheng.ljc@antgroup.com>
2024-05-07 18:07:43 +0800

    feat: support stream on error event

--- packages/utils/src/stream.ts
+++ packages/utils/src/stream.ts
@@ -1,3 +1,6 @@
+import { IDisposable } from './disposable';
+import { EventQueue } from './event';
+
 import type { Readable } from 'stream';
 
 export interface IReadableStream<T> {
@@ -27,4 +30,57 @@ export function listenReadable(stream: IReadableStream<Uint8Array>, options: ILi
   stream.on('end', () => {
     options.onEnd();
   });
+}
+
+export class SumiReadableStream<T> implements IReadableStream<T> {
+  protected dataQueue = new EventQueue<T>();
+  protected endQueue = new EventQueue<void>();
+  protected errorQueue = new EventQueue<Error>();
+
+  on(event: 'error', listener: (err: Error) => void): this;
+  on(event: 'data', listener: (chunk: T) => void): this;
+  on(event: 'end', listener: () => void): this;
+  on(event: string, listener: (...args: any[]) => void): this;
+  on(event: unknown, listener: unknown): this {
+    switch (event) {
+      case 'error':
+        this.onError(listener as (err: Error) => void);
+        break;
+      case 'data':
+        this.onData(listener as (chunk: T) => void);
+        break;
+      case 'end':
+        this.onEnd(listener as () => void);
+        break;
+      default:
+        break;
+    }
+    return this;
+  }
+
+  onData(cb: (data: T) => void): IDisposable {
+    return this.dataQueue.on(cb);
+  }
+
+  onEnd(cb: () => void): IDisposable {
+    return this.endQueue.on(cb);
+  }
+
+  onError(cb: (err: Error) => void): IDisposable {
+    return this.errorQueue.on(cb);
+  }
+
+  emit(buffer: T) {
+    this.dataQueue.push(buffer);
+  }
+
+  error(err: Error) {
+    this.errorQueue.push(err);
+  }
+
+  end() {
+    this.dataQueue.dispose();
+    this.endQueue.push(undefined);
+    this.endQueue.dispose();
+  }
 }
