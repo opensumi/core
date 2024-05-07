5b5781a507791fdfdaa917faf27cb86246d6e4a5
野声 <lijiacheng.ljc@antgroup.com>
2024-05-07 17:31:30 +0800

    chore: add deserialize bench

--- packages/connection/benchmarks/gateway.bench.ts
+++ packages/connection/benchmarks/gateway.bench.ts
@@ -79,6 +79,20 @@ const obj6 = {
   binary: crypto.randomBytes(10 * 1024),
 };
 
+const serialized1 = serializer.serialize(obj);
+const serialized2 = serializer.serialize(obj2);
+const serialized3 = serializer.serialize(obj3);
+const serialized4 = serializer.serialize(obj4);
+const serialized5 = serializer.serialize(obj5);
+const serialized6 = serializer.serialize(obj6);
+
+const stringified1 = JSON.stringify(obj);
+const stringified2 = JSON.stringify(obj2);
+const stringified3 = JSON.stringify(obj3);
+const stringified4 = JSON.stringify(obj4);
+const stringified5 = JSON.stringify(obj5);
+const stringified6 = JSON.stringify(obj6);
+
 bench
   .add('[gateway] fury', () => {
     testIt(obj);
@@ -125,6 +139,22 @@ bench
     JSON.stringify(obj3);
     JSON.stringify(obj4);
     JSON.stringify(obj5);
+  })
+  .add('[gateway] fury deserialize', () => {
+    parse(serialized1);
+    parse(serialized2);
+    parse(serialized3);
+    parse(serialized4);
+    parse(serialized5);
+    parse(serialized6);
+  })
+  .add('[gateway] json parse', () => {
+    JSON.parse(stringified1);
+    JSON.parse(stringified2);
+    JSON.parse(stringified3);
+    JSON.parse(stringified4);
+    JSON.parse(stringified5);
+    JSON.parse(stringified6);
   });
 
 async function main() {
