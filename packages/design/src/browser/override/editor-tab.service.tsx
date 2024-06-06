e262aa0d8fef967ec7fc71b85094536557b7a9d7
kuiwu <danwu.wdw@alibaba-inc.com>
2024-06-06 16:46:01 +0800

    style: simplify EditorTabCloseComponent and adjust button styles

--- packages/design/src/browser/override/editor-tab.service.tsx
+++ packages/design/src/browser/override/editor-tab.service.tsx
@@ -1,28 +1,14 @@
 import cls from 'classnames';
-import React, { ReactNode, useCallback, useMemo } from 'react';
+import React, { ReactNode } from 'react';
 
 import { Injectable } from '@opensumi/di';
-import { Popover } from '@opensumi/ide-core-browser/lib/components';
-import { localize, uuid } from '@opensumi/ide-core-common';
 import { EditorTabService } from '@opensumi/ide-editor/lib/browser/editor.tab.service';
 
 import styles from '../style/design.module.less';
 
 const EditorTabCloseComponent = (props) => {
   const { children } = props;
-  const [display, setDisplay] = React.useState(false);
-
-  const uid = useMemo(() => 'editor-tab-close-' + uuid(6), []);
-
-  const handleClick = useCallback(() => {
-    setDisplay(false);
-  }, []);
-
-  return (
-    <Popover id={uid} title={localize('editor.title.context.close')} onClickAction={handleClick} display={display}>
-      {children}
-    </Popover>
-  );
+  return children;
 };
 
 @Injectable()
