8479c7adc9ff711994853be4d5a3f813d8eb3975
野声 <lijiacheng.ljc@antgroup.com>
2024-06-11 20:21:46 +0800

    chore: update sample

--- packages/startup/entry/sample-modules/ai-native/ai.back.service.ts
+++ packages/startup/entry/sample-modules/ai-native/ai.back.service.ts
@@ -1,5 +1,3 @@
-import { Readable } from 'stream';
-
 import { Autowired, Injectable } from '@opensumi/di';
 import { IAICompletionOption } from '@opensumi/ide-core-common';
 import {
@@ -34,8 +32,8 @@ const streamData = [
   '\n',
   'function greet(person: Person) {\n',
   '  console.log(`Hello, ${person.name}!`);\n',
-  ' #Command#: du -sh *\n',
-  ' #Description#: 查看当前文件夹下所有文件和子文件夹的大小\n',
+  '  // #Command#: du -sh *\n',
+  '  // #Description#: 查看当前文件夹下所有文件和子文件夹的大小\n',
   '}\n',
   '\n',
   'greet(person); // Output: "Hello, John Doe!"\n',
