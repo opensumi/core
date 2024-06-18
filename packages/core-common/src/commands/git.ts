96281e5b995dd0e720de08139b1d77024b369279
野声 <lijiacheng.ljc@antgroup.com>
2024-06-18 11:30:20 +0800

    chore: remove unused code

--- packages/core-common/src/commands/git.ts
+++ packages/core-common/src/commands/git.ts
@@ -1,8 +1,3 @@
-export const MergeConflictCommands = {
-  Previous: 'merge-conflict.previous',
-  Next: 'merge-conflict.next',
-} as const;
-
 export const GitCommands = {
   Stage: 'git.stage',
   StageAllMerge: 'git.stageAllMerge',
