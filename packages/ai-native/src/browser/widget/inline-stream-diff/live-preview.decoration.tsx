84252e2d5e872e08b00b89097eb8ea21e97ae36b
John <qingyi.xjh@antgroup.com>
2024-06-27 20:41:49 +0800

    fix: inline live diff zone range (#3810)
    * style:  improve styels
    
    * fix: live inline diff zone position

--- packages/ai-native/src/browser/widget/inline-stream-diff/live-preview.decoration.tsx
+++ packages/ai-native/src/browser/widget/inline-stream-diff/live-preview.decoration.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactDOMClient from 'react-dom/client';
 
+import { StackingLevel } from '@opensumi/ide-core-browser';
 import { Disposable } from '@opensumi/ide-core-common';
 import { ICodeEditor, IEditorDecorationsCollection, Position, Range, Selection } from '@opensumi/ide-monaco';
 import { EditorOption } from '@opensumi/monaco-editor-core/esm/vs/editor/common/config/editorOptions';
@@ -13,7 +14,12 @@ import { renderLines } from '../ghost-text-widget/index';
 
 import styles from './inline-stream-diff.module.less';
 
-const RemovedWidgetComponent = ({ dom }) => {
+const ZoneDescription = 'zone-description';
+const ActiveLineDecoration = 'activeLine-decoration';
+const AddedRangeDecoration = 'added-range-decoration';
+const PendingRangeDecoration = 'pending-range-decoration';
+
+const RemovedWidgetComponent = ({ dom, marginWidth }) => {
   const ref = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -22,18 +28,19 @@ const RemovedWidgetComponent = ({ dom }) => {
     }
   }, [dom, ref]);
 
-  return <div className={styles.inline_diff_remove_zone} ref={ref}></div>;
+  return <div className={styles.inline_diff_remove_zone} ref={ref} style={{ marginLeft: marginWidth + 'px' }}></div>;
 };
 
 class RemovedZoneWidget extends ZoneWidget {
   private root: ReactDOMClient.Root;
 
   _fillContainer(container: HTMLElement): void {
+    container.classList.add(styles.inline_diff_remove_zone_widget_container);
     this.root = ReactDOMClient.createRoot(container);
   }
 
-  renderDom(dom: HTMLElement): void {
-    this.root.render(<RemovedWidgetComponent dom={dom} />);
+  renderDom(dom: HTMLElement, options: { marginWidth: number }): void {
+    this.root.render(<RemovedWidgetComponent dom={dom} marginWidth={options.marginWidth} />);
   }
 
   override revealRange(): void {}
@@ -49,41 +56,42 @@ interface ITextLinesTokens {
 }
 
 export class LivePreviewDiffDecorationModel extends Disposable {
-  private selectionDec: IEditorDecorationsCollection;
+  private zoneDec: IEditorDecorationsCollection;
 
   private activeLineDec: IEditorDecorationsCollection;
+  private pendingRangeDec: IEditorDecorationsCollection;
   private addedRangeDec: IEditorDecorationsCollection;
 
   private removedZoneWidgets: Array<RemovedZoneWidget> = [];
   private rawOriginalTextLinesTokens: ITextLinesTokens[] = [];
 
+  private zone: LineRange;
+
   constructor(private readonly monacoEditor: ICodeEditor, private readonly selection: Selection) {
     super();
 
-    this.selectionDec = this.monacoEditor.createDecorationsCollection();
+    this.zoneDec = this.monacoEditor.createDecorationsCollection();
 
     this.activeLineDec = this.monacoEditor.createDecorationsCollection();
     this.addedRangeDec = this.monacoEditor.createDecorationsCollection();
+    this.pendingRangeDec = this.monacoEditor.createDecorationsCollection();
 
-    this.selectionDec.set([
-      {
-        range: Range.fromPositions(
+    this.updateZone(
+      LineRange.fromRangeInclusive(
+        Range.fromPositions(
           { lineNumber: this.selection.startLineNumber, column: 1 },
           { lineNumber: this.selection.endLineNumber, column: Number.MAX_SAFE_INTEGER },
         ),
-        options: ModelDecorationOptions.register({
-          description: 'zone-decoration',
-          className: styles.inline_diff_zone,
-          isWholeLine: true,
-        }),
-      },
-    ]);
+      ),
+    );
   }
 
   override dispose(): void {
     super.dispose();
 
-    this.selectionDec.clear();
+    this.zoneDec.clear();
+
+    this.clearPendingLine();
     this.clearActiveLine();
     this.clearAddedLine();
     this.clearRemovedWidgets();
@@ -132,26 +140,33 @@ export class LivePreviewDiffDecorationModel extends Disposable {
       })),
       this.monacoEditor.getOptions(),
     );
-    widget.renderDom(dom);
+
+    const layoutInfo = this.monacoEditor.getOption(EditorOption.layoutInfo);
+    const marginWidth = layoutInfo.contentLeft;
+
+    widget.renderDom(dom, { marginWidth });
     widget.show(position, heightInLines);
 
     this.removedZoneWidgets.push(widget);
   }
 
-  public getZone(): Range {
-    const selectionRange = this.selectionDec.getRange(0)!;
+  public updateZone(newZone: LineRange): void {
+    this.zone = newZone;
 
-    const addedIndex = this.addedRangeDec.getRanges().length - 1;
-    const latestAddedRange = this.addedRangeDec.getRange(Math.max(0, addedIndex));
+    this.zoneDec.set([
+      {
+        range: newZone.toInclusiveRange()!,
+        options: ModelDecorationOptions.register({
+          description: ZoneDescription,
+          className: styles.inline_diff_zone,
+          isWholeLine: true,
+        }),
+      },
+    ]);
+  }
 
-    if (!latestAddedRange) {
-      return selectionRange;
-    }
-
-    return selectionRange.setEndPosition(
-      Math.max(selectionRange.endLineNumber, latestAddedRange.endLineNumber),
-      Math.max(selectionRange.endColumn, latestAddedRange.endColumn),
-    );
+  public getZone(): LineRange {
+    return this.zone;
   }
 
   public touchActiveLine(lineNumber: number) {
@@ -170,9 +185,10 @@ export class LivePreviewDiffDecorationModel extends Disposable {
           },
         ),
         options: ModelDecorationOptions.register({
-          description: 'activeLine-decoration',
+          description: ActiveLineDecoration,
           isWholeLine: true,
           className: styles.inline_diff_current,
+          zIndex: StackingLevel.WorkbenchEditor,
         }),
       },
     ]);
@@ -186,6 +202,30 @@ export class LivePreviewDiffDecorationModel extends Disposable {
         range: Range.fromPositions(
           {
             lineNumber: zone.startLineNumber + range.startLineNumber - 1,
+            column: 1,
+          },
+          {
+            lineNumber: zone.startLineNumber + range.endLineNumberExclusive - 2,
+            column: Number.MAX_SAFE_INTEGER,
+          },
+        ),
+        options: ModelDecorationOptions.register({
+          description: AddedRangeDecoration,
+          isWholeLine: true,
+          className: styles.inline_diff_added_range,
+        }),
+      })),
+    );
+  }
+
+  public touchPendingRange(range: LineRange) {
+    const zone = this.getZone();
+
+    this.pendingRangeDec.set([
+      {
+        range: Range.fromPositions(
+          {
+            lineNumber: zone.startLineNumber + range.startLineNumber - 1,
             column: 0,
           },
           {
@@ -194,12 +234,16 @@ export class LivePreviewDiffDecorationModel extends Disposable {
           },
         ),
         options: ModelDecorationOptions.register({
-          description: 'added-range-decoration',
+          description: PendingRangeDecoration,
           isWholeLine: true,
-          className: styles.inline_diff_added_range,
+          className: styles.inline_diff_pending_range,
         }),
-      })),
-    );
+      },
+    ]);
+  }
+
+  public clearPendingLine() {
+    this.pendingRangeDec.clear();
   }
 
   public clearActiveLine() {
