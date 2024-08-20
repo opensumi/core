8bb34e8b3a7adcd281850e0d65ed570062dbf0aa
野声 <lijiacheng.ljc@antgroup.com>
2024-08-20 13:53:57 +0800

    style: update style

--- packages/core-browser/src/design/rule.ts
+++ packages/core-browser/src/design/rule.ts
@@ -1,38 +1,51 @@
 /**
  * 定义了 zIndex 的层级
  */
-export const StackingLevel = {
+export const StackingLevel = Object.freeze({
   /**
    * 基础层级
    */
   Background: 0,
 
+  /**
+   * IDE 的主要内容区域，基础控件
+   */
   Workbench: 1,
-  WorkbenchEditor: 1,
 
+  /**
+   * 顶部的工具栏
+   */
   Toolbar: 2,
 
-  XtermDecoration: 8, // xterm.css 中 decoration 的 z-index 是 7，所以这里要比它大一点
+  /**
+   * xterm.css 中 decoration 的 z-index 是 7，所以这里要比它大一点
+   */
+  XtermDecoration: 8,
 
+  /**
+   * 顶部工具栏的下拉菜单
+   */
   ToolbarDropdown: 10,
 
   EditorTabbarCurrent: 11,
-  EditorTabbarOverlay: 15,
 
   ResizeHandle: 12,
 
+  EditorTabbarOverlay: 15,
+
   EditorFloatingContainer: 20,
 
-  /**
-   * 一级弹窗
-   */
+  // #region 中级弹窗区域
   Popup: 100,
-  Overlay: 800,
+  // #endregion
 
-  PopoverComponent: 1000,
-  PopoverComponentArrow: 1001,
+  // #region 顶级弹窗区域
+  Overlay: 800,
+  PopoverComponent: 999,
+  PopoverComponentArrow: 1000,
   OverlayTop: 1000,
-} as const;
+  // #endregion
+} as const);
 
 export const StackingLevelStr = Object.fromEntries(
   Object.entries(StackingLevel).map(([key, value]) => [key, value.toString()]),
