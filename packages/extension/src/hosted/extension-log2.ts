2f834f983020c0e50bbd645a0c29c1d5e6f7e68b
蛋总 <xinglong.wangwxl@antgroup.com>
2024-08-22 20:15:42 +0800

    fix: fix lint

--- packages/extension/src/hosted/extension-log2.ts
+++ packages/extension/src/hosted/extension-log2.ts
@@ -1,17 +1,8 @@
 import { Injector } from '@opensumi/di';
-import {
-  IExtensionLogger,
-  ILogService,
-  LogLevel,
-  SupportLogNamespace,
-} from '@opensumi/ide-core-common';
+import { IExtensionLogger, ILogService, LogLevel, SupportLogNamespace } from '@opensumi/ide-core-common';
 import { LogServiceManager } from '@opensumi/ide-logs/lib/node/log-manager';
 
 import { ExtHostAppConfig } from '../common/ext.process';
-import { IExtensionLogger, ILogService, LogLevel, SupportLogNamespace } from '@opensumi/ide-core-common';
-import { AppConfig } from '@opensumi/ide-core-node/lib/types';
-import { LogServiceManager } from '@opensumi/ide-logs/lib/node/log-manager';
-
 import { getNodeRequire } from '../common/utils';
 
 export class ExtensionLogger2 implements IExtensionLogger {
