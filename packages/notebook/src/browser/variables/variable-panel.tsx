6290e8f65089d1cf75f1b5f49bf1472aef32371c
LouisLv <zheyanglv@qq.com>
2024-11-28 12:10:44 +0800

    feat: add notebook variable inspector panel (#4186)
    * feat: add notebook variable inspector panel
    
    * fix: dispose event
    
    * chore: simplify create logic
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix: lint
    
    * fix: missing deps
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ packages/notebook/src/browser/variables/variable-panel.tsx
@@ -0,0 +1,73 @@
+import { Container, ViewManager, ViewRender } from '@difizen/mana-app';
+import { Empty } from 'antd';
+import React, { PropsWithChildren, memo, useEffect, useState } from 'react';
+
+import { URI, ViewState, localize, useInjectable } from '@opensumi/ide-core-browser';
+import { WorkbenchEditorService } from '@opensumi/ide-editor';
+import { WorkbenchEditorServiceImpl } from '@opensumi/ide-editor/lib/browser/workbench-editor.service';
+
+import { LIBRO_COMPONENTS_SCHEME_ID } from '../libro.protocol';
+import { ILibroOpensumiService } from '../libro.service';
+import { ManaContainer } from '../mana';
+
+import { LibroVariablePanelView } from './variable-view';
+
+export const VariablePanel = memo(({ viewState }: PropsWithChildren<{ viewState: ViewState }>) => {
+  const collapsePanelContainerStyle = {
+    width: viewState.width || '100%',
+    height: viewState.height,
+  };
+  const editorService = useInjectable<WorkbenchEditorServiceImpl>(WorkbenchEditorService);
+  const libroOpensumiService = useInjectable<ILibroOpensumiService>(ILibroOpensumiService);
+  const manaContainer = useInjectable<Container>(ManaContainer);
+
+  const [libroVariablePanelView, setLibroVariablePanelView] = useState<LibroVariablePanelView | undefined>(undefined);
+
+  const createVariablePanelView = async (uri: URI, libro: any) => {
+    const viewManager = manaContainer.get(ViewManager);
+    const view = await viewManager.getOrCreateView<LibroVariablePanelView>(LibroVariablePanelView, {
+      id: uri.toString(),
+    });
+    view?.pause();
+    view.parent = libro;
+    view.update();
+    setLibroVariablePanelView(view);
+  };
+
+  useEffect(() => {
+    if (editorService.currentResource?.uri.path.ext === `.${LIBRO_COMPONENTS_SCHEME_ID}`) {
+      libroOpensumiService
+        .getOrCreateLibroView(editorService.currentResource.uri)
+        .then((libro) => createVariablePanelView(editorService.currentResource!.uri, libro));
+    }
+
+    const toDispose = editorService.onActiveResourceChange((e) => {
+      if (e?.uri.path.ext === `.${LIBRO_COMPONENTS_SCHEME_ID}`) {
+        libroOpensumiService.getOrCreateLibroView(e.uri).then((libro) => createVariablePanelView(e.uri, libro));
+      } else {
+        setLibroVariablePanelView(undefined);
+      }
+    });
+
+    return () => {
+      toDispose.dispose();
+    };
+  }, []);
+  if (libroVariablePanelView) {
+    return (
+      <>
+        <div style={collapsePanelContainerStyle}>{<ViewRender view={libroVariablePanelView}></ViewRender>}</div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Empty
+          image={Empty.PRESENTED_IMAGE_SIMPLE}
+          description={localize('notebook.variable.panel.unsupported')}
+          className='libro-variable-empty'
+        />
+      </>
+    );
+  }
+});
