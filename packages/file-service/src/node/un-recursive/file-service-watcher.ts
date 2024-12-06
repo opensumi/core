984bdc6ce8916ae28d7f6ffb3003e9ab8f40963b
xubing.bxb <xubing.bxb@alibaba-inc.com>
2024-12-06 15:03:20 +0800

    chore: rewatch logic

--- packages/file-service/src/node/un-recursive/file-service-watcher.ts
+++ packages/file-service/src/node/un-recursive/file-service-watcher.ts
@@ -136,14 +136,14 @@ export class UnRecursiveFileSystemWatcher implements IFileSystemWatcherServer {
           }, UnRecursiveFileSystemWatcher.FILE_DELETE_HANDLER_DELAY);
         } else {
           setTimeout(async () => {
-            // if (changeFileName === signalDoc) {
-            if (fs.pathExistsSync(basePath)) {
-              this.pushUpdated(basePath);
-            } else {
-              this.pushDeleted(basePath);
-              signalDoc = '';
+            if (changeFileName === signalDoc) {
+              if (fs.pathExistsSync(basePath)) {
+                this.pushUpdated(basePath);
+              } else {
+                this.pushDeleted(basePath);
+                signalDoc = '';
+              }
             }
-            // }
           }, UnRecursiveFileSystemWatcher.FILE_DELETE_HANDLER_DELAY);
         }
       });
