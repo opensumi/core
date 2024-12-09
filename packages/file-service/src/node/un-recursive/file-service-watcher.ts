0ebfd9c3d1dfb18e98d11603f23d9b94d261c7b0
liuqian <xubing.bxb@alibaba-inc.com>
2024-12-09 14:02:36 +0800

    chore: setclient call

--- packages/file-service/src/node/un-recursive/file-service-watcher.ts
+++ packages/file-service/src/node/un-recursive/file-service-watcher.ts
@@ -244,7 +244,7 @@ export class UnRecursiveFileSystemWatcher implements IFileSystemWatcherServer {
   }
 
   setClient(client: FileSystemWatcherClient | undefined) {
-    if (this.client && this.toDispose.disposed) {
+    if (this.toDispose.disposed) {
       return;
     }
     this.client = client;
