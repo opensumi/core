7098925ac40ea294e108416329590b6eab56a700
qingyi.xjh <qingyi.xjh@antgroup.com>
2024-12-19 19:27:54 +0800

    fix: lint

--- packages/monaco/src/common/languages.ts
+++ packages/monaco/src/common/languages.ts
@@ -1,20 +1,20 @@
-export { LanguageConfiguration } from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages/languageConfiguration';
 export { IRelativePattern } from '@opensumi/monaco-editor-core/esm/vs/base/common/glob';
-export { LanguageSelector, LanguageFilter } from '@opensumi/monaco-editor-core/esm/vs/editor/common/languageSelector';
-export * from './common';
-export * from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages/languageConfiguration';
-export * from '@opensumi/monaco-editor-core/esm/vs/editor/standalone/common/monarch/monarchTypes';
 export {
   ILanguageExtensionPoint,
   ILanguageIcon,
   ILanguageSelection,
 } from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages/language';
+export * from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages/languageConfiguration';
+export { LanguageConfiguration } from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages/languageConfiguration';
+export { LanguageFilter, LanguageSelector } from '@opensumi/monaco-editor-core/esm/vs/editor/common/languageSelector';
+export * from '@opensumi/monaco-editor-core/esm/vs/editor/standalone/common/monarch/monarchTypes';
+export * from './common';
 
 export type {
-  TokensProvider,
   EncodedTokensProvider,
   IEncodedLineTokens,
   ILineTokens,
+  TokensProvider,
 } from '@opensumi/monaco-editor-core/esm/vs/editor/standalone/browser/standaloneLanguages';
 
 export { NewSymbolNameTriggerKind } from '@opensumi/monaco-editor-core/esm/vs/editor/common/languages';
--- packages/monaco/src/common/observable.ts
+++ packages/monaco/src/common/observable.ts
@@ -12,53 +12,56 @@ export * from '@opensumi/monaco-editor-core/esm/vs/base/common/observableInterna
 export * from '@opensumi/monaco-editor-core/esm/vs/base/common/observableInternal/utils';
 
 export function autorunDelta<T>(
-    observable: IObservable<T>,
-    handler: (args: { lastValue: T | undefined; newValue: T }) => void
+  observable: IObservable<T>,
+  handler: (args: { lastValue: T | undefined; newValue: T }) => void,
 ): IDisposable {
-    let _lastValue: T | undefined;
-    return autorunOpts({ debugReferenceFn: handler }, (reader) => {
-        const newValue = observable.read(reader);
-        const lastValue = _lastValue;
-        _lastValue = newValue;
-        handler({ lastValue, newValue });
-    });
+  let _lastValue: T | undefined;
+  return autorunOpts({ debugReferenceFn: handler }, (reader) => {
+    const newValue = observable.read(reader);
+    const lastValue = _lastValue;
+    _lastValue = newValue;
+    handler({ lastValue, newValue });
+  });
 }
 
 export function debouncedObservable2<T>(observable: IObservable<T>, debounceMs: number): IObservable<T> {
-	let hasValue = false;
-	let lastValue: T | undefined;
+  let hasValue = false;
+  let lastValue: T | undefined;
 
-	let timeout: any = undefined;
+  let timeout: any;
 
-	return observableFromEvent<T, void>(cb => {
-		const d = autorun(reader => {
-			const value = observable.read(reader);
+  return observableFromEvent<T, void>(
+    (cb) => {
+      const d = autorun((reader) => {
+        const value = observable.read(reader);
 
-			if (!hasValue) {
-				hasValue = true;
-				lastValue = value;
-			} else {
-				if (timeout) {
-					clearTimeout(timeout);
-				}
-				timeout = setTimeout(() => {
-					lastValue = value;
-					cb();
-				}, debounceMs);
-			}
-		});
-		return {
-			dispose() {
-				d.dispose();
-				hasValue = false;
-				lastValue = undefined;
-			},
-		};
-	}, () => {
-		if (hasValue) {
-			return lastValue!;
-		} else {
-			return observable.get();
-		}
-	});
+        if (!hasValue) {
+          hasValue = true;
+          lastValue = value;
+        } else {
+          if (timeout) {
+            clearTimeout(timeout);
+          }
+          timeout = setTimeout(() => {
+            lastValue = value;
+            cb();
+          }, debounceMs);
+        }
+      });
+      return {
+        dispose() {
+          d.dispose();
+          hasValue = false;
+          lastValue = undefined;
+        },
+      };
+    },
+    () => {
+      if (hasValue) {
+        return lastValue!;
+      } else {
+        return observable.get();
+      }
+    },
+  );
 }
