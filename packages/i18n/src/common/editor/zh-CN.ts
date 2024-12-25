b27409e5e6ab41f72d69f7b24be2e7c96fe0edd8
hacke2 <xinglong.wangwxl@antgroup.com>
2024-12-25 13:55:20 +0800

    feat: support toggle column selection (#4265)

--- packages/i18n/src/common/editor/zh-CN.ts
+++ packages/i18n/src/common/editor/zh-CN.ts
@@ -2,4 +2,6 @@ export const editorLocalizations = {
   'editor.format.preferredFormatterNotFound': '未找到 {1} 的首选格式化程序 {0}',
   'editor.configuration.unicodeHighlight.allowedCharacters': '定义不被高亮显示的允许字符',
   'editor.configuration.unicodeHighlight.allowedLocales': '在允许的区域中常见的Unicode字符不会被高亮显示',
+  'editor.configuration.columnSelection': '启用列选择模式',
+  'editor.action.toggleColumnSelection': '切换列选择模式',
 };
