c17457163ba7a9ff94697ecb535d77c1e1315411
一辉 <32118806+hui2334387208@users.noreply.github.com>
2025-01-03 16:10:50 +0800

    chore: update WebviewPortMapping and proposed type (#4295)
    * feat: update WebviewPortMapping type
    
    * chore: update DocumentFilter、WorkspaceTrustRequestOptions、requestWorkspaceTrust type positioin
    
    * chore: add proposed vscode statement
    
    * chore: update proposed quote

--- packages/types/vscode/typings/vscode.d.ts
+++ packages/types/vscode/typings/vscode.d.ts
@@ -955,6 +955,21 @@ declare module 'vscode' {
   }
 
   /**
+   * Defines a port mapping used for localhost inside the webview.
+   */
+  export interface WebviewPortMapping {
+    /**
+     * Localhost port to remap inside the webview.
+     */
+    readonly webviewPort: number;
+
+    /**
+     * Destination port. The `webviewPort` is resolved to this port.
+     */
+    readonly extensionHostPort: number;
+  }
+
+  /**
    * Content settings for a webview.
    */
   export interface WebviewOptions {
