15c0ddff207bafacba9eac59fe4a0d2169fc0285
liuqian <xubing.bxb@alibaba-inc.com>
2025-01-08 09:06:24 +0800

    fix: dispose first

--- packages/file-service/src/node/hosted/watcher.host.service.ts
+++ packages/file-service/src/node/hosted/watcher.host.service.ts
@@ -19,7 +19,7 @@ import { WatcherProcessLogger } from './watch-process-log';
 
 const watcherPlaceHolder = {
   disposable: {
-    dispose: () => {},
+    dispose: () => { },
   },
   handlers: [],
 };
@@ -77,9 +77,9 @@ export class WatcherHostServiceImpl implements IWatcherHostService {
 
       // rewatch
       for (const [_uri, { options, disposable }] of this.watcherCollection) {
-        this.doWatch(Uri.parse(_uri), options);
         this.logger.log('rewatch file changes: ', _uri, ' recursive: ', options?.recursive);
         disposable.dispose();
+        this.doWatch(Uri.parse(_uri), options);
       }
     }
 
