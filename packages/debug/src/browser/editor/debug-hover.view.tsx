96ca281c3f237fd3b4a48c211e0a435ed712b12b
kuiwu <danwu.wdw@alibaba-inc.com>
2025-02-05 14:35:42 +0800

    fix: display debug hover view with correct size config

--- packages/debug/src/browser/editor/debug-hover.view.tsx
+++ packages/debug/src/browser/editor/debug-hover.view.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import { INodeRendererWrapProps, IRecycleTreeHandle, RecycleTree, TreeNodeEvent } from '@opensumi/ide-components';
 import { useInjectable } from '@opensumi/ide-core-browser';
+import { LayoutViewSizeConfig } from '@opensumi/ide-core-browser/lib/layout/constants';
 import { IDisposable } from '@opensumi/ide-core-common';
 
 import {
@@ -19,8 +20,11 @@ import styles from './debug-hover.module.less';
 
 export const DebugHoverView = () => {
   const debugHoverTreeModelService: DebugHoverTreeModelService = useInjectable(DebugHoverTreeModelService);
+  const layoutViewSize = useInjectable<LayoutViewSizeConfig>(LayoutViewSizeConfig);
 
   const DEFAULT_LAYOUT_HEIGHT = 250;
+  const DEFAULT_MAX_HEIGHT = 500;
+  const DEFAULT_HOVER_WEIGET_MARGIN_BOTTOM = 4;
   const [model, setModel] = React.useState<{ treeModel?: DebugHoverModel; variable?: DebugVariable }>({});
   const [treeLayoutHeight, setTreeLayoutHeight] = React.useState<number>(DEFAULT_LAYOUT_HEIGHT);
   const wrapperRef = React.useRef<HTMLDivElement | null>(null);
@@ -44,9 +48,17 @@ export const DebugHoverView = () => {
     setTreeLayoutHeight(DEFAULT_LAYOUT_HEIGHT);
 
     if (model.treeModel) {
-      disposable = model.treeModel.root.watcher.on(TreeNodeEvent.DidChangeExpansionState, (data) => {
-        const treeHeight = Math.max(DEFAULT_LAYOUT_HEIGHT, ~~model.treeModel?.root.branchSize! * 22);
-        setTreeLayoutHeight(Math.min(500, treeHeight));
+      disposable = model.treeModel.root.watcher.on(TreeNodeEvent.DidChangeExpansionState, () => {
+        const treeHeight = Math.max(DEFAULT_LAYOUT_HEIGHT, (model.treeModel?.root.branchSize || 0) * 22);
+        const rect = wrapperRef.current?.getBoundingClientRect();
+        if (rect) {
+          const top = rect.top;
+          const maxHeight =
+            window.innerHeight - top - layoutViewSize.statusBarHeight - DEFAULT_HOVER_WEIGET_MARGIN_BOTTOM;
+          setTreeLayoutHeight(Math.min(maxHeight, treeHeight));
+        } else {
+          setTreeLayoutHeight(Math.min(DEFAULT_MAX_HEIGHT, treeHeight));
+        }
       });
     }
     return () => {
