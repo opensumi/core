7ad29c4b255567bafe151de6d3a71cf1e9a6c36f
ensorrow <zheyanglv@qq.com>
2025-02-24 11:40:28 +0800

    refactor: use openai-compatible to support providerOptions

--- packages/ai-native/src/node/openai/openai-language-model.ts
+++ packages/ai-native/src/node/openai/openai-language-model.ts
@@ -1,25 +1,28 @@
-import { OpenAIProvider, createOpenAI } from '@ai-sdk/openai';
+import { OpenAICompatibleProvider, createOpenAICompatible } from '@ai-sdk/openai-compatible';
+import { LanguageModelV1 } from 'ai';
 
 import { Injectable } from '@opensumi/di';
 import { AINativeSettingSectionsId, IAIBackServiceOption } from '@opensumi/ide-core-common';
 
 import { BaseLanguageModel } from '../base-language-model';
+
 export const DeepSeekModelIdentifier = Symbol('DeepSeekModelIdentifier');
 
 @Injectable()
 export class OpenAIModel extends BaseLanguageModel {
-  protected initializeProvider(options: IAIBackServiceOption): OpenAIProvider {
+  protected initializeProvider(options: IAIBackServiceOption): OpenAICompatibleProvider {
     const apiKey = options.apiKey;
     if (!apiKey) {
       throw new Error(`Please provide OpenAI API Key in preferences (${AINativeSettingSectionsId.OpenaiApiKey})`);
     }
-    return createOpenAI({
+    return createOpenAICompatible({
       apiKey,
       baseURL: options.baseURL || 'https://dashscope.aliyuncs.com/compatible-mode/v1',
+      name: 'openai',
     });
   }
 
-  protected getModelIdentifier(provider: OpenAIProvider, modelId?: string) {
+  protected getModelIdentifier(provider: OpenAICompatibleProvider, modelId?: string): LanguageModelV1 {
     return provider(modelId || 'qwen-max');
   }
 }
