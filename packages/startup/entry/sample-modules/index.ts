7c555828d1e4a21d62989c12b64339e9bbeef011
xinglong.wangwxl <xinglong.wangwxl@antgroup.com>
2025-03-06 17:18:11 +0800

    feat: provider default apply service

--- packages/startup/entry/sample-modules/index.ts
+++ packages/startup/entry/sample-modules/index.ts
@@ -1,10 +1,8 @@
 import { Injectable, Provider } from '@opensumi/di';
-import { BaseApplyService } from '@opensumi/ide-ai-native/lib/browser/mcp/base-apply.service';
 import { BrowserModule } from '@opensumi/ide-core-browser';
 import { AbstractNodeExtProcessService } from '@opensumi/ide-extension/lib/common/extension.service';
 
 import { AINativeContribution } from './ai-native/ai-native.contribution';
-import { ApplyService } from './ai-native/apply.service';
 import { DebugConfigurationContribution } from './debug-configuration.contribution';
 import { EditorEmptyComponentContribution } from './editor-empty-component.contribution';
 import { MenuBarContribution } from './menu-bar/menu-bar.contribution';
@@ -23,10 +21,6 @@ export class SampleModule extends BrowserModule {
       token: AbstractNodeExtProcessService,
       useClass: OverrideExtensionNodeService,
       override: true,
-    },
-    {
-      token: BaseApplyService,
-      useClass: ApplyService,
     },
   ];
 }
