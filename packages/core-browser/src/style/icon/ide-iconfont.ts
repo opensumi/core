f369bd36a7c2162034c69353a7462bdabad0a8bb
kuiwu <danwu.wdw@alibaba-inc.com>
2025-03-08 22:54:38 +0800

    chore: update css source

--- packages/core-browser/src/style/icon/ide-iconfont.ts
+++ packages/core-browser/src/style/icon/ide-iconfont.ts
@@ -1,4 +1,4 @@
-export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/a/font_2993996_4qu7xlrrnek.css';
+export const IDE_ICONFONT_CN_CSS = '//at.alicdn.com/t/a/font_1432262_wmroomx4lyf.css';
 
 export const IDE_ICONFONT_CN_JS = IDE_ICONFONT_CN_CSS.replace(/\.css$/, '.js');
 
